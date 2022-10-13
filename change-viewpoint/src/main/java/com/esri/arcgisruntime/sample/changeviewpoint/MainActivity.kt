d9184f05534c043043dfdd97d8f17406eb5fdae9
Shubham Sharma <shubhamsharma@esri.com>
2022-10-13 16:52:57 -0700

    sample init

---
+++ change-viewpoint/src/main/java/com/esri/arcgisruntime/sample/changeviewpoint/MainActivity.kt
@@ -0,0 +1,50 @@
+/* Copyright 2022 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.esri.arcgisruntime.sample.changeviewpoint
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import arcgisruntime.ApiKey
+import arcgisruntime.ArcGISRuntimeEnvironment
+import arcgisruntime.mapping.ArcGISMap
+import arcgisruntime.mapping.BasemapStyle
+import arcgisruntime.mapping.view.MapView
+import com.esri.arcgisruntime.sample.changeviewpoint.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = MainActivity::class.java.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // authentication with an API key or named user is
+        // required to access basemaps and other location services
+        ArcGISRuntimeEnvironment.apiKey = ApiKey.create(BuildConfig.API_KEY)
+
+        // set up data binding for the activity
+        val activityMainBinding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val mapView = activityMainBinding.mapView
+        lifecycle.addObserver(mapView)
+
+        // create and add a map with a navigation night basemap style
+        val map = ArcGISMap(BasemapStyle.ArcGISNavigationNight)
+        mapView.map = map
+    }
+}
