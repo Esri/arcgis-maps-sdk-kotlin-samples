83f380c2e7eda68c29cbf0099e37d1f26c206235
Shubham Sharma <shubhamsharma@esri.com>
2022-11-08 11:05:56 -0800

    write up sample!

--- find-address-with-reverse-geocode/src/main/java/com/esri/arcgismaps/sample/findaddresswithreversegeocode/MainActivity.kt
+++ find-address-with-reverse-geocode/src/main/java/com/esri/arcgismaps/sample/findaddresswithreversegeocode/MainActivity.kt
@@ -16,19 +16,66 @@
 
 package com.esri.arcgismaps.sample.findaddresswithreversegeocode
 
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import com.arcgismaps.ApiKey
 import com.arcgismaps.ArcGISEnvironment
+import com.arcgismaps.geometry.GeometryEngine
+import com.arcgismaps.geometry.Point
 import com.arcgismaps.mapping.ArcGISMap
 import com.arcgismaps.mapping.BasemapStyle
+import com.arcgismaps.mapping.Viewpoint
+import com.arcgismaps.mapping.symbology.PictureMarkerSymbol
+import com.arcgismaps.mapping.view.Graphic
+import com.arcgismaps.mapping.view.GraphicsOverlay
+import com.arcgismaps.mapping.view.SingleTapConfirmedEvent
+import com.arcgismaps.tasks.geocode.GeocodeResult
+import com.arcgismaps.tasks.geocode.LocatorTask
 
 import com.esri.arcgismaps.sample.findaddresswithreversegeocode.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = MainActivity::class.java.simpleName
+
+    // set up data binding for the activity
+    private val activityMainBinding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    // create a MapView using binding
+    private val mapView by lazy {
+        activityMainBinding.mapView
+    }
+
+    // display the city of the tapped location
+    private val titleTV by lazy {
+        activityMainBinding.titleTV
+    }
+
+    // display the metro area of the tapped location
+    private val descriptionTV by lazy {
+        activityMainBinding.descriptionTV
+    }
+
+    // create a graphics overlay
+    private val graphicsOverlay = GraphicsOverlay()
+
+    // service url to be provided to the LocatorTask (geocoder)
+    private val geocodeServer =
+        "https://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer"
+
+    // locator task to provide geocoding services
+    private val locatorTask = LocatorTask(geocodeServer)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,15 +83,81 @@ class MainActivity : AppCompatActivity() {
         // authentication with an API key or named user is
         // required to access basemaps and other location services
         ArcGISEnvironment.apiKey = ApiKey.create(BuildConfig.API_KEY)
-
-        // set up data binding for the activity
-        val activityMainBinding: ActivityMainBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_main)
-        val mapView = activityMainBinding.mapView
         lifecycle.addObserver(mapView)
 
-        // create and add a map with a navigation night basemap style
-        val map = ArcGISMap(BasemapStyle.ArcGISNavigationNight)
-        mapView.map = map
+        mapView.apply {
+            // add a map with a imagery basemap style
+            map = ArcGISMap(BasemapStyle.ArcGISImagery)
+            // add a graphics overlay to the map for showing where the user tapped
+            graphicsOverlays.add(graphicsOverlay)
+            // set initial viewpoint
+            setViewpoint(Viewpoint(34.058, -117.195, 5e4))
+        }
+
+        lifecycleScope.launch {
+            locatorTask.load().getOrElse {
+                showError(it.message.toString())
+            }
+            // locator task loaded, look for geo view tapped
+            geoViewTapped(mapView.onSingleTapConfirmed)
+        }
+    }
+
+    /**
+     * Displays a pin of the tapped location using [onSingleTapConfirmed]
+     * and finds address with reverse geocode
+     */
+    private suspend fun geoViewTapped(onSingleTapConfirmed: SharedFlow<SingleTapConfirmedEvent>) {
+        // clear existing graphics
+        graphicsOverlay.graphics.clear()
+
+        //add a graphic for the tapped point
+        val pinSymbol = PictureMarkerSymbol(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.ic_baseline_pin_24
+            ) as BitmapDrawable
+        )
+        pinSymbol.apply {
+            // resize the dimensions of the symbol
+            width = 60f
+            height = 60f
+            // the image is a pin so offset the image so that the pinpoint
+            // is on the point rather than the image's true center.
+            leaderOffsetX = 30f
+            offsetY = 14f
+        }
+        // collect map tapped event
+        onSingleTapConfirmed.collect { event ->
+            // get map point tapped, return if null
+            val mapPoint = event.mapPoint ?: return@collect
+            // add a graphic for the tapped point
+            val pinGraphic = Graphic(mapPoint, pinSymbol)
+            graphicsOverlay.graphics.add(pinGraphic)
+            // normalize the geometry - needed if the user crosses the international date line.
+            val normalizedPoint = GeometryEngine.normalizeCentralMeridian(mapPoint) as Point
+            // reverse geocode to get address
+            val addresses = locatorTask.reverseGeocode(normalizedPoint).getOrElse {
+                showError(it.message.toString())
+            } as List<GeocodeResult>
+            // get the first result
+            val address = addresses.first()
+            // use the city and region for the title
+            val title = address.attributes["Address"].toString()
+            // use the metro area for the description details
+            val description = "${address.attributes["City"]} " +
+                    "${address.attributes["Region"]} " +
+                    "${address.attributes["CountryCode"]}"
+            // set the strings to the text views
+            titleTV.text = title
+            descriptionTV.text = description
+        }
+    }
+
+
+    private fun showError(errorMessage: String) {
+        Log.e(TAG, errorMessage)
+        Snackbar.make(mapView, errorMessage, Snackbar.LENGTH_SHORT).show()
     }
 }
+
