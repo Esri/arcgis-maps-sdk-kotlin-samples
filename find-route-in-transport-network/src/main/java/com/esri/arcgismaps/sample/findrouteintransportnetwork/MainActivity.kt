a965dd5c0c557d668c8ce3a241c92d3a0f7930af
Shubham Sharma <shubhamsharma@esri.com>
2023-01-18 19:45:19 -0800

    added sample implementation

--- find-route-in-transport-network/src/main/java/com/esri/arcgismaps/sample/findrouteintransportnetwork/MainActivity.kt
+++ find-route-in-transport-network/src/main/java/com/esri/arcgismaps/sample/findrouteintransportnetwork/MainActivity.kt
@@ -16,20 +16,40 @@
 
 package com.esri.arcgismaps.sample.findrouteintransportnetwork
 
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import com.arcgismaps.ApiKey
 import com.arcgismaps.ArcGISEnvironment
+import com.arcgismaps.Color
+import com.arcgismaps.geometry.Envelope
+import com.arcgismaps.geometry.Point
+import com.arcgismaps.geometry.SpatialReference
 import com.arcgismaps.mapping.ArcGISMap
-import com.arcgismaps.mapping.BasemapStyle
+import com.arcgismaps.mapping.Basemap
+import com.arcgismaps.mapping.layers.ArcGISTiledLayer
+import com.arcgismaps.mapping.layers.TileCache
+import com.arcgismaps.mapping.symbology.CompositeSymbol
+import com.arcgismaps.mapping.symbology.HorizontalAlignment
+import com.arcgismaps.mapping.symbology.PictureMarkerSymbol
+import com.arcgismaps.mapping.symbology.SimpleLineSymbol
+import com.arcgismaps.mapping.symbology.SimpleLineSymbolStyle
+import com.arcgismaps.mapping.symbology.TextSymbol
+import com.arcgismaps.mapping.symbology.VerticalAlignment
+import com.arcgismaps.mapping.view.Graphic
 import com.arcgismaps.mapping.view.GraphicsOverlay
+import com.arcgismaps.mapping.view.ScreenCoordinate
 import com.arcgismaps.tasks.networkanalysis.RouteParameters
 import com.arcgismaps.tasks.networkanalysis.RouteTask
+import com.arcgismaps.tasks.networkanalysis.Stop
 import com.esri.arcgismaps.sample.findrouteintransportnetwork.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.launch
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
@@ -40,6 +60,10 @@ class MainActivity : AppCompatActivity() {
         DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 
+    private val provisionPath: String by lazy {
+        getExternalFilesDir(null)?.path.toString() + File.separator + getString(R.string.app_name)
+    }
+
     private val mapView by lazy {
         activityMainBinding.mapView
     }
@@ -48,8 +72,8 @@ class MainActivity : AppCompatActivity() {
         activityMainBinding.toggleButtons
     }
 
-    private val deleteButton by lazy {
-        activityMainBinding.deleteButton
+    private val clearButton by lazy {
+        activityMainBinding.clearButton
     }
 
     private val distanceTimeTextView by lazy {
@@ -59,6 +83,9 @@ class MainActivity : AppCompatActivity() {
     private val stopsOverlay: GraphicsOverlay by lazy { GraphicsOverlay() }
     private val routeOverlay: GraphicsOverlay by lazy { GraphicsOverlay() }
 
+    // create a route task to calculate routes
+    private var routeTask: RouteTask? = null
+
     private var routeParameters: RouteParameters? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -67,11 +94,192 @@ class MainActivity : AppCompatActivity() {
         // authentication with an API key or named user is
         // required to access basemaps and other location services
         ArcGISEnvironment.apiKey = ApiKey.create(BuildConfig.API_KEY)
+        // some parts of the API require an Android Context to properly interact with Android system
+        // features, such as LocationProvider and application resources
+        ArcGISEnvironment.applicationContext = applicationContext
         lifecycle.addObserver(mapView)
 
-        // create and add a map with a navigation night basemap style
-        val map = ArcGISMap(BasemapStyle.ArcGISNavigationNight)
-        mapView.map = map
+
+        // create a tile cache from the .tpkx file
+        val tileCache = TileCache(provisionPath + getString(R.string.tpkx_path))
+        val tiledLayer = ArcGISTiledLayer(tileCache)
+        // make a basemap with the tiled layer and add it to the mapview as an ArcGISMap
+        mapView.map = ArcGISMap(Basemap(tiledLayer))
+
+        // add the graphics overlays to the map view
+        mapView.graphicsOverlays.addAll(listOf(stopsOverlay, routeOverlay))
+
+        val temp = File(provisionPath + getString(R.string.geodatabase_path))
+        routeTask = RouteTask(temp.path, "Streets_ND")
+
+        // load the route task
+        lifecycleScope.launch {
+            routeTask?.load()?.onFailure {
+                showError(it.message.toString())
+            }?.onSuccess {
+                // use the default parameters for the route calculation
+                routeParameters = routeTask?.createDefaultParameters()?.getOrThrow()
+            }
+        }
+
+        toggleButtons.addOnButtonCheckedListener { _, checkedId, _ ->
+            when (checkedId) {
+                R.id.fastestButton -> routeParameters?.travelMode =
+                    routeTask?.getRouteTaskInfo()?.travelModes?.get(0) // calculate fastest route
+                R.id.shortestButton -> routeParameters?.travelMode =
+                    routeTask?.getRouteTaskInfo()?.travelModes?.get(1) // calculate shortest route
+            }
+            // update route based on selection
+            updateRoute()
+        }
+
+        // make a clear button to reset the stops and routes
+        clearButton.setOnClickListener {
+            stopsOverlay.graphics.clear()
+            routeOverlay.graphics.clear()
+        }
+
+        val envelope = Envelope(
+            Point(-13045352.223196, 3864910.900750, 0.0, SpatialReference.webMercator()),
+            Point(-13024588.857198, 3838880.505604, 0.0, SpatialReference.webMercator())
+        )
+        val boundarySymbol = SimpleLineSymbol(SimpleLineSymbolStyle.Dash, Color.green, 5f)
+
+        val graphicsOverlay = GraphicsOverlay().apply {
+            graphics.add(Graphic(envelope, boundarySymbol))
+        }
+        mapView.graphicsOverlays.add(graphicsOverlay)
+
+        // set up the touch listeners on the map view
+        createMapGestures()
+    }
+
+    /**
+     * Sets up the onTouchListener for the mapView.
+     * For single taps, graphics will be selected.
+     * For double touch drags, graphics will be moved.
+     * */
+    private fun createMapGestures() {
+        with(lifecycleScope) {
+
+            // add graphic at the tapped coordinate
+            launch {
+                mapView.onSingleTapConfirmed.collect { tapEvent ->
+                    val screenCoordinate = tapEvent.screenCoordinate
+                    addOrSelectGraphic(screenCoordinate)
+                }
+            }
+            // move the graphic to a different coordinate
+            launch {
+                //TODO
+            }
+        }
+    }
+
+    /**
+     * Updates the calculated route by calling routeTask.solveRouteAsync().
+     * Creates a graphic to display the route.
+     * */
+    private fun updateRoute() = lifecycleScope.launch {
+        // get a list of stops from the graphics currently on the graphics overlay.
+        val stops = stopsOverlay.graphics.map {
+            Stop(it.geometry as Point)
+        }
+
+        // do not calculate a route if there is only one stop
+        if (stops.size <= 1) return@launch
+
+        routeParameters?.setStops(stops)
+
+        // solve the route
+        val results = routeParameters?.let { routeTask?.solveRoute(it) }
+        if (results != null) {
+            results.onFailure {
+                showError("No route solution. ${it.message}")
+                routeOverlay.graphics.clear()
+            }.onSuccess { routeResult ->
+                // get the first solved route result
+                val route = routeResult.routes[0]
+
+                // create graphic for route
+                val graphic = Graphic(
+                    route.routeGeometry, SimpleLineSymbol(
+                        SimpleLineSymbolStyle.Solid,
+                        Color.green, 3F
+                    )
+                )
+                routeOverlay.graphics.clear()
+                routeOverlay.graphics.add(graphic)
+            }
+        }
+    }
+
+    /**
+     * Selects a graphic if there is one at the
+     * provided [screenCoordinate] or, if there is
+     * none, creates a new graphic.
+     * */
+    private suspend fun addOrSelectGraphic(screenCoordinate: ScreenCoordinate) {
+        // identify the selected graphic
+        val result = mapView.identifyGraphicsOverlay(stopsOverlay, screenCoordinate, 10.0, false)
+
+        result.onFailure {
+            showError(it.message.toString())
+        }.onSuccess { identifyGraphicsOverlayResult ->
+            val graphics = identifyGraphicsOverlayResult.graphics
+
+            // unselect everything
+            if (stopsOverlay.selectedGraphics.isNotEmpty()) {
+                stopsOverlay.unselectGraphics(stopsOverlay.selectedGraphics)
+            }
+
+            // if the user tapped on something, select it
+            if (graphics.isNotEmpty()) {
+                val firstGraphic = graphics[0]
+                firstGraphic.isSelected = true
+            } else {
+                // there is no graphic at this location
+                // make a new graphic at the tapped location
+                val locationPoint = mapView.screenToLocation(screenCoordinate)
+                createStopSymbol(stopsOverlay.graphics.size + 1, locationPoint)
+            }
+        }
+    }
+
+    /**
+     * Creates a composite symbol to represent a numbered stop.
+     * The [stopNumber] is the ordinal number of this stop and the
+     * symbol will be placed at the [locationPoint].
+     */
+    private fun createStopSymbol(stopNumber: Int, locationPoint: Point?) {
+        // create a orange pin PictureMarkerSymbol
+        val pinSymbol = PictureMarkerSymbol(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.pin_symbol
+            ) as BitmapDrawable
+        )
+
+        // create black stop number TextSymbol
+        val stopNumberSymbol = TextSymbol(
+            stopNumber.toString(),
+            Color.black,
+            12f,
+            HorizontalAlignment.Center,
+            VerticalAlignment.Bottom
+        ).apply {
+            offsetY = -4f
+        }
+
+        // create a composite symbol and add the picture marker symbol and text symbol
+        val compositeSymbol = CompositeSymbol()
+        compositeSymbol.symbols.addAll(listOf(pinSymbol, stopNumberSymbol))
+
+        // create a graphic to add to the overlay and update the route
+        val graphic = Graphic(locationPoint, compositeSymbol)
+        stopsOverlay.graphics.add(graphic)
+
+        updateRoute()
     }
 
     private fun showError(message: String) {
