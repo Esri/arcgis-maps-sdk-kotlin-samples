508bccc68df56362d6ebb628190a2cd4bcf4ae0e
Priyanka Rupani <prupani@esri.com>
2023-01-20 12:49:09 -0800

    working sample

--- geocodeoffline/src/main/java/com/esri/arcgismaps/sample/geocodeoffline/MainActivity.kt
+++ geocodeoffline/src/main/java/com/esri/arcgismaps/sample/geocodeoffline/MainActivity.kt
@@ -21,6 +21,7 @@ import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.provider.BaseColumns
 import android.util.Log
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.core.content.ContextCompat
@@ -34,21 +35,19 @@ import com.arcgismaps.geometry.Point
 import com.arcgismaps.mapping.ArcGISMap
 import com.arcgismaps.mapping.Basemap
 import com.arcgismaps.mapping.Viewpoint
+import com.arcgismaps.mapping.ViewpointType
 import com.arcgismaps.mapping.layers.ArcGISTiledLayer
 import com.arcgismaps.mapping.layers.TileCache
 import com.arcgismaps.mapping.symbology.PictureMarkerSymbol
 import com.arcgismaps.mapping.view.Graphic
 import com.arcgismaps.mapping.view.GraphicsOverlay
 import com.arcgismaps.tasks.geocode.GeocodeParameters
+import com.arcgismaps.tasks.geocode.GeocodeResult
 import com.arcgismaps.tasks.geocode.LocatorTask
-import com.arcgismaps.tasks.geocode.ReverseGeocodeParameters
 import com.esri.arcgismaps.sample.geocodeoffline.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.launch
 import java.io.File
-import android.R
-
-import android.widget.AutoCompleteTextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -63,21 +62,6 @@ class MainActivity : AppCompatActivity() {
         activityMainBinding.mapView
     }
 
-    private val provisionPath: String by lazy {
-        getExternalFilesDir(null)?.path.toString() + File.separator + getString(R.string.app_name)
-    }
-
-    private val geocodeParameters: GeocodeParameters by lazy { GeocodeParameters() }
-
-    private val reverseGeocodeParameters: ReverseGeocodeParameters by lazy { ReverseGeocodeParameters() }
-
-    private val pinSymbol by lazy {
-        createPinSymbol()
-    }
-
-    // create a graphics overlay
-    private val graphicsOverlay by lazy { GraphicsOverlay() }
-
     // display the street of the tapped location
     private val titleTV by lazy {
         activityMainBinding.titleTV
@@ -88,8 +72,30 @@ class MainActivity : AppCompatActivity() {
         activityMainBinding.descriptionTV
     }
 
-    private val addressSearchView by lazy {
-        activityMainBinding.searchView.addressSearchView
+    private val provisionPath: String by lazy {
+        getExternalFilesDir(null)?.path.toString() + File.separator + getString(R.string.app_name)
+    }
+
+    private val pinSymbol by lazy {
+        createPinSymbol()
+    }
+
+    // create a graphics overlay
+    private val graphicsOverlay: GraphicsOverlay = GraphicsOverlay()
+
+    private val geocodeParameters: GeocodeParameters by lazy {
+        GeocodeParameters().apply {
+            // get all attributes
+            resultAttributeNames.add("*")
+            // get only the closest result
+            maxResults = 1
+        }
+    }
+
+    // Locator Task
+//    private val locatorTaskZip = File(provisionPath, getString(R.string.san_diego_loc))
+    private val locatorTask: LocatorTask by lazy {
+        LocatorTask(File(provisionPath, getString(R.string.san_diego_loc)).path)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -101,77 +107,95 @@ class MainActivity : AppCompatActivity() {
         lifecycle.addObserver(mapView)
 
         val tileCache = TileCache("$provisionPath/streetmap_SD.tpkx")
-        lifecycleScope.launch {
-            tileCache.load().onSuccess {
-                // set the map's viewpoint to the tileCache's full extent
-                val extent = tileCache.fullExtent
-                    ?: return@launch showError("Error retrieving extent of the feature layer")
-                mapView.setViewpoint(Viewpoint(extent))
-            }.onFailure {
-                showError("Error loading tileCache")
-            }
-        }
+//        lifecycleScope.launch {
+//            tileCache.load().onSuccess {
+//                // set the map's viewpoint to the tileCache's full extent
+//                val extent = tileCache.fullExtent
+//                    ?: return@launch showError("Error retrieving extent of the feature layer")
+//                mapView.setViewpoint(Viewpoint(extent))
+//            }.onFailure {
+//                showError("Error loading tileCache")
+//            }
+//        }
 
         // create a tiled layer and add it to as the base map
         val tiledLayer = ArcGISTiledLayer(tileCache)
         mapView.map = ArcGISMap(Basemap(tiledLayer))
+        // set map initial viewpoint
+        mapView.map?.initialViewpoint = Viewpoint(32.72, -117.155, 120000.0)
 
-        // Locator Task
+        // add a graphics overlay to the map view
+        mapView.graphicsOverlays.add(graphicsOverlay)
 
-        val locatorTaskZip = File(provisionPath, getString(R.string.san_diego_loc))
-        val locatorTask = LocatorTask(locatorTaskZip.path)
+//        // Locator Task
+//
+//        val locatorTaskZip = File(provisionPath, getString(R.string.san_diego_loc))
+//        val locatorTask = LocatorTask(locatorTaskZip.path)
 
         lifecycleScope.launch {
             locatorTask.load().onSuccess {
                 mapView.onSingleTapConfirmed.collect { event ->
                     event.mapPoint?.let { mapPoint -> findAddressReverseGeocode(mapPoint, locatorTask) }
                 }
-                setupAddressSearchView()
             }.onFailure {
                 showError(it.message.toString())
             }
         }
     }
 
-    private fun setupAddressSearchView() {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        val search = menu.findItem(R.id.appSearchBar)
+        // set up address search view and listeners
+        setupAddressSearchView(search.actionView as SearchView)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun setupAddressSearchView(addressSearchView: SearchView) {
         // get the list of pre-made suggestions
         val suggestions = resources.getStringArray(R.array.suggestion_items)
-
         // set up parameters for searching with MatrixCursor
         val columnNames = arrayOf(BaseColumns._ID, "address")
         val suggestionsCursor = MatrixCursor(columnNames)
-
         // add each address suggestion to a new row
         suggestions.forEachIndexed { i, s -> suggestionsCursor.addRow(arrayOf(i, s)) }
 
+//        // show the suggestions as soon as the user opens the search view
+//        findViewById<AutoCompleteTextView>(R.id.search_src_text).threshold = 0
+
         // column names for the adapter to look at when mapping data
         val cols = arrayOf("address")
-
         // ids that show where data should be assigned in the layout
         val to = intArrayOf(R.id.suggestionAddress)
+        val suggestionsAdapter = SimpleCursorAdapter(
+            this@MainActivity,
+            R.layout.suggestion_address,
+            suggestionsCursor,
+            cols,
+            to,
+            0)
 
-            val suggestionsAdapter = SimpleCursorAdapter(
-                this@MainActivity,
-                R.layout.suggestion_address,
-                suggestionsCursor,
-                cols,
-                to,
-                0)
         addressSearchView.suggestionsAdapter = suggestionsAdapter
         addressSearchView.setOnSuggestionListener(object: SearchView.OnSuggestionListener {
             override fun onSuggestionSelect(position: Int): Boolean {
                 return false
             }
-
             override fun onSuggestionClick(position: Int): Boolean {
-                geocode(suggestions[position])
+                geocodeAddress(suggestions[position])
+                addressSearchView.clearFocus()
                 return true
             }
         })
 
-        // show the suggestions as soon as the user opens the search view
-        findViewById<AutoCompleteTextView>(R.id.search_src_text).threshold = 0
-
+        // geocode the searched address on submit
+        addressSearchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(address: String): Boolean {
+                geocodeAddress(address)
+                addressSearchView.clearFocus()
+                return true
+            }
+            override fun onQueryTextChange(newText: String?) = true
+        })
     }
 
     /**
@@ -179,7 +203,46 @@ class MainActivity : AppCompatActivity() {
      *
      * @param address as a string to geocode
      */
-    private fun geocode(address: String) {
+    private fun geocodeAddress(address: String) = lifecycleScope.launch {
+
+        // clear graphics on map before displaying search results
+        graphicsOverlay.graphics.clear()
+
+        // load the locator task
+        locatorTask.load().getOrThrow()
+
+        // run the locatorTask geocode task, passing in the address
+        val geocodeResults = locatorTask.geocode(address, geocodeParameters).getOrThrow()
+        // no address found in geocode so return
+        if(geocodeResults.isEmpty()) {
+                showError("No address found for $address")
+                return@launch
+            }
+            // address found in geocode
+            else displaySearchResultOnMap(geocodeResults)
+    }
+
+    /**
+     * Turns a list of [geocodeResultList] into a point markers and adds it to the graphic overlay of the map.
+     */
+    private fun displaySearchResultOnMap(geocodeResultList: List<GeocodeResult>) {
+        // clear graphics overlay of existing graphics
+        graphicsOverlay.graphics.clear()
+
+        val resultLocationGraphic = Graphic(
+            geocodeResultList[0].displayLocation,
+            geocodeResultList[0].attributes, pinSymbol
+        )
+        graphicsOverlay.graphics.add(resultLocationGraphic)
+
+        descriptionTV.text = geocodeResultList[0].label
+
+        // get the envelop to set the viewpoint
+        val envelope = graphicsOverlay.extent ?: return showError("Geocode result extent is null")
+        // animate viewpoint to geocode result's extent
+        lifecycleScope.launch {
+            mapView.setViewpointGeometry(envelope, 25.0)
+        }
 
     }
 
