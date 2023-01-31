31e55d920071b02262bb4647034c543fe69c00fc
Shubham Sharma <shubhamsharma@esri.com>
2023-01-30 16:05:27 -0800

    sample implementation

--- play-kml-tour/src/main/java/com/esri/arcgismaps/sample/playkmltour/MainActivity.kt
+++ play-kml-tour/src/main/java/com/esri/arcgismaps/sample/playkmltour/MainActivity.kt
@@ -18,7 +18,6 @@ package com.esri.arcgismaps.sample.playkmltour
 
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.databinding.DataBindingUtil
@@ -34,11 +33,14 @@ import com.arcgismaps.mapping.kml.KmlDataset
 import com.arcgismaps.mapping.kml.KmlNode
 import com.arcgismaps.mapping.kml.KmlTour
 import com.arcgismaps.mapping.kml.KmlTourController
+import com.arcgismaps.mapping.kml.KmlTourStatus
 import com.arcgismaps.mapping.layers.KmlLayer
 import com.esri.arcgismaps.sample.playkmltour.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.io.File
+import kotlin.math.roundToInt
 
 
 class MainActivity : AppCompatActivity() {
@@ -66,6 +68,14 @@ class MainActivity : AppCompatActivity() {
         activityMainBinding.resetTourButton
     }
 
+    private val tourStatusTV by lazy {
+        activityMainBinding.tourStatusTV
+    }
+
+    private val tourProgressBar by lazy {
+        activityMainBinding.tourProgressBar
+    }
+
     private val kmlTourController = KmlTourController()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -82,7 +92,7 @@ class MainActivity : AppCompatActivity() {
             elevationSources.add(ArcGISTiledElevationSource(getString(R.string.world_terrain_service)))
         }
 
-        // create a scene
+        // create a scene and set the surface
         sceneView.scene = ArcGISScene(BasemapStyle.ArcGISImagery).apply {
             baseSurface = surface
         }
@@ -90,55 +100,149 @@ class MainActivity : AppCompatActivity() {
         // add a KML layer from a KML dataset with a KML tour
         val kmlDataset = KmlDataset(provisionPath + getString(R.string.kml_tour_path))
         val kmlLayer = KmlLayer(kmlDataset)
+
+        // add the layer to the scene view's operational layers
         sceneView.scene?.operationalLayers?.add(kmlLayer)
 
+        // load the KML layer
         lifecycleScope.launch {
             kmlLayer.load().onFailure {
                 showError(it.message.toString())
             }.onSuccess {
+                // get the first loaded KML tour
                 val kmlTour = findFirstKMLTour(kmlDataset.rootNodes)
-                if(kmlTour == null){
+                if (kmlTour == null) {
                     showError("Cannot find KML tour in dataset")
                     return@onSuccess
                 }
 
+                // collect changes in KML tour status
+                collectKmlTourStatus(kmlTour)
+
+                // set the KML tour to the controller
                 kmlTourController.tour = kmlTour
-                playPauseButton.isEnabled = true
-                resetTourButton.isEnabled = true
             }
         }
 
-        resetTourButton.setOnClickListener {
-            kmlTourController.reset()
-        }
+        resetTourButton.setOnClickListener { kmlTourController.reset() }
 
         playPauseButton.setOnClickListener {
             // button was clicked when tour was playing
-            if (kmlTourController.tour?.isVisible == true) {
-                // pause tour and update button
+            if (kmlTourController.tour?.status?.value == KmlTourStatus.Playing)
+            // pause tour and update button
                 kmlTourController.pause()
-                playPauseButton.icon = AppCompatResources.getDrawable(this, R.drawable.ic_round_play_arrow_24)
-            }
-            else {
-                // play tour and update button
+            else
+            // play tour and update button
                 kmlTourController.play()
-                playPauseButton.icon = AppCompatResources.getDrawable(this, R.drawable.ic_round_pause_24)
-            }
-        }
-
-        resetTourButton.setOnClickListener {
-
         }
     }
 
+    /**
+     * Recursively searches for the first KML tour in a list of [kmlNodes].
+     * Returns the first [KmlTour], or null if there are no tours.
+     */
     private fun findFirstKMLTour(kmlNodes: List<KmlNode>): KmlTour? {
         kmlNodes.forEach { node ->
-            if(node is KmlTour)
+            if (node is KmlTour)
                 return node
             else if (node is KmlContainer)
                 return findFirstKMLTour(node.childNodes)
         }
         return null
+    }
+
+    /**
+     * Collects KmlTourStatus events from the [kmlTour] and then calls
+     * showKmlTourStatus()
+     */
+    private fun collectKmlTourStatus(kmlTour: KmlTour) = lifecycleScope.launch {
+        kmlTour.status.collect { kmlTourStatus ->
+            when (kmlTourStatus) {
+                KmlTourStatus.Completed -> {
+                    showKmlTourStatus(
+                        "Completed",
+                        isResetEnabled = false,
+                        isPlayingTour = false
+                    )
+                }
+                KmlTourStatus.Initialized -> {
+                    showKmlTourStatus(
+                        "Initialized",
+                        isResetEnabled = false,
+                        isPlayingTour = false
+                    )
+                }
+                KmlTourStatus.Paused -> {
+                    showKmlTourStatus(
+                        "Paused",
+                        isResetEnabled = true,
+                        isPlayingTour = false
+                    )
+                }
+                KmlTourStatus.Playing -> {
+                    showKmlTourStatus(
+                        "Playing",
+                        isResetEnabled = true,
+                        isPlayingTour = true
+                    )
+                }
+                else -> {}
+            }
+        }
+    }
+
+    /**
+     * Displays the KML tour status using the [kmlTourStatus], display [resetTourButton]
+     * if [isResetEnabled] and set [playPauseButton] based on [isPlayingTour].
+     */
+    private fun showKmlTourStatus(
+        kmlTourStatus: String,
+        isResetEnabled: Boolean,
+        isPlayingTour: Boolean
+    ) {
+        // set the KML tour status
+        tourStatusTV.text = String.format("Tour status: %s", kmlTourStatus)
+
+        // enable the buttons
+        resetTourButton.isEnabled = isResetEnabled
+        playPauseButton.isEnabled = true
+
+        // show pause button if true
+        if (isPlayingTour) {
+            playPauseButton.apply {
+                // set button icon
+                icon = AppCompatResources.getDrawable(
+                    this@MainActivity,
+                    R.drawable.ic_round_pause_24
+                )
+                // set button text
+                text = String.format("Pause")
+            }
+        } else { // show play button if false
+            playPauseButton.apply {
+                // set button icon
+                icon = AppCompatResources.getDrawable(
+                    this@MainActivity,
+                    R.drawable.ic_round_play_arrow_24
+                )
+                // set button text
+                text = String.format("Play")
+            }
+        }
+
+        // get progress of tour every second
+        lifecycleScope.launch {
+            // run as long as KML tour status is "Playing"
+            while (kmlTourStatus == "Playing") {
+                // get percentage of current position over total duration
+                val tourProgressInt = ((kmlTourController.currentPosition.value * 100.0)
+                        / (kmlTourController.totalDuration.value)).roundToInt()
+                tourProgressBar.progress = tourProgressInt
+                // set a second delay
+                delay(1000)
+            }
+        }
+
     }
 
     private fun showError(message: String) {
