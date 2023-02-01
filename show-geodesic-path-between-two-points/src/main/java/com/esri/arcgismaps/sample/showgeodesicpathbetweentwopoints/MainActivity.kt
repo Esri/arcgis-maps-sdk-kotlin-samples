1da9a74e902ef42617683a9c63b2a1748f9d51d5
Kaushik Meesala <kaushik.pulagara@gmail.com>
2023-02-01 10:33:38 -0800

    added sample functionality

--- show-geodesic-path-between-two-points/src/main/java/com/esri/arcgismaps/sample/showgeodesicpathbetweentwopoints/MainActivity.kt
+++ show-geodesic-path-between-two-points/src/main/java/com/esri/arcgismaps/sample/showgeodesicpathbetweentwopoints/MainActivity.kt
@@ -22,12 +22,23 @@ import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import com.arcgismaps.ApiKey
 import com.arcgismaps.ArcGISEnvironment
+import com.arcgismaps.Color
+import com.arcgismaps.geometry.*
 import com.arcgismaps.mapping.ArcGISMap
 import com.arcgismaps.mapping.BasemapStyle
+import com.arcgismaps.mapping.symbology.SimpleLineSymbol
+import com.arcgismaps.mapping.symbology.SimpleLineSymbolStyle
+import com.arcgismaps.mapping.symbology.SimpleMarkerSymbol
+import com.arcgismaps.mapping.symbology.SimpleMarkerSymbolStyle
+import com.arcgismaps.mapping.view.Graphic
+import com.arcgismaps.mapping.view.GraphicsOverlay
 import com.esri.arcgismaps.sample.showgeodesicpathbetweentwopoints.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,9 +49,61 @@ class MainActivity : AppCompatActivity() {
         DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 
+    // set up data binding for the mapView
     private val mapView by lazy {
         activityMainBinding.mapView
     }
+
+    // shows the distance information as a textview
+    private val infoTextView by lazy {
+        activityMainBinding.infoTextView
+    }
+
+    // starting location for the distance calculation
+    private val startingPoint by lazy {
+        Point(-73.7781, 40.6413, SpatialReference.wgs84())
+    }
+
+    // a blue marker symbol for the location points
+    private val locationMarkerSymbol by lazy {
+        SimpleMarkerSymbol(
+            SimpleMarkerSymbolStyle.Circle,
+            Color.blue,
+            10f
+        )
+    }
+
+    // the actual marker graphic for the starting location
+    private val startingLocationMarkerGraphic by lazy {
+        Graphic().apply {
+            symbol = locationMarkerSymbol
+            geometry = startingPoint
+        }
+    }
+
+    // marker graphic for the destination, location information
+    // populated on tap
+    private val endLocationMarkerGraphic by lazy {
+        Graphic().apply {
+            symbol = locationMarkerSymbol
+        }
+    }
+
+    // the geodesic path represented as line graphic
+    private val geodesicPathGraphic by lazy {
+        val lineSymbol = SimpleLineSymbol(SimpleLineSymbolStyle.Dash, Color.blue)
+        Graphic().apply {
+            symbol = lineSymbol
+        }
+    }
+
+    // the unit of distance measurement in kilometers
+    private val unitsOfMeasurement by lazy {
+        LinearUnit(LinearUnitId.Kilometers)
+    }
+
+    // creates a graphic overlay to draw all graphics
+    private val graphicsOverlay = GraphicsOverlay()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,12 +114,88 @@ class MainActivity : AppCompatActivity() {
         lifecycle.addObserver(mapView)
 
         // create and add a map with a navigation night basemap style
-        val map = ArcGISMap(BasemapStyle.ArcGISNavigationNight)
-        mapView.map = map
+        val map = ArcGISMap(BasemapStyle.ArcGISImageryStandard)
+        // configure mapView assignments
+        mapView.apply {
+            this.map = map
+            // add the graphics overlay to display marker graphics
+            graphicsOverlays.add(graphicsOverlay)
+        }
+
+        // add all our marker graphics to the graphics overlay
+        graphicsOverlay.graphics.add(startingLocationMarkerGraphic)
+        graphicsOverlay.graphics.add(endLocationMarkerGraphic)
+        graphicsOverlay.graphics.add(geodesicPathGraphic)
+
+        lifecycleScope.launch {
+            // check if the map has loaded successfully
+            map.load().onSuccess {
+                // capture and collect when the user taps on the screen
+                mapView.onSingleTapConfirmed.collect { event ->
+                    event.mapPoint?.let { point -> onGeoViewTapped(point) }
+                }
+            }.onFailure {
+                // if map load failed, show the error
+                showError("Error Loading Map", mapView)
+            }
+        }
     }
 
+    /**
+     * Handles the SingleTapEvent by drawing the destination location marker
+     * and a geodesic path curve
+     * using GeometryEngine.densifyGeodetic and computes the distance using
+     * GeometryEngine.lengthGeodetic
+     */
+    private fun onGeoViewTapped(point: Point) {
+        // project the tapped point into the same spatial reference
+        // as the source point
+        val destinationPoint = GeometryEngine.project(point, SpatialReference.wgs84())
+
+        // check if the destination point is within the map bounds
+        // isEmpty returns true if out of bounds
+        if (!destinationPoint.isEmpty) {
+            // update the end location marker location on map
+            endLocationMarkerGraphic.geometry = destinationPoint
+            // create a poly between source and destination points
+            val polyline = Polyline(listOf(startingPoint, destinationPoint))
+            // generate a geodesic curve using the polyline
+            GeometryEngine.densifyGeodetic(
+                polyline,
+                1.0,
+                unitsOfMeasurement,
+                GeodeticCurveType.Geodesic
+                // only compute the distance if the returned curved path geometry
+                // is not null
+            )?.let { pathGeometry ->
+                // update the path graphic
+                geodesicPathGraphic.geometry = pathGeometry
+                // compute the path distance in kms
+                val distance =
+                    GeometryEngine.lengthGeodetic(
+                        pathGeometry,
+                        unitsOfMeasurement,
+                        GeodeticCurveType.Geodesic
+                    )
+                // display the distance result
+                infoTextView.text =
+                    getString(R.string.distance_info_text, distance.roundToInt())
+            }
+        }
+    }
+
+    /**
+     * Displays an error onscreen
+     */
     private fun showError(message: String, view: View) {
         Log.e(TAG, message)
         Snackbar.make(view, message, Snackbar.LENGTH_SHORT).show()
     }
 }
+
+/**
+ * Extension property for the Color type that represents a
+ * blue color
+ */
+private val Color.Companion.blue
+    get() = fromRgba(0, 0, 255)
