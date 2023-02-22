485b27ffe3c2473581bc5f99a790e9afdca6d213
Priyanka Rupani <prupani@esri.com>
2023-02-22 11:24:22 -0800

    add sample implementation

---
+++ apply-dictionary-renderer-to-feature-layer/README.md
@@ -0,0 +1,1 @@
+# Apply Dictionary Renderer To Feature Layer
--- apply-dictionary-renderer-to-feature-layer/src/main/java/com/esri/arcgismaps/sample/applydictionaryrenderertofeaturelayer/MainActivity.kt
+++ apply-dictionary-renderer-to-feature-layer/src/main/java/com/esri/arcgismaps/sample/applydictionaryrenderertofeaturelayer/MainActivity.kt
@@ -18,16 +18,22 @@ package com.esri.arcgismaps.sample.applydictionaryrenderertofeaturelayer
 
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import com.arcgismaps.ApiKey
 import com.arcgismaps.ArcGISEnvironment
 import com.arcgismaps.data.Geodatabase
 import com.arcgismaps.mapping.ArcGISMap
 import com.arcgismaps.mapping.BasemapStyle
+import com.arcgismaps.mapping.Viewpoint
+import com.arcgismaps.mapping.layers.ArcGISVectorTiledLayer
+import com.arcgismaps.mapping.layers.FeatureLayer
+import com.arcgismaps.mapping.symbology.DictionaryRenderer
+import com.arcgismaps.mapping.symbology.DictionarySymbolStyle
 import com.esri.arcgismaps.sample.applydictionaryrenderertofeaturelayer.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.launch
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
@@ -47,8 +53,6 @@ class MainActivity : AppCompatActivity() {
         getExternalFilesDir(null)?.path.toString() + File.separator + getString(R.string.app_name)
     }
 
-    private val geoDatabase by lazy { Geodatabase(File(provisionPath, getString(R.string.militaryoverlay_geodatbase)).path) }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -58,14 +62,53 @@ class MainActivity : AppCompatActivity() {
         lifecycle.addObserver(mapView)
 
         // create and add a map with a navigation night basemap style
-        val map = ArcGISMap(BasemapStyle.ArcGISNavigationNight)
+        val map = ArcGISMap(BasemapStyle.ArcGISTopographic)
         mapView.map = map
 
+        // locate the .geodatabase file in the device
+        val geoDatabaseFile = File(provisionPath, getString(R.string.militaryoverlay_geodatabase))
+        // instantiate the geodatabase with the file path
+        val geoDatabase = Geodatabase(geoDatabaseFile.path)
 
+        val styleFile = File(provisionPath, getString(R.string.mil2525d_stylx))
+        val dictionarySymbolStyle = DictionarySymbolStyle.createFromFile(styleFile.path)
+
+        lifecycleScope.launch {
+            // load the dictionary symbol style
+            dictionarySymbolStyle.load().getOrElse {
+                return@launch showError("Error loading DictionarySymbolStyle: ${it.message}")
+            }
+        }
+
+        lifecycleScope.launch {
+            geoDatabase.load().getOrElse {
+                showError("Error loading GeoDatabase: ${it.message}")
+            }
+
+            geoDatabase.featureTables.forEach { geoDatabaseFeatureTable ->
+                geoDatabaseFeatureTable.load().getOrElse {
+                    return@launch showError("Error loading GeoDatabaseFeatureTable: ${it.message}")
+                }
+                val featureLayer = FeatureLayer(geoDatabaseFeatureTable)
+                featureLayer.load().getOrElse {
+                    return@launch showError("Error loading FeatureLayer: ${it.message}")
+                }
+                mapView.map?.operationalLayers?.add(featureLayer)
+
+                val dictionaryRenderer = DictionaryRenderer(dictionarySymbolStyle)
+                featureLayer.renderer = dictionaryRenderer
+                // get the envelop to set the viewpoint
+                val extent = featureLayer.fullExtent
+                    ?: return@launch showError("Error retrieving extent of the feature layer")
+                mapView.setViewpoint(Viewpoint(extent))
+            }
+        }
     }
 
-    private fun showError(message: String, view: View) {
+    private fun showError(message: String) {
         Log.e(TAG, message)
-        Snackbar.make(view, message, Snackbar.LENGTH_SHORT).show()
+        Snackbar.make(mapView, message, Snackbar.LENGTH_SHORT).show()
     }
+
 }
+
