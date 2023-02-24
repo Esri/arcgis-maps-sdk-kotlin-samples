8b60e73aabbbc544b912d80089a7f0c17dabcb52
Shubham Sharma <shubhamsharma@esri.com>
2023-02-24 15:17:41 -0800

    Updated to use a dropdown to select geometry

--- sketch-on-map/src/main/java/com/esri/arcgismaps/sample/sketchonmap/MainActivity.kt
+++ sketch-on-map/src/main/java/com/esri/arcgismaps/sample/sketchonmap/MainActivity.kt
@@ -19,6 +19,8 @@ package com.esri.arcgismaps.sample.sketchonmap
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
@@ -47,6 +49,7 @@ import com.arcgismaps.mapping.view.geometryeditor.GeometryEditor
 import com.arcgismaps.mapping.view.geometryeditor.VertexTool
 import com.esri.arcgismaps.sample.sketchonmap.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -61,13 +64,15 @@ class MainActivity : AppCompatActivity() {
         activityMainBinding.mapView
     }
 
+    private val selectedGeometryDropdown by lazy {
+        activityMainBinding.pointLinePolygonToolbar.selectGeometryDropdown
+    }
+
     // create a symbol for the point graphic
-    private val pointSymbol: SimpleMarkerSymbol by lazy{
-        SimpleMarkerSymbol(
-            SimpleMarkerSymbolStyle.Square,
+    private val pointSymbol: SimpleMarkerSymbol by lazy {
+        SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Square,
             Color(getColor(R.color.point_symbol_color)),
-            20f
-        )
+            20f)
     }
 
     // create a symbol for a line graphic
@@ -117,111 +122,75 @@ class MainActivity : AppCompatActivity() {
 
         // set MapView's geometry editor to sketch on map
         mapView.geometryEditor = geometryEditor
-    }
 
-    /**
-     * When the point button is clicked, reset other buttons, show the point button as selected,
-     * and start point drawing mode.
-     */
-    fun createModePoint(view: View) {
-        setCurrentSelectionText("Point")
-        resetButtons()
-        activityMainBinding.pointLinePolygonToolbar.pointButton.isSelected = true
-        geometryEditor.start(GeometryType.Point)
-    }
-
-    /**
-     * When the multipoint button is clicked, reset other buttons, show the multipoint button as
-     * selected, and start multipoint drawing mode.
-     */
-    fun createModeMultipoint(view: View) {
-        setCurrentSelectionText("Multipoint")
-        resetButtons()
-        activityMainBinding.pointLinePolygonToolbar.multipointButton.isSelected = true
-        geometryEditor.apply {
-            tool = vertexTool
-            start(GeometryType.Multipoint)
+        // enable/disable the undo button if last event can be undone
+        lifecycleScope.launch {
+            geometryEditor.canUndo.collect { value ->
+                activityMainBinding.pointLinePolygonToolbar.undoButton.isEnabled = value
+            }
         }
-    }
 
-    /**
-     * When the polyline button is clicked, reset other buttons, show the polyline button as
-     * selected, and start polyline drawing mode.
-     */
-    fun createModePolyline(view: View) {
-        setCurrentSelectionText("Polyline")
-        resetButtons()
-        activityMainBinding.pointLinePolygonToolbar.polylineButton.isSelected = true
-        geometryEditor.apply {
-            tool = vertexTool
-            start(GeometryType.Polyline)
+        // enable/disable the redo button if the last event can be redone
+        lifecycleScope.launch {
+            geometryEditor.canRedo.collect { value ->
+                activityMainBinding.pointLinePolygonToolbar.redoButton.isEnabled = value
+            }
         }
-    }
 
-    /**
-     * When the polygon button is clicked, reset other buttons, show the polygon button as
-     * selected, and start polygon drawing mode.
-     */
-    fun createModePolygon(view: View) {
-        setCurrentSelectionText("Polygon")
-        resetButtons()
-        activityMainBinding.pointLinePolygonToolbar.polygonButton.isSelected = true
-        geometryEditor.apply {
-            tool = vertexTool
-            start(GeometryType.Polygon)
-        }
-    }
+        // set up the geometry list dropdown
+        selectedGeometryDropdown.apply {
+            // set the adapter to the list of geometries
+            setAdapter(ArrayAdapter(applicationContext,
+                android.R.layout.simple_list_item_1,
+                resources.getStringArray(R.array.geometry_list)))
 
-    /**
-     * When the freehand line button is clicked, reset other buttons, show the freehand line
-     * button as selected, and start freehand line drawing mode.
-     */
-    fun createModeFreehandLine(view: View) {
-        setCurrentSelectionText("FreehandPolyline")
-        resetButtons()
-        activityMainBinding.pointLinePolygonToolbar.freehandLineButton.isSelected = true
-        geometryEditor.apply {
-            tool = freehandTool
-            start(GeometryType.Polyline)
-        }
-    }
-
-    /**
-     * When the freehand polygon button is clicked, reset other buttons, show the freehand
-     * polygon button as selected, and enable freehand polygon drawing mode.
-     */
-    fun createModeFreehandPolygon(view: View) {
-        setCurrentSelectionText("FreehandPolygon")
-        resetButtons()
-        activityMainBinding.pointLinePolygonToolbar.freehandPolygonButton.isSelected = true
-        geometryEditor.apply {
-            tool = freehandTool
-            start(GeometryType.Polygon)
-        }
-    }
-
-    /**
-     * When the undo button is clicked, undo the last event on the GeometryEditor.
-     */
-    fun undo(view: View) = lifecycleScope.launch{
-        geometryEditor.canUndo.collect{ value ->
-            if(value){
-                geometryEditor.undo()
-                setCurrentSelectionText(getString(R.string.undo))
+            // set the dropdown click listener
+            onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+                // set the GeometryEditorTool and then start the editing process
+                geometryEditor.apply {
+                    when (position) {
+                        0 -> {
+                            tool = freehandTool
+                            start(GeometryType.Polygon)
+                        }
+                        1 -> {
+                            tool = freehandTool
+                            start(GeometryType.Polyline)
+                        }
+                        2 -> {
+                            tool = vertexTool
+                            start(GeometryType.Multipoint)
+                        }
+                        3 -> {
+                            tool = vertexTool
+                            start(GeometryType.Point)
+                        }
+                        4 -> {
+                            tool = vertexTool
+                            start(GeometryType.Polygon)
+                        }
+                        5 -> {
+                            tool = vertexTool
+                            start(GeometryType.Polyline)
+                        }
+                    }
+                }
             }
         }
     }
 
     /**
-     * When the redo button is clicked, redo the last undone event on the GeometryEditor.
+     * Undo the last event on the GeometryEditor.
      */
-    fun redo(view: View) = lifecycleScope.launch{
-        geometryEditor.canRedo.collect{ value ->
-            if(value){
-                geometryEditor.redo()
-                setCurrentSelectionText(getString(R.string.redo))
-            }
-        }
+    fun undo(view: View) {
+        geometryEditor.undo()
+    }
+
+    /**
+     * Redo the last undone event on the GeometryEditor.
+     */
+    fun redo(view: View) {
+        geometryEditor.redo()
     }
 
     /**
@@ -231,7 +200,7 @@ class MainActivity : AppCompatActivity() {
     fun stop(view: View) {
         // get the geometry from sketch editor
         val sketchGeometry = geometryEditor.geometry.value
-            ?: return showError("Error retrieving geometry")
+            ?: return showMessage("Error retrieving geometry")
 
         if (GeometryBuilder.builder(sketchGeometry)?.isSketchValid == false) {
             return reportNotValid()
@@ -239,9 +208,6 @@ class MainActivity : AppCompatActivity() {
 
         // stops the editing session
         geometryEditor.stop()
-
-        // clear button selection
-        resetButtons()
 
         // create a graphic from the sketch editor geometry
         val graphic = Graphic(sketchGeometry).apply {
@@ -256,44 +222,31 @@ class MainActivity : AppCompatActivity() {
 
         // add the graphic to the graphics overlay
         graphicsOverlay.graphics.add(graphic)
-        setCurrentSelectionText("Added graphic to map")
-    }
-
-    /**
-     * De-selects all buttons.
-     */
-    private fun resetButtons() {
-        activityMainBinding.pointLinePolygonToolbar.apply {
-            pointButton.isSelected = false
-            multipointButton.isSelected = false
-            polylineButton.isSelected = false
-            polygonButton.isSelected = false
-            freehandLineButton.isSelected = false
-            freehandPolygonButton.isSelected = false
-        }
     }
 
     /**
      * Clear the MapView of all the graphics and reset selections
      */
     fun clear(view: View) {
-        resetButtons()
         geometryEditor.clearGeometry()
         geometryEditor.clearSelection()
         geometryEditor.stop()
-        activityMainBinding.currentSelection.text = getString(R.string.cleared_message)
+        selectedGeometryDropdown.setText("")
+        selectedGeometryDropdown.clearFocus()
+        showMessage(getString(R.string.cleared_message))
     }
 
     /**
-     * Clear all editing and committed graphics on the map
+     * Clear all editing and applied graphics on the map
      */
     fun restart(view: View) {
-        resetButtons()
         graphicsOverlay.graphics.clear()
         geometryEditor.clearGeometry()
         geometryEditor.clearSelection()
         geometryEditor.stop()
-        activityMainBinding.currentSelection.text = getString(R.string.restart_message)
+        selectedGeometryDropdown.setText("")
+        selectedGeometryDropdown.clearFocus()
+        showMessage(getString(R.string.restart_message))
     }
 
     /**
@@ -301,29 +254,20 @@ class MainActivity : AppCompatActivity() {
      */
     private fun reportNotValid() {
         // get the geometry currently being added to map
-        val geometry = geometryEditor.geometry.value ?: return showError("Geometry not found")
+        val geometry = geometryEditor.geometry.value ?: return showMessage("Geometry not found")
         // find the geometry type, and set the valid message
-        val validIfText: String =
-            when (geometry) {
-                is Point -> getString(R.string.invalid_point_message)
-                is Multipoint -> getString(R.string.invalid_multipoint_message)
-                is Polyline -> getString(R.string.invalid_polyline_message)
-                is Polygon -> getString(R.string.invalid_polygon_message)
-                else -> getString(R.string.none_selected_message)
-            }
+        val validIfText: String = when (geometry) {
+            is Point -> getString(R.string.invalid_point_message)
+            is Multipoint -> getString(R.string.invalid_multipoint_message)
+            is Polyline -> getString(R.string.invalid_polyline_message)
+            is Polygon -> getString(R.stri  ng.invalid_polygon_message)
+            else -> getString(R.string.none_selected_message)
+        }
         // set the invalid message to the TextView.
-        activityMainBinding.currentSelection.text = validIfText
+        showMessage(validIfText)
     }
 
-    /**
-     * Set the current selection text to the [selectedItem]
-     */
-    private fun setCurrentSelectionText(selectedItem: String) {
-        activityMainBinding.currentSelection.text =
-            getString(R.string.current_selection, selectedItem)
-    }
-
-    private fun showError(message: String) {
+    private fun showMessage(message: String) {
         Log.e(TAG, message)
         Snackbar.make(mapView, message, Snackbar.LENGTH_SHORT).show()
     }
