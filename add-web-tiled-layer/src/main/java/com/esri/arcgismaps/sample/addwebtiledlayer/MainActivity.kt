7028b23b72219666c095c7a096be2095ad493e28
Priyanka Rupani <prupani@esri.com>
2023-03-01 11:12:27 -0800

    add implementation

---
+++ add-web-tiled-layer/src/main/java/com/esri/arcgismaps/sample/addwebtiledlayer/MainActivity.kt
@@ -0,0 +1,71 @@
+/* Copyright 2023 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.esri.arcgismaps.sample.addwebtiledlayer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.arcgismaps.ApiKey
+import com.arcgismaps.ArcGISEnvironment
+import com.arcgismaps.geometry.Point
+import com.arcgismaps.geometry.SpatialReference
+import com.arcgismaps.mapping.ArcGISMap
+import com.arcgismaps.mapping.Basemap
+import com.arcgismaps.mapping.Viewpoint
+import com.arcgismaps.mapping.layers.WebTiledLayer
+import com.esri.arcgismaps.sample.addwebtiledlayer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    // set up data binding for the activity
+    private val activityMainBinding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    private val mapView by lazy {
+        activityMainBinding.mapView
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // authentication with an API key or named user is
+        // required to access basemaps and other location services
+        ArcGISEnvironment.apiKey = ApiKey.create(BuildConfig.API_KEY)
+        lifecycle.addObserver(mapView)
+
+
+        // list of sub-domains
+        var subDomains = listOf("a", "b", "c", "d");
+
+        // build the web tiled layer from stamen
+        var webTiledLayer =
+            WebTiledLayer.create(getString(R.string.template_uri_stamen), subDomains).apply {
+                attribution = getString(R.string.stamen_attribution)
+            }
+
+        // use web tiled layer as Basemap
+        val map = ArcGISMap(Basemap(webTiledLayer)).also {
+            // Kotlin Island
+            it.initialViewpoint = Viewpoint(
+                Point(3307706.566137, 8403714.435952, SpatialReference(102100)),
+                94594.86820903532
+            )
+        }
+        mapView.map = map
+    }
+}
