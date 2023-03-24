b667f3bcca4d7b21b2ca2f4cce7dfbb37dadb97f
Shubham Sharma <shubhamsharma@esri.com>
2023-03-24 15:55:07 -0700

    match library versions with API

--- display-composable-mapview/src/main/java/com/esri/arcgismaps/sample/displaycomposablemapview/MainActivity.kt
+++ display-composable-mapview/src/main/java/com/esri/arcgismaps/sample/displaycomposablemapview/MainActivity.kt
@@ -20,8 +20,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -48,7 +48,7 @@ class MainActivity : ComponentActivity() {
             KotlinExampleAppTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colors.background
                 ) {
                     ComposableMapView()
                 }
--- display-composable-mapview/src/main/java/com/esri/arcgismaps/sample/displaycomposablemapview/theme/Theme.kt
+++ display-composable-mapview/src/main/java/com/esri/arcgismaps/sample/displaycomposablemapview/theme/Theme.kt
@@ -1,20 +1,20 @@
 package com.esri.arcgismaps.sample.displaycomposablemapview.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColorScheme(
+private val DarkColorPalette = darkColors(
     primary = colorPrimary,
-    primaryContainer = colorPrimaryDark,
+    primaryVariant = colorPrimaryDark,
     secondary = colorAccent
 )
 
-private val LightColorPalette = lightColorScheme(
+private val LightColorPalette = lightColors(
     primary = colorPrimary,
-    primaryContainer = colorPrimaryDark,
+    primaryVariant = colorPrimaryDark,
     secondary = colorAccent
 )
 
@@ -26,7 +26,7 @@ fun KotlinExampleAppTheme(
     val colors = if (darkTheme) DarkColorPalette else LightColorPalette
 
     MaterialTheme(
-        colorScheme = colors,
+        colors = colors,
         typography = Typography,
         shapes = Shapes,
         content = content
