a79a9dc8b29100e1978420181a486ca6fb1dd1e1
Priyanka Rupani <prupani@esri.com>
2023-05-02 14:30:33 -0700

    show bottom sheet only when directions list is not empty

--- find-route-around-barriers/src/main/java/com/esri/arcgismaps/sample/findroutearoundbarriers/MainActivity.kt
+++ find-route-around-barriers/src/main/java/com/esri/arcgismaps/sample/findroutearoundbarriers/MainActivity.kt
@@ -219,6 +219,7 @@ class MainActivity : AppCompatActivity() {
 
         // display the bottom sheet with directions when the button is clicked
         directionsButton.setOnClickListener {
+            if (directionsList.isEmpty()) return@setOnClickListener
             setupBottomSheet(directionsList)
         }
 
