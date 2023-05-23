00073012a640a26cd50859a176a8ad566c3441f9
Priyanka Rupani <prupani@esri.com>
2023-05-23 14:27:54 -0700

    add component in samples lib

---
+++ samples-lib/src/main/java/com/esri/arcgismaps/sample/sampleslib/components/LoadingDialog.kt
@@ -0,0 +1,97 @@
+package com.esri.arcgismaps.sample.sampleslib.components
+
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+
+@Composable
+fun LoadingDialog(
+    title: String,
+    showDialog: Boolean
+) {
+    if (showDialog) {
+        Dialog(
+            onDismissRequest = { }
+        ) {
+            Surface(
+                elevation = 4.dp,
+                shape = RoundedCornerShape(16.dp)
+            ) {
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    ProgressIndicatorLoading()
+
+                    // Custom Text
+                    Text(
+                        modifier = Modifier
+                            .padding(30.dp),
+                        text = title,
+                        style = TextStyle(
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.SemiBold
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ProgressIndicatorLoading() {
+
+    val infiniteTransition = rememberInfiniteTransition()
+
+    val angle by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = keyframes {
+                durationMillis = 600
+            }
+        )
+    )
+    CircularProgressIndicator(
+        progress = 1f,
+        modifier = Modifier
+            .padding(top = 30.dp)
+            .rotate(angle)
+            .border(
+                8.dp,
+                brush = Brush.sweepGradient(
+                    listOf(
+                        Color.White,
+                        MaterialTheme.colors.primary
+                    )
+                ),
+                shape = CircleShape
+            ),
+        strokeWidth = 1.dp
+    )
+}
+
