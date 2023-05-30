15459550c4af412e13b4eb85ec6e29ff41ace5c5
Priyanka Rupani <44178002+prupani-7@users.noreply.github.com>
2023-05-30 13:51:46 -0700

    Update samples-lib/src/main/java/com/esri/arcgismaps/sample/sampleslib/components/LoadingDialog.kt
    Co-authored-by: Shubham Sharma <shubhamsharma@esri.com>

--- samples-lib/src/main/java/com/esri/arcgismaps/sample/sampleslib/components/LoadingDialog.kt
+++ samples-lib/src/main/java/com/esri/arcgismaps/sample/sampleslib/components/LoadingDialog.kt
@@ -10,10 +10,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
