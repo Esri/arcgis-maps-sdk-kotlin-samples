f11ec086d812c6a4e7183532b6bb28bd30d961a3
Priyanka Rupani <prupani@esri.com>
2023-05-30 14:32:14 -0700

    update loading dialog

--- samples-lib/src/main/java/com/esri/arcgismaps/sample/sampleslib/components/LoadingDialog.kt
+++ samples-lib/src/main/java/com/esri/arcgismaps/sample/sampleslib/components/LoadingDialog.kt
@@ -1,31 +1,21 @@
 package com.esri.arcgismaps.sample.sampleslib.components
 
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.keyframes
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.foundation.border
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import com.esri.arcgismaps.sample.sampleslib.theme.SampleAppTheme
 
 @Composable
 fun LoadingDialog(
@@ -37,14 +27,16 @@ fun LoadingDialog(
             onDismissRequest = { }
         ) {
             Surface(
-                elevation = 4.dp,
                 shape = RoundedCornerShape(16.dp)
             ) {
                 Column(
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    ProgressIndicatorLoading()
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .padding(top = 30.dp)
+                    )
 
                     // Custom Text
                     Text( 
@@ -59,43 +51,11 @@ fun LoadingDialog(
     }
 }
 
-@Composable
-fun ProgressIndicatorLoading() {
-
-    val infiniteTransition = rememberInfiniteTransition()
-
-    val angle by infiniteTransition.animateFloat(
-        initialValue = 0f,
-        targetValue = 360f,
-        animationSpec = infiniteRepeatable(
-            animation = keyframes {
-                durationMillis = 600
-            }
-        )
-    )
-    CircularProgressIndicator(
-        progress = 1f,
-        modifier = Modifier
-            .padding(top = 30.dp)
-            .rotate(angle)
-            .border(
-                8.dp,
-                brush = Brush.sweepGradient(
-                    listOf(
-                        Color.White,
-                        MaterialTheme.colors.primary
-                    )
-                ),
-                shape = CircleShape
-            ),
-        strokeWidth = 1.dp
-    )
-}
 @Preview
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun PreviewLoadingDialog(){
-    SampleAppTheme() {
+    SampleAppTheme {
         LoadingDialog(title = "Dialog loading message here", showDialog = true)
     }
 }
