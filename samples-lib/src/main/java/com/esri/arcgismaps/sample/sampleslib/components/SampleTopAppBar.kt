808f45cff92ce4a8d2e4f590389cb6559783f41e
Shubham Sharma <shubhamsharma@esri.com>
2023-06-01 08:57:07 -0700

    Samples Compose Material3  (#107)

--- samples-lib/src/main/java/com/esri/arcgismaps/sample/sampleslib/components/SampleTopAppBar.kt
+++ samples-lib/src/main/java/com/esri/arcgismaps/sample/sampleslib/components/SampleTopAppBar.kt
@@ -17,12 +17,14 @@
 package com.esri.arcgismaps.sample.sampleslib.components
 
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SampleTopAppBar(
     title: String,
