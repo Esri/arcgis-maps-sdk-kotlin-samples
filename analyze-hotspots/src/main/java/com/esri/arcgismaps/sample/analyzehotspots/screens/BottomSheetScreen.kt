9d7b665ab9143fe2ff41cf064eca71a59908968e
Shubham Sharma <shubhamsharma@esri.com>
2023-06-28 09:17:00 -0700

    Analyze hotspots (#114)
    Co-authored-by: Priyanka Rupani <prupani@esri.com>
    Co-authored-by: Priyanka Rupani <44178002+prupani-7@users.noreply.github.com>

---
+++ analyze-hotspots/src/main/java/com/esri/arcgismaps/sample/analyzehotspots/screens/BottomSheetScreen.kt
@@ -0,0 +1,117 @@
+/* Copyright 2023 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.esri.arcgismaps.sample.analyzehotspots.screens
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DisplayMode
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.esri.arcgismaps.sample.sampleslib.theme.SampleAppTheme
+import kotlinx.coroutines.launch
+
+/**
+ * Bottom sheet layout to select a date range
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DateRangeSelectorLayout(
+    onBottomSheetDismiss: () -> Unit,
+    onRunAnalysisClicked: (Long?, Long?) -> Unit,
+    bottomSheetState: SheetState,
+) {
+    // coroutineScope that will be cancelled when this call leaves the composition
+    val scope = rememberCoroutineScope()
+
+    // From: Dec 31st, 1997
+    val fromDatePickerState = rememberDatePickerState(
+        initialSelectedDateMillis = 883526400000,
+        initialDisplayMode = DisplayMode.Picker
+    )
+    // From: Jan 14th, 1998
+    val toDatePickerState = rememberDatePickerState(
+        initialSelectedDateMillis = 884736000000,
+        initialDisplayMode = DisplayMode.Picker
+    )
+    Column {
+        Button(
+            modifier = Modifier.wrapContentWidth().padding(12.dp).align(CenterHorizontally),
+            onClick = {
+                // get the selected date range in millis
+                onRunAnalysisClicked(
+                    fromDatePickerState.selectedDateMillis,
+                    toDatePickerState.selectedDateMillis
+                )
+                // hide the bottom sheet
+                scope.launch {
+                    bottomSheetState.hide()
+                }.invokeOnCompletion {
+                    if (!bottomSheetState.isVisible) {
+                        onBottomSheetDismiss()
+                    }
+                }
+            }
+        ) {
+            Text("Run analysis")
+        }
+        LazyColumn {
+            // from date picker
+            item {
+                DatePicker(
+                    state = fromDatePickerState, modifier = Modifier.padding(16.dp),
+                    title = { Text(text = "FROM") }
+                )
+            }
+            // to date picker
+            item {
+                DatePicker(
+                    state = toDatePickerState,
+                    modifier = Modifier.padding(16.dp),
+                    title = { Text(text = "TO") }
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun PreviewBottomSheetScreen() {
+    SampleAppTheme {
+        DateRangeSelectorLayout(
+            onBottomSheetDismiss = {},
+            onRunAnalysisClicked = { _, _ -> },
+            bottomSheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+        )
+    }
+}
--- samples-lib/src/main/java/com/esri/arcgismaps/sample/sampleslib/components/MessageDialog.kt
+++ samples-lib/src/main/java/com/esri/arcgismaps/sample/sampleslib/components/MessageDialog.kt
@@ -1,3 +1,19 @@
+/* Copyright 2023 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.esri.arcgismaps.sample.sampleslib.components
 
 import android.content.res.Configuration
@@ -9,7 +25,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModel
 import com.esri.arcgismaps.sample.sampleslib.theme.SampleAppTheme
@@ -67,28 +85,28 @@ fun PreviewMessageDialog() {
 
 class MessageDialogViewModel : ViewModel() {
     // display dialog when status is true
-    var dialogStatus = mutableStateOf(false)
+    var dialogStatus by mutableStateOf(false)
         private set
 
-    var messageTitle = ""
+    var messageTitle by mutableStateOf("")
         private set
 
-    var messageDescription = ""
+    var messageDescription by mutableStateOf("")
         private set
 
     /**
      * Displays an message dialog
      */
-    fun showErrorDialog(title: String, description: String = "") {
+    fun showMessageDialog(title: String, description: String = "") {
         messageTitle = title
         messageDescription = description
-        dialogStatus.value = true
+        dialogStatus = true
     }
 
     /**
      * Dismiss the message dialog
      */
     fun dismissDialog() {
-        dialogStatus.value = false
+        dialogStatus = false
     }
 }
