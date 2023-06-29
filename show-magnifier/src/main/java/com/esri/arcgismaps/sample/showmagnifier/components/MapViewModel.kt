549092b54829c635324d83d22409b7fdb6e5f155
Priyanka Rupani <44178002+prupani-7@users.noreply.github.com>
2023-06-28 22:15:04 -0700

    Show magnifier sample (#117)
    Show magnifier

---
+++ show-magnifier/src/main/java/com/esri/arcgismaps/sample/showmagnifier/components/MapViewModel.kt
@@ -0,0 +1,46 @@
+/* Copyright 2023 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.esri.arcgismaps.sample.showmagnifier.components
+
+import android.app.Application
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.AndroidViewModel
+import com.arcgismaps.mapping.ArcGISMap
+import com.arcgismaps.mapping.BasemapStyle
+import com.arcgismaps.mapping.Viewpoint
+import com.arcgismaps.mapping.view.MapViewInteractionOptions
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class MapViewModel(application: Application) : AndroidViewModel(application) {
+    // get an instance of the MapView state
+    val mapViewState = MapViewState()
+}
+
+/**
+ * Class that represents the MapView's current state
+ */
+class MapViewState {
+    val arcGISMap: ArcGISMap = ArcGISMap(BasemapStyle.ArcGISTopographic)
+    val viewpoint: Viewpoint = Viewpoint(34.056295, -117.195800, 1000000.0)
+    // setting `isMagnifierEnabled` property to true. `allowMagnifierToPan` by default is true
+    val interactionOptions: MapViewInteractionOptions = MapViewInteractionOptions(
+        isMagnifierEnabled = true
+    )
+}
+
