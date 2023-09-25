1e67d7c86948955139fe9c41ab166c3e176c2ca8
Priyanka Rupani <prupani@esri.com>
2023-09-25 13:16:42 -0700

    Intial working app

---
+++ show-callout/src/main/java/com/esri/arcgismaps/sample/showcallout/components/MapViewModel.kt
@@ -0,0 +1,42 @@
+/* Copyright 2023 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.esri.arcgismaps.sample.showcallout.components
+
+import android.app.Application
+import android.widget.TextView
+import androidx.lifecycle.AndroidViewModel
+import com.arcgismaps.mapping.ArcGISMap
+import com.arcgismaps.mapping.BasemapStyle
+import com.arcgismaps.mapping.Viewpoint
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class MapViewModel(application: Application) : AndroidViewModel(application) {
+    // set the MapView mutable stateflow
+    val mapViewState = MutableStateFlow(MapViewState())
+
+    val calloutView = TextView(application)
+
+}
+
+
+/**
+ * Data class that represents the MapView state
+ */
+data class MapViewState(
+    var arcGISMap: ArcGISMap = ArcGISMap(BasemapStyle.ArcGISNavigationNight),
+    var viewpoint: Viewpoint = Viewpoint(39.8, -98.6, 10e7)
+)
