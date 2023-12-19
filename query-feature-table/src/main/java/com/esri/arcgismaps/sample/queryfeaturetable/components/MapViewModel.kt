21c07a47dbe1fa50cadc7055c95cb8fe622d78af
Priyanka Rupani <prupani@esri.com>
2023-12-19 15:28:02 -0800

    update sample

--- query-feature-table/src/main/java/com/esri/arcgismaps/sample/queryfeaturetable/components/MapViewModel.kt
+++ query-feature-table/src/main/java/com/esri/arcgismaps/sample/queryfeaturetable/components/MapViewModel.kt
@@ -21,7 +21,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.AndroidViewModel
-import com.arcgismaps.Color
 import com.arcgismaps.data.FeatureQueryResult
 import com.arcgismaps.data.QueryParameters
 import com.arcgismaps.data.ServiceFeatureTable
@@ -32,11 +31,6 @@ import com.arcgismaps.mapping.ArcGISMap
 import com.arcgismaps.mapping.BasemapStyle
 import com.arcgismaps.mapping.Viewpoint
 import com.arcgismaps.mapping.layers.FeatureLayer
-import com.arcgismaps.mapping.symbology.SimpleFillSymbol
-import com.arcgismaps.mapping.symbology.SimpleFillSymbolStyle
-import com.arcgismaps.mapping.symbology.SimpleLineSymbol
-import com.arcgismaps.mapping.symbology.SimpleLineSymbolStyle
-import com.arcgismaps.mapping.symbology.SimpleRenderer
 import com.esri.arcgismaps.sample.queryfeaturetable.R
 import com.esri.arcgismaps.sample.sampleslib.components.MessageDialogViewModel
 import kotlinx.coroutines.CoroutineScope
@@ -48,50 +42,24 @@ class MapViewModel(
     private val sampleCoroutineScope: CoroutineScope
 ) : AndroidViewModel(application) {
 
-    // get an instance of the MapView state
-    val mapViewState = MapViewState()
-
     // create a ViewModel to handle dialog interactions
     val messageDialogVM: MessageDialogViewModel = MessageDialogViewModel()
 
     // create a service feature table and a feature layer from it
-    private val serviceFeatureTable: ServiceFeatureTable = ServiceFeatureTable(
+    val serviceFeatureTable = ServiceFeatureTable(
         application.getString(R.string.us_daytime_population_url)
     )
 
-    // create the feature layer using the service feature table
-    private val featureLayer: FeatureLayer by lazy {
-        FeatureLayer.createWithFeatureTable(serviceFeatureTable)
-    }
 
-    init {
-        // use symbols to show U.S. states with a black outline and yellow fill
-        val lineSymbol = SimpleLineSymbol(
-            style = SimpleLineSymbolStyle.Solid,
-            color = Color.black,
-            width = 1.0f
-        )
-        val fillSymbol = SimpleFillSymbol(
-            style = SimpleFillSymbolStyle.Solid,
-            color = Color.fromRgba(255, 255, 0, 255),
-            outline = lineSymbol
-        )
+    // geometry of the queried state
+    var stateGeometry: Geometry? by mutableStateOf(null)
 
-        featureLayer.apply {
-            // set renderer for the feature layer
-            renderer = SimpleRenderer(fillSymbol)
-            opacity = 0.8f
-            maxScale = 10000.0
-        }
-        // add the feature layer to the map's operational layers
-        mapViewState.arcGISMap.operationalLayers.add(featureLayer)
-    }
 
     /**
      * Search for a U.S. state using [searchQuery] in the feature table, and if found add it
      * to the featureLayer, zoom to it, and select it.
      */
-    fun searchForState(searchQuery: String) {
+    fun searchForState(searchQuery: String, featureLayer: FeatureLayer) {
         // clear any previous selections
         featureLayer.clearSelection()
         // create a query for the state that was entered
@@ -114,7 +82,7 @@ class MapViewModel(
                 val envelope = feature.geometry?.extent
                     ?: return@launch messageDialogVM.showMessageDialog("Error retrieving geometry extent")
                 // update the map view to set the viewpoint to the state geometry
-                mapViewState.stateGeometry = envelope
+                stateGeometry = envelope
             } else {
                 messageDialogVM.showMessageDialog("No states found with name: $searchQuery")
             }
@@ -125,16 +93,5 @@ class MapViewModel(
 /**
  * Class that represents the MapView state
  */
-class MapViewState {
-    // map used to display the feature layer
-    var arcGISMap: ArcGISMap by mutableStateOf(ArcGISMap(BasemapStyle.ArcGISTopographic))
 
-    // geometry of the queried state
-    var stateGeometry: Geometry? by mutableStateOf(null)
 
-    // set an initial viewpoint over the USA
-    val initialViewpoint: Viewpoint = Viewpoint(
-        center = Point(-11e6, 5e6, SpatialReference.webMercator()),
-        scale = 1e8
-    )
-}
--- query-feature-table/src/main/java/com/esri/arcgismaps/sample/queryfeaturetable/screens/MainScreen.kt
+++ query-feature-table/src/main/java/com/esri/arcgismaps/sample/queryfeaturetable/screens/MainScreen.kt
@@ -17,16 +17,38 @@
 package com.esri.arcgismaps.sample.queryfeaturetable.screens
 
 import android.app.Application
+import android.view.View
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import com.esri.arcgismaps.sample.queryfeaturetable.components.ComposeMapView
+import com.arcgismaps.Color
+import com.arcgismaps.data.ServiceFeatureTable
+import com.arcgismaps.geometry.Envelope
+import com.arcgismaps.geometry.Point
+import com.arcgismaps.geometry.SpatialReference
+import com.arcgismaps.mapping.ArcGISMap
+import com.arcgismaps.mapping.BasemapStyle
+import com.arcgismaps.mapping.Viewpoint
+import com.arcgismaps.mapping.layers.FeatureLayer
+import com.arcgismaps.mapping.symbology.SimpleFillSymbol
+import com.arcgismaps.mapping.symbology.SimpleFillSymbolStyle
+import com.arcgismaps.mapping.symbology.SimpleLineSymbol
+import com.arcgismaps.mapping.symbology.SimpleLineSymbolStyle
+import com.arcgismaps.mapping.symbology.SimpleRenderer
+import com.arcgismaps.toolkit.geocompose.MapView
+import com.arcgismaps.toolkit.geocompose.MapViewpointOperation
+import com.arcgismaps.toolkit.geocompose.ViewpointChangedState
+import com.arcgismaps.toolkit.geocompose.rememberViewpointChangedStateForBoundingGeometry
+import com.esri.arcgismaps.sample.queryfeaturetable.R
 import com.esri.arcgismaps.sample.queryfeaturetable.components.MapViewModel
 import com.esri.arcgismaps.sample.sampleslib.components.MessageDialog
 import com.esri.arcgismaps.sample.sampleslib.components.SampleTopAppBar
@@ -40,19 +62,65 @@ fun MainScreen(sampleName: String, application: Application) {
     val sampleCoroutineScope = rememberCoroutineScope()
     // create a ViewModel to handle MapView interactions
     val mapViewModel = remember { MapViewModel(application, sampleCoroutineScope) }
+    // map used to display the feature layer
+    val map = ArcGISMap(BasemapStyle.ArcGISTopographic)
+    val usaViewpoint = Viewpoint(
+        center = Point(-11e6, 5e6, SpatialReference.webMercator()),
+        scale = 1e8
+    )
+
+
+    // use symbols to show U.S. states with a black outline and yellow fill
+    val lineSymbol = SimpleLineSymbol(
+        style = SimpleLineSymbolStyle.Solid,
+        color = Color.black,
+        width = 1.0f
+    )
+    val fillSymbol = SimpleFillSymbol(
+        style = SimpleFillSymbolStyle.Solid,
+        color = Color.fromRgba(255, 255, 0, 255),
+        outline = lineSymbol
+    )
+
+    // create the feature layer using the service feature table
+    val featureLayer by lazy {
+        FeatureLayer.createWithFeatureTable(mapViewModel.serviceFeatureTable)
+    }
+
+    featureLayer.apply {
+        // set renderer for the feature layer
+        renderer = SimpleRenderer(fillSymbol)
+        opacity = 0.8f
+        maxScale = 10000.0
+    }
+
+    // add the feature layer to the map's operational layers
+    map.operationalLayers.add(featureLayer)
+//    var mapViewpointOperation: MapViewpointOperation? by remember { mutableStateOf(null) }
+//    mapViewpointOperation = MapViewpointOperation.Set(usaViewpoint)
 
     Scaffold(
         topBar = { SampleTopAppBar(title = sampleName) },
         content = {
-            Column(modifier = Modifier.fillMaxSize().padding(it)) {
+            Column(modifier = Modifier
+                .fillMaxSize()
+                .padding(it)) {
                 // composable function that wraps the MapView
-                ComposeMapView(
-                    modifier = Modifier.fillMaxWidth().weight(1f),
-                    mapViewModel = mapViewModel
+                MapView(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f),
+                    arcGISMap = map,
+                    viewpointOperation = MapViewpointOperation.Set(viewpoint = usaViewpoint),
+                    viewpointChangedState = rememberViewpointChangedStateForBoundingGeometry(
+                        onViewpointChanged = { mapViewModel.stateGeometry }
+                    )
+
                 )
                 SearchBar(
                     modifier = Modifier.fillMaxWidth(),
-                    onQuerySubmit = mapViewModel::searchForState
+                    onQuerySubmit = { searchQuery ->
+                        mapViewModel.searchForState(searchQuery, featureLayer) }
                 )
             }
 
