f3160bc672e57d0e40d85b4682fb49b16b4a2796
Ruiqi Ma <rma@esri.com>
2024-01-03 17:03:01 -0800

    finish migrating samples starting with b to e

---
+++ display-composable-mapview/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "com.esri.arcgismaps.sample.displaycomposablemapview"
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = libs.versions.versionCode.get().toInt()
+        versionName = libs.versions.versionName.get()
+        buildConfigField("String", "API_KEY", project.properties["API_KEY"].toString())
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.kotlinCompilerExt.get()
+    }
+
+    namespace = "com.esri.arcgismaps.sample.displaycomposablemapview"
+}
+
+dependencies {
+    // lib dependencies from rootProject build.gradle.kts
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.activity.compose)
+    // Jetpack Compose Bill of Materials
+    implementation(platform(libs.androidx.compose.bom))
+    // Jetpack Compose dependencies
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(project(":samples-lib"))
+}
