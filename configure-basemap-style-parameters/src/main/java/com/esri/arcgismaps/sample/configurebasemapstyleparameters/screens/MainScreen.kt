121195f324a519a1416a460cef539c629c812a3f
Trevor Draeseke <trev8939@esri.com>
2024-05-22 17:01:48 +0100

    working sample

--- configure-basemap-style-parameters/src/main/java/com/esri/arcgismaps/sample/configurebasemapstyleparameters/screens/MainScreen.kt
+++ configure-basemap-style-parameters/src/main/java/com/esri/arcgismaps/sample/configurebasemapstyleparameters/screens/MainScreen.kt
@@ -16,31 +16,34 @@
 
 package com.esri.arcgismaps.sample.configurebasemapstyleparameters.screens
 
-import android.app.Application
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -49,13 +52,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.arcgismaps.toolkit.geoviewcompose.MapView
 import com.esri.arcgismaps.sample.configurebasemapstyleparameters.components.MapViewModel
 import com.esri.arcgismaps.sample.sampleslib.components.SampleTopAppBar
 import kotlinx.coroutines.launch
-import kotlin.reflect.KFunction1
 
 /**
  * Main screen layout for the sample app
@@ -63,88 +65,111 @@ import kotlin.reflect.KFunction1
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(sampleName: String) {
-    // get the application context
-    val application = LocalContext.current.applicationContext as Application
-    // create a ViewModel to handle SceneView interactions
-    val mapViewModel = MapViewModel(application)
+    // create a ViewModel to handle MapView interactions
+    val mapViewModel: MapViewModel = viewModel()
+    val createNewBasemapStyleParameters = mapViewModel::createNewBasemapStyleParameters
     val languageStrategyOptions = mapViewModel.languageStrategyOptions
-    val chosenStrategy: MutableState<String> = mapViewModel.chosenStrategy
+    val languageStrategy = mapViewModel.languageStrategy
+    val onLanguageStrategyChange = mapViewModel.onLanguageStrategyChange
     val specificLanguageOptions = mapViewModel.specificLanguageOptions
-    val chosenLanguage = mapViewModel.chosenLanguage
+    val specificLanguage = mapViewModel.specificLanguage
+    val onSpecificLanguageChange = mapViewModel.onSpecificLanguageChange
 
-    val setBasemap = mapViewModel::setNewBasemap
+    val bottomSheetScope = rememberCoroutineScope()
+    val bottomSheetState = rememberBottomSheetScaffoldState().apply {
+        bottomSheetScope.launch {
+            bottomSheetState.show()
+        }
+    }
 
-    val bottomSheetState = rememberModalBottomSheetState()
-    val scope = rememberCoroutineScope()
-    var showBottomSheet by remember { mutableStateOf(false) }
     Scaffold(
         topBar = { SampleTopAppBar(title = sampleName) },
         content = {
             Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(it)
+                    .padding(it),
+                contentAlignment = Alignment.Center
             ) {
                 MapView(
                     modifier = Modifier
                         .fillMaxSize(),
                     arcGISMap = mapViewModel.map
                 )
-                Button(
-                    modifier = Modifier
-                        .align(Alignment.BottomCenter)
-                        .padding(bottom = 24.dp),
-                    onClick = {
-                        scope.launch {
-                            showBottomSheet = true
-                        }
-                    },
-                ) {
-                    Text(
-                        text = "Show controls"
-                    )
+                if (!bottomSheetState.bottomSheetState.isVisible) {
+                    Button(
+                        modifier = Modifier
+                            .align(Alignment.BottomCenter)
+                            .padding(bottom = 24.dp),
+                        onClick = {
+                            bottomSheetScope.launch {
+                                bottomSheetState.bottomSheetState.expand()
+                            }
+                        },
+                    ) {
+                        Text(
+                            text = "Show controls"
+                        )
+                    }
                 }
-            }
-            if (showBottomSheet) {
-                ModalBottomSheet(
-                    onDismissRequest = {
-                        showBottomSheet = false
-                    },
-                    sheetState = bottomSheetState
+                Box(
+                    modifier = Modifier
+                        .widthIn(0.dp, 380.dp)
                 ) {
-                    Controls(setBasemap, languageStrategyOptions, chosenStrategy, specificLanguageOptions, chosenLanguage)
+                    BottomSheetScaffold(
+                        scaffoldState = bottomSheetState,
+                        sheetContent = {
+                            BoxWithConstraints(
+                                Modifier
+                                    .heightIn(max = 160.dp)
+                                    .padding(8.dp)
+                                    .align(Alignment.CenterHorizontally)
+                            ) {
+                                Row {
+                                    Column {
+                                        SetLanguageStrategy(
+                                            languageStrategyOptions = languageStrategyOptions,
+                                            onLanguageStrategyChange = { languageStrategy ->
+                                                createNewBasemapStyleParameters(
+                                                    languageStrategy,
+                                                    specificLanguage.value
+                                                )
+                                                onLanguageStrategyChange(languageStrategy)
+                                            },
+                                            languageStrategy = languageStrategy.value,
+                                            enabled = (specificLanguage.value == "None")
+                                        )
+                                    }
+                                    Divider(
+                                        color = Color.LightGray,
+                                        modifier = Modifier
+                                            .padding(8.dp)
+                                            .fillMaxHeight()
+                                            .width(2.dp)
+
+                                    )
+                                    Column {
+                                        SetSpecificLanguage(
+                                            specificLanguageOptions = specificLanguageOptions,
+                                            onSpecificLanguageChange = { specificLanguage ->
+                                                createNewBasemapStyleParameters(
+                                                    languageStrategy.value,
+                                                    specificLanguage
+                                                )
+                                                onSpecificLanguageChange(specificLanguage)
+                                            },
+                                            specificLanguage = specificLanguage.value
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    ) {
+                    }
                 }
             }
         }
     )
-}
-
-/**
- * Displays messages to the user. This may be used to display instructions, portal info, or error messages.
- *
- */
-@Composable
-private fun Controls(
-    setBasemap: KFunction1<String, Unit>,
-    languageStrategyOptions: List<String>,
-    chosenStrategy: MutableState<String>,
-    specificLanguageOptions: List<String>,
-    chosenLanguage: MutableState<String>
-) {
-
-    Box(
-        Modifier
-            .padding(8.dp)
-            .height(200.dp)
-            .wrapContentHeight()
-    ) {
-
-
-        Column {
-            SetLanguageStrategy(setBasemap, languageStrategyOptions, chosenStrategy, chosenLanguage.value == "None")
-            SetSpecificLanguage(setBasemap, specificLanguageOptions, chosenLanguage)
-        }
-    }
 }
 
 /**
@@ -152,42 +177,44 @@ private fun Controls(
  */
 @Composable
 private fun SetLanguageStrategy(
-    setBasemap: KFunction1<String, Unit>,
-    languageStrategies: List<String>,
-    chosenStrategy: MutableState<String>,
+    languageStrategyOptions: List<String>,
+    onLanguageStrategyChange: (String) -> Unit,
+    languageStrategy: String,
     enabled: Boolean
 ) {
     Text(
         text = "Set language strategy:"
     )
 
-    languageStrategies.forEach { text ->
+    languageStrategyOptions.forEach { text ->
         Row(
             Modifier
-                .fillMaxWidth()
+                .wrapContentWidth()
+                .wrapContentHeight()
                 .selectable(
-                    selected = (text == chosenStrategy.value),
+                    selected = (text == languageStrategy),
                     onClick = {
                         if (enabled) {
-                            chosenStrategy.value = text
-                            setBasemap(text)
+                            onLanguageStrategyChange(text)
                         }
                     }
                 )
         ) {
             RadioButton(
-                selected = (text == chosenStrategy.value),
+                selected = (text == languageStrategy),
                 onClick = {
                     if (enabled) {
-                        chosenStrategy.value = text
-                        setBasemap(text)
+                        onLanguageStrategyChange(text)
                     }
                 },
                 enabled = enabled
             )
             Text(
                 text = text,
-                modifier = Modifier.align(Alignment.CenterVertically)
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .wrapContentHeight()
+                    .wrapContentWidth()
             )
         }
     }
@@ -199,9 +226,9 @@ private fun SetLanguageStrategy(
  */
 @Composable
 private fun SetSpecificLanguage(
-    setBasemap: KFunction1<String, Unit>,
     specificLanguageOptions: List<String>,
-    chosenLanguage: MutableState<String>
+    onSpecificLanguageChange: (String) -> Unit,
+    specificLanguage: String
 ) {
     Text(
         text = "Set specific language:"
@@ -209,32 +236,31 @@ private fun SetSpecificLanguage(
     var expanded by remember { mutableStateOf(false) }
     Box(
         modifier = Modifier
-            .fillMaxSize()
+            .wrapContentHeight()
+            .wrapContentWidth()
             .wrapContentSize(Alignment.TopStart)
-            .padding(8.dp)
+            .padding(top = 16.dp)
     ) {
         Text(
-            chosenLanguage.value,
+            specificLanguage,
             modifier = Modifier
+                .fillMaxWidth()
                 .clickable(onClick = { expanded = true })
                 .border(
                     2.dp,
                     Color.LightGray
                 )
-                .padding(horizontal = 24.dp, vertical = 12.dp)
+                .padding(horizontal = 16.dp, vertical = 12.dp)
         )
         DropdownMenu(
             expanded = expanded,
-            onDismissRequest = { expanded = false },
-            modifier = Modifier
-                .wrapContentSize()
+            onDismissRequest = { expanded = false }
         ) {
             specificLanguageOptions.forEachIndexed { index, specificLanguage ->
                 DropdownMenuItem(
                     text = { Text(specificLanguage) },
                     onClick = {
-                        setBasemap(specificLanguage)
-                        chosenLanguage.value = specificLanguage
+                        onSpecificLanguageChange(specificLanguage)
                         expanded = false
                     })
                 // show a divider between dropdown menu options
