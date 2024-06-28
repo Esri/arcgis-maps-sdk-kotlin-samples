4e10a613a97a1875b97b8d3aed1c722d92da16de
Trevor Draeseke <trev8939@esri.com>
2024-06-28 15:34:26 +0100

    Oauth with toolkit component working

--- authenticate-with-oauth/src/main/java/com/esri/arcgismaps/sample/authenticatewithoauth/MainActivity.kt
+++ authenticate-with-oauth/src/main/java/com/esri/arcgismaps/sample/authenticatewithoauth/MainActivity.kt
@@ -1,4 +1,4 @@
-/* Copyright 2023 Esri
+/* Copyright 2024 Esri
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,103 +17,39 @@
 package com.esri.arcgismaps.sample.authenticatewithoauth
 
 import android.os.Bundle
-import android.view.View
-import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import com.arcgismaps.ApiKey
 import com.arcgismaps.ArcGISEnvironment
-import com.arcgismaps.httpcore.authentication.ArcGISAuthenticationChallengeHandler
-import com.arcgismaps.httpcore.authentication.ArcGISAuthenticationChallengeResponse
-import com.arcgismaps.httpcore.authentication.OAuthUserConfiguration
-import com.arcgismaps.httpcore.authentication.OAuthUserCredential
-import com.arcgismaps.mapping.ArcGISMap
-import com.arcgismaps.mapping.PortalItem
-import com.arcgismaps.portal.Portal
-import com.esri.arcgismaps.sample.authenticatewithoauth.databinding.ActivityMainBinding
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import kotlinx.coroutines.launch
+import com.esri.arcgismaps.sample.authenticatewithoauth.screens.MainScreen
+import com.esri.arcgismaps.sample.sampleslib.theme.SampleAppTheme
 
-class MainActivity : AppCompatActivity() {
-
-    // set up data binding for the activity
-    private val activityMainBinding: ActivityMainBinding by lazy {
-        DataBindingUtil.setContentView(this, R.layout.activity_main)
-    }
-
-    private val mapView by lazy {
-        activityMainBinding.mapView
-    }
-
-    // to view the traffic layer in the portal, you must enter valid ArcGIS Online credentials.
-    private val portal by lazy {
-        Portal(getString(R.string.oauth_sample_portal_url), Portal.Connection.Authenticated)
-    }
-
-    private val oAuthConfiguration by lazy {
-        OAuthUserConfiguration(
-            portalUrl = portal.url,
-            clientId = getString(R.string.oauth_sample_client_id),
-            redirectUrl = getString(R.string.oauth_sample_redirect_uri)
-        )
-    }
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // authentication with an API key or named user is
+        // required to access basemaps and other location services
+        ArcGISEnvironment.apiKey = ApiKey.create(BuildConfig.API_KEY)
 
-        // initialize the OAuth sign in view model to receive
-        // the result from the activity result contract
-        val oAuthUserSignInViewModel = ViewModelProvider(
-            owner = this,
-            factory = OAuthUserSignInViewModel.getFactory { activityResultRegistry }
-        )[OAuthUserSignInViewModel::class.java]
-
-        lifecycle.addObserver(oAuthUserSignInViewModel)
-        lifecycle.addObserver(mapView)
-
-        setUpArcGISAuthenticationChallengeHandler(oAuthUserSignInViewModel)
-
-        // check if the portal can be loaded
-        lifecycleScope.launch {
-            portal.load().onSuccess {
-                MaterialAlertDialogBuilder(this@MainActivity).setMessage(
-                    "Portal succeeded to load, portal user: ${portal.user?.username}"
-                ).show()
-                // authentication complete, display PortalItem
-                mapView.apply {
-                    visibility = View.VISIBLE
-                    map = ArcGISMap(PortalItem(portal.url))
-                }
-            }.onFailure { throwable ->
-                // authentication failed, display error message
-                activityMainBinding.authFailedMessage.apply {
-                    visibility = View.VISIBLE
-                    text = String.format("Portal failed to load, ${throwable.message}")
-                }
+        setContent {
+            SampleAppTheme {
+                AuthenticateWithOAuthApp()
             }
         }
     }
 
-    /**
-     * Sets up the [ArcGISAuthenticationChallengeHandler] to create an
-     * [OAuthUserCredential] by launching a browser page to perform a OAuth user
-     * login prompt using [oAuthUserSignInViewModel]
-     */
-    private fun setUpArcGISAuthenticationChallengeHandler(oAuthUserSignInViewModel: OAuthUserSignInViewModel) {
-        ArcGISEnvironment.authenticationManager.arcGISAuthenticationChallengeHandler =
-            ArcGISAuthenticationChallengeHandler { challenge ->
-                if (oAuthConfiguration.canBeUsedForUrl(challenge.requestUrl)) {
-                    val oAuthUserCredential =
-                        OAuthUserCredential.create(oAuthConfiguration) { oAuthUserSignIn ->
-                            oAuthUserSignInViewModel.promptForOAuthUserSignIn(oAuthUserSignIn)
-                        }.getOrThrow()
-
-                    ArcGISAuthenticationChallengeResponse.ContinueWithCredential(oAuthUserCredential)
-                } else {
-                    ArcGISAuthenticationChallengeResponse.ContinueAndFailWithError(
-                        UnsupportedOperationException()
-                    )
-                }
-            }
+    @Composable
+    private fun AuthenticateWithOAuthApp() {
+        Surface(
+            color = MaterialTheme.colorScheme.background
+        ) {
+            MainScreen(
+                sampleName = getString(R.string.app_name)
+            )
+        }
     }
 }
