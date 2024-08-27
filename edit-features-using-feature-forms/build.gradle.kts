61553ca7b3137572dbcb63b1f5c7d542184e0fd5
Shubham Sharma <shubhamsharma@esri.com>
2024-08-27 16:18:20 -0700

    Release v200.5.0 (#230)

--- edit-features-using-feature-forms/build.gradle.kts
+++ edit-features-using-feature-forms/build.gradle.kts
@@ -7,7 +7,7 @@ android {
     compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
-        applicationId = "com.esri.arcgismaps.sample.generateofflinemap"
+        applicationId = "com.esri.arcgismaps.sample.editfeaturesusingfeatureforms"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = libs.versions.versionCode.get().toInt()
@@ -31,7 +31,7 @@ android {
         kotlinCompilerExtensionVersion = libs.versions.kotlinCompilerExt.get()
     }
 
-    namespace = "com.esri.arcgismaps.sample.generateofflinemap"
+    namespace = "com.esri.arcgismaps.sample.editfeaturesusingfeatureforms"
 }
 
 dependencies {
@@ -51,4 +51,5 @@ dependencies {
     // Toolkit dependencies
     implementation(platform(libs.arcgis.maps.kotlin.toolkit.bom))
     implementation(libs.arcgis.maps.kotlin.toolkit.geoview.compose)
+    implementation(libs.arcgis.maps.kotlin.toolkit.featureforms)
 }
---
+++ generate-offline-map/src/main/java/com/esri/arcgismaps/sample/generateofflinemap/screens/MainScreen.kt
@@ -0,0 +1,128 @@
+/* Copyright 2024 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.esri.arcgismaps.sample.generateofflinemap.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.arcgismaps.LoadStatus
+import com.arcgismaps.toolkit.geoviewcompose.MapView
+import com.esri.arcgismaps.sample.generateofflinemap.R
+import com.esri.arcgismaps.sample.generateofflinemap.components.MapViewModel
+import com.esri.arcgismaps.sample.sampleslib.components.JobLoadingDialog
+import com.esri.arcgismaps.sample.sampleslib.components.MessageDialog
+import com.esri.arcgismaps.sample.sampleslib.components.SampleTopAppBar
+
+/**
+ * Main screen layout for the sample app
+ */
+@Composable
+fun MainScreen(sampleName: String) {
+
+    val application = LocalContext.current.applicationContext
+    // Create a ViewModel to handle MapView interactions
+    val mapViewModel: MapViewModel = viewModel()
+
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = mapViewModel.snackbarHostState) },
+        topBar = { SampleTopAppBar(title = sampleName) },
+        content = {
+            // Column composable that wraps around MapView
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(it)
+            ) {
+                MapView(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxWidth()
+                        // Retrieve the size of the Composable MapView
+                        .onSizeChanged { size ->
+                            mapViewModel.updateMapViewSize(size)
+                        },
+                    arcGISMap = mapViewModel.map,
+                    graphicsOverlays = listOf(mapViewModel.graphicsOverlay),
+                    mapViewProxy = mapViewModel.mapViewProxy,
+                    onLayerViewStateChanged = {
+                        // On launch, ensure the map is loaded before calculating the download area
+                        if (mapViewModel.map.loadStatus.value == LoadStatus.Loaded) {
+                            mapViewModel.calculateDownloadOfflineArea()
+                        }
+                    },
+                    onViewpointChangedForCenterAndScale = {
+                        // Ensure the map is loaded before calculating the download area
+                        if (mapViewModel.map.loadStatus.value == LoadStatus.Loaded) {
+                            mapViewModel.calculateDownloadOfflineArea()
+                        }
+                    },
+
+                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Button(
+                        onClick = {
+                            when (mapViewModel.takeMapOfflineButtonText) {
+                                application.getString(R.string.take_map_offline) -> mapViewModel.createOfflineMapJob()
+                                else -> mapViewModel.resetButtonClick()
+                            }
+                        },
+                    ) {
+                        Text(text = mapViewModel.takeMapOfflineButtonText)
+                    }
+                }
+
+                // Display progress dialog while generating an offline map
+                if (mapViewModel.showJobProgressDialog) {
+                    JobLoadingDialog(
+                        title = "Generating offline map...",
+                        progress = mapViewModel.offlineMapJobProgress,
+                        cancelJobRequest = { mapViewModel.cancelOfflineMapJob() }
+                    )
+                }
+
+                // Display a dialog if the sample encounters an error
+                mapViewModel.messageDialogVM.apply {
+                    if (dialogStatus) {
+                        MessageDialog(
+                            title = messageTitle,
+                            description = messageDescription,
+                            onDismissRequest = ::dismissDialog
+                        )
+                    }
+                }
+            }
+        }
+    )
+}
