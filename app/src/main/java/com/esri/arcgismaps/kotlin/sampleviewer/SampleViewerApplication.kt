c7c85ea0250a6fdaa694dadc47b73618f334f8e6
Trevor Draeseke <trev8939@esri.com>
2024-10-28 10:45:15 -0700

    use bounded coroutine scope

--- app/src/main/java/com/esri/arcgismaps/kotlin/sampleviewer/SampleViewerApplication.kt
+++ app/src/main/java/com/esri/arcgismaps/kotlin/sampleviewer/SampleViewerApplication.kt
@@ -2,14 +2,15 @@ package com.esri.arcgismaps.kotlin.sampleviewer
 
 import android.app.Application
 import com.esri.arcgismaps.kotlin.sampleviewer.model.DefaultSampleInfoRepository
-import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class SampleViewerApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        MainScope().launch {
+        CoroutineScope(Dispatchers.IO).launch {
             // Load the repository once at app launch
             DefaultSampleInfoRepository.load(applicationContext)
         }
