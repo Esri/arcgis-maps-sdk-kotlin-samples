3e765173a2b4de0e23ac3dd1ab7a0bfcde745223
Oliver Smith <osmith@esri.com>
2024-10-31 15:02:55 +0000

    Minimal repro (doesn't crash)

--- geoview-npe-repro/src/main/java/com/esri/arcgismaps/sample/geoviewnperepro/screens/MainScreen.kt
+++ geoview-npe-repro/src/main/java/com/esri/arcgismaps/sample/geoviewnperepro/screens/MainScreen.kt
@@ -17,17 +17,25 @@
 package com.esri.arcgismaps.sample.geoviewnperepro.screens
 
 import android.app.Application
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.arcgismaps.mapping.ArcGISMap
 import com.arcgismaps.mapping.BasemapStyle
+import com.arcgismaps.mapping.view.MapViewInteractionOptions
 import com.arcgismaps.toolkit.geoviewcompose.MapView
 import com.esri.arcgismaps.sample.geoviewnperepro.components.MapViewModel
 import com.esri.arcgismaps.sample.sampleslib.components.SampleTopAppBar
@@ -38,24 +46,57 @@ import com.esri.arcgismaps.sample.sampleslib.components.SampleTopAppBar
 @Composable
 fun MainScreen(sampleName: String) {
     val application = LocalContext.current.applicationContext as Application
-    // create a ViewModel to handle MapView interactions
-    val mapViewModel = MapViewModel(application)
-    val arcGISMap by remember {
-        mutableStateOf(ArcGISMap(BasemapStyle.ArcGISNavigationNight).apply {
-            initialViewpoint = mapViewModel.viewpoint.value
-        })
+
+    // create a list with a few maps in it
+    val maps = List(10) {
+        Pair(MapViewModel(application), remember { ArcGISMap(BasemapStyle.ArcGISStreets) })
+    }
+    for ((mvm, map) in maps) {
+        map.apply { initialViewpoint = mvm.viewpoint.value }
     }
 
     Scaffold(
         topBar = { SampleTopAppBar(title = sampleName) },
         content = {
-            MapView(
-                modifier = Modifier.fillMaxSize().padding(it),
-                arcGISMap = arcGISMap,
-                onSingleTapConfirmed = {
-                    mapViewModel.changeBasemap()
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(it)
+            ) {
+                LazyColumn(Modifier.fillMaxWidth()) {
+                    for ((_, map) in maps) {
+                        item { TripItemCard(arcGISMap = map) }
+                    }
                 }
-            )
+            }
         }
     )
+}
+
+@Composable
+fun TripItemCard(arcGISMap: ArcGISMap) {
+    Card(
+        Modifier
+            .height(250.dp)
+            .padding(horizontal = 25.dp, vertical = 10.dp)
+            .fillMaxSize()
+    ) {
+        Row(Modifier.fillMaxWidth()) {
+            MapView(
+                modifier = Modifier.width(250.dp),
+                arcGISMap = arcGISMap,
+                mapViewInteractionOptions = MapViewInteractionOptions(
+                    isEnabled = false,
+                    isMagnifierEnabled = false,
+                    isZoomEnabled = false,
+                    isRotateEnabled = false,
+                    isFlingEnabled = false,
+                    allowMagnifierToPan = false,
+                    isPanEnabled = false
+                ),
+                onSingleTapConfirmed = {}
+            )
+            Text(text = "lorem ipsum dolor sit amet")
+        }
+    }
 }
