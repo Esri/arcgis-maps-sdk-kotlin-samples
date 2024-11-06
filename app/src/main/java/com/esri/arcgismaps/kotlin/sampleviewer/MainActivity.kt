bae2e11604805ad68bf27aed6c356116fc660d1f
Shubham Sharma <shubhamsharma@esri.com>
2024-11-06 15:52:09 -0800

    Update MainActivity.kt

--- app/src/main/java/com/esri/arcgismaps/kotlin/sampleviewer/MainActivity.kt
+++ app/src/main/java/com/esri/arcgismaps/kotlin/sampleviewer/MainActivity.kt
@@ -29,9 +29,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
         setContent {
             setContent {
+                
                 SampleAppTheme {
                     Surface(color = MaterialTheme.colorScheme.background) {
                         NavGraph()
