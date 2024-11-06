af32a29ccfc9f22e2c7682924a0461e581b042df
Shubham Sharma <shubhamsharma@esri.com>
2024-11-06 15:51:28 -0800

    Update MainActivity.kt

--- app/src/main/java/com/esri/arcgismaps/kotlin/sampleviewer/MainActivity.kt
+++ app/src/main/java/com/esri/arcgismaps/kotlin/sampleviewer/MainActivity.kt
@@ -19,6 +19,7 @@ package com.esri.arcgismaps.kotlin.sampleviewer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import com.esri.arcgismaps.kotlin.sampleviewer.navigation.NavGraph
@@ -27,6 +28,8 @@ import com.esri.arcgismaps.sample.sampleslib.theme.SampleAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
         setContent {
             setContent {
                 SampleAppTheme {
