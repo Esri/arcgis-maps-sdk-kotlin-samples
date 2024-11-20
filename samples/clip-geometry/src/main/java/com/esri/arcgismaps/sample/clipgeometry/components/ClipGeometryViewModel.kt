9d0fe9d19b62c0bf0d369238c2785543923b8eb3
Darryl Lynch <dlynch@esri.com>
2024-11-20 14:50:57 +0000

    First draft of updated clip geometry

--- samples/clip-geometry/src/main/java/com/esri/arcgismaps/sample/clipgeometry/components/ClipGeometryViewModel.kt
+++ samples/clip-geometry/src/main/java/com/esri/arcgismaps/sample/clipgeometry/components/ClipGeometryViewModel.kt
@@ -21,21 +21,50 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import com.arcgismaps.Color
+import com.arcgismaps.geometry.Envelope
+import com.arcgismaps.geometry.Geometry
+import com.arcgismaps.geometry.GeometryEngine
+import com.arcgismaps.geometry.Point
 import com.arcgismaps.mapping.ArcGISMap
 import com.arcgismaps.mapping.BasemapStyle
 import com.arcgismaps.mapping.Viewpoint
+import com.arcgismaps.mapping.symbology.SimpleFillSymbol
+import com.arcgismaps.mapping.symbology.SimpleFillSymbolStyle
+import com.arcgismaps.mapping.symbology.SimpleLineSymbol
+import com.arcgismaps.mapping.symbology.SimpleLineSymbolStyle
+import com.arcgismaps.mapping.view.Graphic
+import com.arcgismaps.mapping.view.GraphicsOverlay
+import com.arcgismaps.toolkit.geoviewcompose.MapViewProxy
+import com.esri.arcgismaps.sample.clipgeometry.R
 import com.esri.arcgismaps.sample.sampleslib.components.MessageDialogViewModel
 import kotlinx.coroutines.launch
 
 class ClipGeometryViewModel(application: Application) : AndroidViewModel(application) {
     val arcGISMap by mutableStateOf(
-        ArcGISMap(BasemapStyle.ArcGISNavigationNight).apply {
-            initialViewpoint = Viewpoint(39.8, -98.6, 10e7)
-        }
+        // create  a map with a topographic basemap style
+        ArcGISMap(BasemapStyle.ArcGISTopographic)
     )
+
+    // create a MapViewProxy to interact with the MapView
+    val mapViewProxy = MapViewProxy()
+
+    // graphics overlay to display graphics
+    val graphicsOverlay = GraphicsOverlay()
+
+    private var coloradoGraphic: Graphic? = null
+    private var coloradoFillSymbol = SimpleFillSymbol(
+        SimpleFillSymbolStyle.Solid,
+        Color(R.color.transparentDarkBlue),
+        SimpleLineSymbol(SimpleLineSymbolStyle.Solid, Color.green, 2f)
+    )
+    private var envelopesGraphics = emptyList<Graphic>()
 
     // Create a message dialog view model for handling error messages
     val messageDialogVM = MessageDialogViewModel()
+
+    val isClipButtonEnabled = mutableStateOf(true)
+    val isResetButtonEnabled = mutableStateOf(false)
 
     init {
         viewModelScope.launch {
@@ -44,7 +73,105 @@ class ClipGeometryViewModel(application: Application) : AndroidViewModel(applica
                     "Failed to load map",
                     error.message.toString()
                 )
+            }.onSuccess {
+
+
+                // set the viewpoint of the map
+                mapViewProxy.setViewpoint(Viewpoint(40.0, -106.0, 10000000.0))
+
+                resetGeometry()
+
             }
         }
     }
+
+
+
+    fun resetGeometry() {
+
+        graphicsOverlay.graphics.clear()
+
+        coloradoGraphic = createColoradoGraphic()
+
+        graphicsOverlay.graphics.add(coloradoGraphic!!)
+
+        envelopesGraphics = createEnvelopeGraphics()
+
+        envelopesGraphics.forEach { graphic ->
+            graphicsOverlay.graphics.add(graphic)
+        }
+
+        isResetButtonEnabled.value = false
+        isClipButtonEnabled.value = true
+    }
+
+    fun clipGeometry() {
+
+        coloradoGraphic?.isVisible = false
+
+        envelopesGraphics.forEach { graphic ->
+            val geometry =
+                coloradoGraphic?.geometry?.let { coloradoGeometry ->
+                    GeometryEngine.clipOrNull(coloradoGeometry, graphic.geometry as Envelope)
+                }
+            val clippedGraphic = Graphic(geometry, coloradoFillSymbol)
+            graphicsOverlay.graphics.add(clippedGraphic)
+        }
+
+        isResetButtonEnabled.value = true
+        isClipButtonEnabled.value = false
+    }
+
+    /**
+     * Create colorado graphic.
+     */
+    private fun createColoradoGraphic(
+    ) : Graphic {
+        // create a blue graphic of Colorado
+        val colorado = Envelope(
+            Point(-11362327.128340, 5012861.290274),
+            Point(-12138232.018408, 4441198.773776)
+        )
+        val fillSymbol = SimpleFillSymbol(
+            SimpleFillSymbolStyle.Solid,
+            Color(R.color.transparentDarkBlue),
+            SimpleLineSymbol(SimpleLineSymbolStyle.Solid, Color.green, 2f)
+        )
+        val coloradoGraphic = Graphic(colorado, fillSymbol)
+
+        return coloradoGraphic
+    }
+
+    /**
+     * Create three envelopes, outside, inside and intersecting colorado graphic.
+     */
+    private fun createEnvelopeGraphics(
+    ) : List<Graphic> {
+        // create a dotted red outline symbol
+        val redOutline = SimpleLineSymbol(SimpleLineSymbolStyle.Dot, Color.red, 3f)
+
+        // create a envelope outside Colorado
+        val outsideEnvelope = Envelope(
+            Point(-11858344.321294, 5147942.225174),
+            Point(-12201990.219681, 5297071.577304)
+        )
+        val outside = Graphic(outsideEnvelope, redOutline)
+
+        // create a envelope intersecting Colorado
+        val intersectingEnvelope = Envelope(
+            Point(-11962086.479298, 4566553.881363),
+            Point(-12260345.183558, 4332053.378376)
+        )
+        val intersecting = Graphic(intersectingEnvelope, redOutline)
+
+        // create a envelope inside Colorado
+        val containedEnvelope = Envelope(
+            Point(-11655182.595204, 4741618.772994),
+            Point(-11431488.567009, 4593570.068343)
+        )
+        val contained = Graphic(containedEnvelope, redOutline)
+
+        return listOf(outside, intersecting, contained)
+    }
+
 }
