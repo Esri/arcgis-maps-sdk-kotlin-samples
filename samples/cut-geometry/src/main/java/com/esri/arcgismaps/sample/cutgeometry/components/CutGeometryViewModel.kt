71f3f2ea33f52a9636da3ad14c8fce797165304b
Darryl Lynch <dlynch@esri.com>
2025-01-08 18:54:51 +0000

    Basic cut geometry sample implementation

--- samples/cut-geometry/src/main/java/com/esri/arcgismaps/sample/cutgeometry/components/CutGeometryViewModel.kt
+++ samples/cut-geometry/src/main/java/com/esri/arcgismaps/sample/cutgeometry/components/CutGeometryViewModel.kt
@@ -21,19 +21,117 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import com.arcgismaps.Color
+import com.arcgismaps.geometry.GeometryEngine
+import com.arcgismaps.geometry.Point
+import com.arcgismaps.geometry.PolygonBuilder
+import com.arcgismaps.geometry.Polyline
+import com.arcgismaps.geometry.PolylineBuilder
+import com.arcgismaps.geometry.SpatialReference
 import com.arcgismaps.mapping.ArcGISMap
 import com.arcgismaps.mapping.BasemapStyle
 import com.arcgismaps.mapping.Viewpoint
+import com.arcgismaps.mapping.symbology.SimpleFillSymbol
+import com.arcgismaps.mapping.symbology.SimpleFillSymbolStyle
+import com.arcgismaps.mapping.symbology.SimpleLineSymbol
+import com.arcgismaps.mapping.symbology.SimpleLineSymbolStyle
+import com.arcgismaps.mapping.view.Graphic
+import com.arcgismaps.mapping.view.GraphicsOverlay
+import com.arcgismaps.toolkit.geoviewcompose.MapViewProxy
+import com.esri.arcgismaps.sample.cutgeometry.R
 import com.esri.arcgismaps.sample.sampleslib.components.MessageDialogViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class CutGeometryViewModel(application: Application) : AndroidViewModel(application) {
+
     //TODO - delete mutable state when the map does not change or the screen does not need to observe changes
     val arcGISMap by mutableStateOf(
-        ArcGISMap(BasemapStyle.ArcGISNavigationNight).apply {
-            initialViewpoint = Viewpoint(39.8, -98.6, 10e7)
+        ArcGISMap(BasemapStyle.ArcGISTopographic).apply {
+            initialViewpoint = Viewpoint(
+                latitude = 39.8,
+                longitude = -98.6,
+                scale = 10e7
+            )
         }
     )
+
+    // create a MapViewProxy to interact with the MapView
+    val mapViewProxy = MapViewProxy()
+
+    private val lakeSuperiorPolygon by lazy {
+        PolygonBuilder(SpatialReference.webMercator()) {
+            addPoint(Point(-10254374.668616, 5908345.076380))
+            addPoint(Point(-10178382.525314, 5971402.386779))
+            addPoint(Point(-10118558.923141, 6034459.697178))
+            addPoint(Point(-9993252.729399, 6093474.872295))
+            addPoint(Point(-9882498.222673, 6209888.368416))
+            addPoint(Point(-9821057.766387, 6274562.532928))
+            addPoint(Point(-9690092.583250, 6241417.023616))
+            addPoint(Point(-9605207.742329, 6206654.660191))
+            addPoint(Point(-9564786.389509, 6108834.986367))
+            addPoint(Point(-9449989.747500, 6095091.726408))
+            addPoint(Point(-9462116.153346, 6044160.821855))
+            addPoint(Point(-9417652.665244, 5985145.646738))
+            addPoint(Point(-9438671.768711, 5946341.148031))
+            addPoint(Point(-9398250.415891, 5922088.336339))
+            addPoint(Point(-9419269.519357, 5855797.317714))
+            addPoint(Point(-9467775.142741, 5858222.598884))
+            addPoint(Point(-9462924.580403, 5902686.086985))
+            addPoint(Point(-9598740.325877, 5884092.264688))
+            addPoint(Point(-9643203.813979, 5845287.765981))
+            addPoint(Point(-9739406.633691, 5879241.702350))
+            addPoint(Point(-9783061.694736, 5922896.763395))
+            addPoint(Point(-9844502.151022, 5936640.023354))
+            addPoint(Point(-9773360.570059, 6019099.583107))
+            addPoint(Point(-9883306.649729, 5968977.105610))
+            addPoint(Point(-9957681.938918, 5912387.211662))
+            addPoint(Point(-10055501.612742, 5871965.858842))
+            addPoint(Point(-10116942.069028, 5884092.264688))
+            addPoint(Point(-10111283.079633, 5933406.315128))
+            addPoint(Point(-10214761.742852, 5888134.399970))
+            addPoint(Point(-10254374.668616, 5901877.659929))
+        }.toGeometry()
+    }
+
+    private val borderPolyline by lazy {
+        PolylineBuilder(SpatialReference.webMercator()) {
+            addPoint(Point(-9981328.687124, 6111053.281447))
+            addPoint(Point(-9946518.044066, 6102350.620682))
+            addPoint(Point(-9872545.427566, 6152390.920079))
+            addPoint(Point(-9838822.617103, 6157830.083057))
+            addPoint(Point(-9446115.050097, 5927209.572793))
+            addPoint(Point(-9430885.393759, 5876081.440801))
+            addPoint(Point(-9415655.737420, 5860851.784463))
+        }.toGeometry()
+    }
+
+    // create a blue polygon graphic to cut
+    private val polygonGraphic = Graphic(
+        lakeSuperiorPolygon,
+        SimpleFillSymbol(
+            SimpleFillSymbolStyle.Solid, Color(R.color.transparentBlue),
+            SimpleLineSymbol(SimpleLineSymbolStyle.Solid, Color.blue, 2F)
+        )
+    )
+
+    // create a red polyline graphic to cut the polygon
+    private val polylineGraphic = Graphic(
+        borderPolyline, SimpleLineSymbol(
+            SimpleLineSymbolStyle.Dot,
+            Color.red, 3F
+        )
+    )
+
+    private val _isResetButtonEnabled = MutableStateFlow(false)
+    val isResetButtonEnabled = _isResetButtonEnabled.asStateFlow()
+
+    private val _isCutButtonEnabled = MutableStateFlow(false)
+    val isCutButtonEnabled = _isCutButtonEnabled.asStateFlow()
+
+    // create a graphic overlay
+    val graphicsOverlay = GraphicsOverlay()
 
     // Create a message dialog view model for handling error messages
     val messageDialogVM = MessageDialogViewModel()
@@ -45,7 +143,63 @@ class CutGeometryViewModel(application: Application) : AndroidViewModel(applicat
                     "Failed to load map",
                     error.message.toString()
                 )
+            }.onSuccess {
+                graphicsOverlay.graphics.add(polylineGraphic)
+                graphicsOverlay.graphics.add(polygonGraphic)
+                polygonGraphic.geometry?.let { polygonToCut ->
+                    mapViewProxy.setViewpoint(Viewpoint(polygonToCut))
+                }
+                _isCutButtonEnabled.value = true
             }
         }
     }
+
+    fun resetGeometry() {
+        graphicsOverlay.graphics.clear()
+        graphicsOverlay.graphics.add(polylineGraphic)
+        graphicsOverlay.graphics.add(polygonGraphic)
+        polygonGraphic.geometry?.let { polygonToCut ->
+            mapViewProxy.setViewpoint(Viewpoint(polygonToCut))
+        }
+
+        _isCutButtonEnabled.value = true
+
+        _isResetButtonEnabled.value = false
+        _isCutButtonEnabled.value = true
+    }
+
+    fun cutGeometry() {
+        polygonGraphic.geometry?.let { graphicGeometry ->
+            val parts = GeometryEngine.tryCut(
+                graphicGeometry,
+                polylineGraphic.geometry as Polyline
+            )
+
+            // create graphics for the US and Canada sides
+            val canadaSide = Graphic(
+                parts[0], SimpleFillSymbol(
+                    SimpleFillSymbolStyle.BackwardDiagonal,
+                    Color.green, SimpleLineSymbol(SimpleLineSymbolStyle.Null, Color.blue, 0F)
+                )
+            )
+            val usSide = Graphic(
+                parts[1], SimpleFillSymbol(
+                    SimpleFillSymbolStyle.ForwardDiagonal,
+                    Color.yellow, SimpleLineSymbol(SimpleLineSymbolStyle.Null, Color.blue, 0F)
+                )
+            )
+            // add the graphics to the graphics overlay
+            graphicsOverlay.graphics.addAll(listOf(canadaSide, usSide))
+
+            // swap button state
+            _isCutButtonEnabled.value = false
+            _isResetButtonEnabled.value = true
+        }
+    }
+
+    private val Color.Companion.blue: Color
+        get() {
+            return fromRgba(0, 0, 255, 255)
+        }
+
 }
--- samples/cut-geometry/src/main/java/com/esri/arcgismaps/sample/cutgeometry/screens/CutGeometryScreen.kt
+++ samples/cut-geometry/src/main/java/com/esri/arcgismaps/sample/cutgeometry/screens/CutGeometryScreen.kt
@@ -16,15 +16,24 @@
 
 package com.esri.arcgismaps.sample.cutgeometry.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.arcgismaps.toolkit.geoviewcompose.MapView
+import com.esri.arcgismaps.sample.cutgeometry.R
 import com.esri.arcgismaps.sample.cutgeometry.components.CutGeometryViewModel
 import com.esri.arcgismaps.sample.sampleslib.components.MessageDialog
 import com.esri.arcgismaps.sample.sampleslib.components.SampleTopAppBar
@@ -35,6 +44,8 @@ import com.esri.arcgismaps.sample.sampleslib.components.SampleTopAppBar
 @Composable
 fun CutGeometryScreen(sampleName: String) {
     val mapViewModel: CutGeometryViewModel = viewModel()
+    val isResetButtonEnabled by mapViewModel.isResetButtonEnabled.collectAsStateWithLifecycle()
+    val isCutButtonEnabled by mapViewModel.isCutButtonEnabled.collectAsStateWithLifecycle()
     Scaffold(
         topBar = { SampleTopAppBar(title = sampleName) },
         content = {
@@ -47,18 +58,46 @@ fun CutGeometryScreen(sampleName: String) {
                     modifier = Modifier
                         .fillMaxSize()
                         .weight(1f),
-                    arcGISMap = mapViewModel.arcGISMap
+                    arcGISMap = mapViewModel.arcGISMap,
+                    graphicsOverlays = listOf(mapViewModel.graphicsOverlay),
+                    mapViewProxy = mapViewModel.mapViewProxy
                 )
-                // TODO: Add UI components in this Column ...
-            }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Button(
+                        modifier = Modifier.padding(12.dp),
+                        enabled = isResetButtonEnabled,
+                        onClick = {
+                            mapViewModel.resetGeometry()
+                        }
+                    ) {
+                        Text(
+                            text = stringResource(R.string.reset_button_text)
+                        )
+                    }
+                    Button(
+                        modifier = Modifier.padding(12.dp),
+                        enabled = isCutButtonEnabled,
+                        onClick = {
+                            mapViewModel.cutGeometry()
+                        }
+                    ) {
+                        Text(
+                            text = stringResource(R.string.cut_geometry_button_text)
+                        )
+                    }
+                }
 
-            mapViewModel.messageDialogVM.apply {
-                if (dialogStatus) {
-                    MessageDialog(
-                        title = messageTitle,
-                        description = messageDescription,
-                        onDismissRequest = ::dismissDialog
-                    )
+                mapViewModel.messageDialogVM.apply {
+                    if (dialogStatus) {
+                        MessageDialog(
+                            title = messageTitle,
+                            description = messageDescription,
+                            onDismissRequest = ::dismissDialog
+                        )
+                    }
                 }
             }
         }
