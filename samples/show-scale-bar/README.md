4b060a4296ff6c3afdcfb5635363dd96ea249d7d
Shubham Sharma <shubhamsharma@esri.com>
2025-02-13 09:01:49 -0800

    added sample implementation

--- samples/show-scale-bar/README.md
+++ samples/show-scale-bar/README.md
@@ -10,15 +10,14 @@ Allows a user to have a visual reference for distances when navigating the map.
 
 ## How to use the sample
 
-Zoom in or out of the map. The scale bar will automatically display the appropriate scale based on zoom level. Units can be in metric and/or imperial.
+Zoom in or out of the map. The scale bar will automatically display the appropriate scale based on zoom level. Units can be in metric and/or imperial based on locale/optional settings.
 
 ## How it works
 
-1. Instantiate an `ArcGISMap`object and display it in a `MapView` object.
-2. Instantiate a `ScaleBar` object passing in the map view.
-3. Set the scale bar's skin style with `setSkinStyle(skinStyle)`.
-4. Set the unit system with `setUnitSystem(unitSystem)`.
-5. Add the scale bar to the UI overlaying the map view.
+1. Create an `ArcGISMap` and add it to a `MapView` composable.
+2. Listen and track callback changes from `onUnitsPerDipChanged`, `onViewpointChangedForCenterAndScale`, `onSpatialReferenceChanged` using the composable `MapView`.
+3. Add the `ScaleBar` composable positioned on top of the `MapView`
+4. Pass in the latest values of `unitsPerDip`, `viewpoint`, `spatialReference` & use a preferred `maxWidth` into the `ScaleBar`.
 
 ## Relevant API
 
@@ -31,7 +30,7 @@ Zoom in or out of the map. The scale bar will automatically display the appropri
 
 The scale will be accurate for the center of the map, and in general more accurate at larger scales (zoomed in). This means at smaller scales (zoomed out), the reading may be inaccurate at the extremes of the visible extent.
 
-This sample uses the scale bar toolkit component, which requires the [toolkit](https://github.com/Esri/arcgis-runtime-toolkit-qt) to be cloned and set up locally. For information about setting up the toolkit, as well as code for the underlying component, visit the repository's [UI Tools page](https://github.com/Esri/arcgis-runtime-toolkit-qt/blob/main/uitools).
+This sample uses the scale bar toolkit component, which requires the [toolkit](https://github.com/Esri/arcgis-maps-sdk-kotlin-toolkit). For information about setting up the toolkit, as well as code for the underlying component, visit the the developer's [documentation page](https://developers.arcgis.com/kotlin/toolkit/).
 
 ## Tags
 
--- samples/show-scale-bar/src/main/java/com/esri/arcgismaps/sample/showscalebar/screens/ShowScaleBarScreen.kt
+++ samples/show-scale-bar/src/main/java/com/esri/arcgismaps/sample/showscalebar/screens/ShowScaleBarScreen.kt
@@ -18,21 +18,29 @@
 
 package com.esri.arcgismaps.sample.showscalebar.screens
 
+import android.content.res.Configuration
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.MenuAnchorType
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -40,14 +48,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.arcgismaps.UnitSystem
 import com.arcgismaps.toolkit.geoviewcompose.MapView
 import com.arcgismaps.toolkit.scalebar.Scalebar
 import com.arcgismaps.toolkit.scalebar.ScalebarStyle
+import com.esri.arcgismaps.sample.sampleslib.components.DropDownMenuBox
 import com.esri.arcgismaps.sample.sampleslib.components.MessageDialog
 import com.esri.arcgismaps.sample.sampleslib.components.SampleTopAppBar
+import com.esri.arcgismaps.sample.sampleslib.theme.SampleAppTheme
 import com.esri.arcgismaps.sample.showscalebar.components.ShowScaleBarViewModel
+import kotlin.time.Duration
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
 
 /**
  * Main screen layout for the sample app
@@ -57,42 +73,65 @@ fun ShowScaleBarScreen(sampleName: String) {
     val mapViewModel: ShowScaleBarViewModel = viewModel()
     // Keep track of the currently selected scalebar
     var currentScalebarStyle by remember { mutableStateOf(ScalebarStyle.AlternatingBar) }
+    var isScalebarDialogOptionsVisible by remember { mutableStateOf(false) }
+    var currentAutoHideDelay by remember { mutableStateOf(Duration.ZERO) }
+    var isGeodeticCalculationsEnabled by remember { mutableStateOf(true) }
+    var currentScalebarUnitSystem by remember { mutableStateOf<UnitSystem>(UnitSystem.Metric) }
 
     Scaffold(
         topBar = { SampleTopAppBar(title = sampleName) },
+        floatingActionButton = {
+            if (!isScalebarDialogOptionsVisible) {
+                FloatingActionButton(
+                    modifier = Modifier.padding(bottom = 36.dp, end = 12.dp),
+                    onClick = { isScalebarDialogOptionsVisible = true }
+                ) { Icon(Icons.Filled.Settings, contentDescription = "Show Scalebar Options") }
+            }
+        },
         content = { paddingValues ->
-            Column {
-                ScalebarStylesDropDown(
-                    currentScalebarStyle = currentScalebarStyle,
-                    onScalebarStyleSelected = { currentScalebarStyle = it }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+            ) {
+                MapView(
+                    modifier = Modifier.fillMaxSize(),
+                    arcGISMap = mapViewModel.arcGISMap,
+                    isAttributionBarVisible = false,
+                    onSpatialReferenceChanged = mapViewModel::updateSpacialReference,
+                    onUnitsPerDipChanged = mapViewModel::updateUnitsPerDip,
+                    onViewpointChangedForCenterAndScale = mapViewModel::updateViewpoint
                 )
-                Box(
+                Scalebar(
                     modifier = Modifier
-                        .fillMaxSize()
-                        .weight(1f)
-                        .padding(paddingValues)
-                ) {
-                    MapView(
-                        modifier = Modifier.fillMaxSize(),
-                        arcGISMap = mapViewModel.arcGISMap,
-                        onSpatialReferenceChanged = mapViewModel::updateSpacialReference,
-                        onUnitsPerDipChanged = mapViewModel::updateUnitsPerDip,
-                        onViewpointChangedForCenterAndScale = mapViewModel::updateViewpoint
-                    )
+                        .padding(25.dp)
+                        .align(Alignment.BottomStart),
+                    maxWidth = 300.dp,
+                    unitsPerDip = mapViewModel.unitsPerDip,
+                    viewpoint = mapViewModel.viewpoint,
+                    spatialReference = mapViewModel.spatialReference,
+                    style = currentScalebarStyle,
+                    units = currentScalebarUnitSystem,
+                    autoHideDelay = currentAutoHideDelay,
+                    useGeodeticCalculations = isGeodeticCalculationsEnabled
+                )
 
-                    Scalebar(
-                        modifier = Modifier
-                            .padding(25.dp)
-                            .align(Alignment.BottomStart),
-                        maxWidth = 175.dp,
-                        unitsPerDip = mapViewModel.unitsPerDip,
-                        viewpoint = mapViewModel.viewpoint,
-                        spatialReference = mapViewModel.spatialReference,
-                        style = currentScalebarStyle
+                if (isScalebarDialogOptionsVisible) {
+                    ScalebarDialogOptions(
+                        currentScalebarStyle = currentScalebarStyle,
+                        currentScalebarUnitSystem = currentScalebarUnitSystem,
+                        currentAutoHideDelay = currentAutoHideDelay,
+                        isGeodeticCalculationsEnabled = isGeodeticCalculationsEnabled,
+                        onScalebarUnitSystemSelected = { currentScalebarUnitSystem = it },
+                        onScalebarStyleSelected = { currentScalebarStyle = it },
+                        onDismissRequest = { isScalebarDialogOptionsVisible = false },
+                        onAutoHideDelaySelected = { currentAutoHideDelay = it },
+                        onGeodeticCalculationsToggled = {
+                            isGeodeticCalculationsEnabled = !isGeodeticCalculationsEnabled
+                        }
                     )
                 }
             }
-
             mapViewModel.messageDialogVM.apply {
                 if (dialogStatus) {
                     MessageDialog(
@@ -107,50 +146,92 @@ fun ShowScaleBarScreen(sampleName: String) {
 }
 
 @Composable
-fun ScalebarStylesDropDown(
+fun ScalebarDialogOptions(
     currentScalebarStyle: ScalebarStyle,
-    onScalebarStyleSelected: (ScalebarStyle) -> Unit
+    currentScalebarUnitSystem: UnitSystem,
+    currentAutoHideDelay: Duration,
+    onDismissRequest: () -> Unit,
+    isGeodeticCalculationsEnabled: Boolean,
+    onScalebarStyleSelected: (ScalebarStyle) -> Unit,
+    onScalebarUnitSystemSelected: (UnitSystem) -> Unit,
+    onAutoHideDelaySelected: (Duration) -> Unit,
+    onGeodeticCalculationsToggled: (Boolean) -> Unit
 ) {
-    // List of all the supported scale bar types
+    // List of all the supported scale bar properties
     val scalebarStyles = ScalebarStyle.entries.toList()
-    var expanded by remember { mutableStateOf(false) }
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(12.dp),
-        verticalArrangement = Arrangement.spacedBy(12.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+    val scalebarUnitSystems = listOf(UnitSystem.Metric, UnitSystem.Imperial)
+    val autoHideDelays = listOf(
+        Duration.ZERO,
+        1.toDuration(DurationUnit.SECONDS),
+        3.toDuration(DurationUnit.SECONDS),
+        5.toDuration(DurationUnit.SECONDS)
+    )
 
-    ) {
-        ExposedDropdownMenuBox(
-            expanded = expanded,
-            onExpandedChange = { expanded = !expanded }
+    BasicAlertDialog(onDismissRequest = onDismissRequest) {
+        Column(
+            modifier = Modifier
+                .clip(RoundedCornerShape(12.dp))
+                .background(MaterialTheme.colorScheme.background)
+                .padding(12.dp)
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            TextField(
-                label = { Text("Select Scalebar Style: ") },
-                value = currentScalebarStyle.name,
-                onValueChange = {},
-                readOnly = true,
-                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
-                modifier = Modifier.menuAnchor(type = MenuAnchorType.PrimaryNotEditable)
+            Text("Scale bar options: ", style = MaterialTheme.typography.headlineSmall)
+            DropDownMenuBox(
+                textFieldLabel = "Select scale bar style: ",
+                textFieldValue = currentScalebarStyle.name,
+                dropDownItemList = scalebarStyles.map { it.name },
+                onIndexSelected = { index -> onScalebarStyleSelected(scalebarStyles[index]) }
             )
-            ExposedDropdownMenu(
-                expanded = expanded,
-                onDismissRequest = { expanded = false }
+            DropDownMenuBox(
+                textFieldLabel = "Select scale bar unit system: ",
+                textFieldValue = currentScalebarUnitSystem.javaClass.simpleName,
+                dropDownItemList = scalebarUnitSystems.map { it.javaClass.simpleName },
+                onIndexSelected = { index -> onScalebarUnitSystemSelected(scalebarUnitSystems[index]) }
+            )
+            DropDownMenuBox(
+                textFieldLabel = "Select scale bar auto-hide delay: ",
+                textFieldValue = currentAutoHideDelay.inWholeSeconds.toString(),
+                dropDownItemList = autoHideDelays.map { it.inWholeSeconds.toString() },
+                onIndexSelected = { index -> onAutoHideDelaySelected(autoHideDelays[index]) }
+            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                scalebarStyles.forEachIndexed { index, scalebarStyle ->
-                    DropdownMenuItem(
-                        text = { Text(scalebarStyle.name) },
-                        onClick = {
-                            onScalebarStyleSelected(scalebarStyle)
-                            expanded = false
-                        })
-                    // Show a divider between dropdown menu options
-                    if (index < scalebarStyles.lastIndex) {
-                        HorizontalDivider()
-                    }
-                }
+                Text(
+                    text = "Enable Geodetic Calculations",
+                    style = MaterialTheme.typography.labelLarge
+                )
+                Switch(
+                    checked = isGeodeticCalculationsEnabled,
+                    onCheckedChange = onGeodeticCalculationsToggled
+                )
             }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun PreviewScalebarDialogOptions() {
+    SampleAppTheme {
+        Surface {
+            ScalebarDialogOptions(
+                currentScalebarStyle = ScalebarStyle.AlternatingBar,
+                currentScalebarUnitSystem = UnitSystem.Metric,
+                currentAutoHideDelay = Duration.ZERO,
+                isGeodeticCalculationsEnabled = true,
+                onDismissRequest = { },
+                onScalebarStyleSelected = { },
+                onScalebarUnitSystemSelected = { },
+                onAutoHideDelaySelected = { },
+                onGeodeticCalculationsToggled = { }
+            )
         }
     }
 }
