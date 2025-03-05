e86b081795258c100381bdc6d840a4e4a8f56220
Darryl Lynch <dlynch@esri.com>
2025-03-05 15:51:49 +0000

    Add initial geometries

--- samples/create-and-edit-geometries/src/main/java/com/esri/arcgismaps/sample/createandeditgeometries/components/CreateAndEditGeometriesViewModel.kt
+++ samples/create-and-edit-geometries/src/main/java/com/esri/arcgismaps/sample/createandeditgeometries/components/CreateAndEditGeometriesViewModel.kt
@@ -34,12 +34,15 @@ import com.arcgismaps.mapping.BasemapStyle
 import com.arcgismaps.mapping.Viewpoint
 import com.arcgismaps.mapping.symbology.SimpleFillSymbol
 import com.arcgismaps.mapping.symbology.SimpleFillSymbolStyle
+import com.arcgismaps.mapping.symbology.SimpleLineSymbol
+import com.arcgismaps.mapping.symbology.SimpleLineSymbolStyle
+import com.arcgismaps.mapping.symbology.SimpleMarkerSymbol
+import com.arcgismaps.mapping.symbology.SimpleMarkerSymbolStyle
 import com.arcgismaps.mapping.view.Graphic
 import com.arcgismaps.mapping.view.GraphicsOverlay
 import com.arcgismaps.mapping.view.SingleTapConfirmedEvent
 import com.arcgismaps.mapping.view.geometryeditor.FreehandTool
 import com.arcgismaps.mapping.view.geometryeditor.GeometryEditor
-import com.arcgismaps.mapping.view.geometryeditor.GeometryEditorStyle
 import com.arcgismaps.mapping.view.geometryeditor.ReticleVertexTool
 import com.arcgismaps.mapping.view.geometryeditor.ShapeTool
 import com.arcgismaps.mapping.view.geometryeditor.ShapeToolType
@@ -68,8 +71,6 @@ class CreateAndEditGeometriesViewModel(application: Application) : AndroidViewMo
     // create a MapViewProxy that will be used to identify features in the MapView and set the viewpoint
     val mapViewProxy = MapViewProxy()
 
-    // create a geometryEditorStyle
-    private val geometryEditorStyle = GeometryEditorStyle()
     // create a graphic to hold graphics identified on tap
     private var identifiedGraphic = Graphic()
     // create a graphics overlay
@@ -104,45 +105,32 @@ class CreateAndEditGeometriesViewModel(application: Application) : AndroidViewMo
     private val _currentGeometryType = MutableStateFlow<GeometryType>(GeometryType.Unknown)
     val currentGeometryType = _currentGeometryType.asStateFlow()
 
-    // Json formatted strings for initial geometries
-    val houseCoordinatesJson = """{"x": -1067898.59, "y": 6998366.62,
-    "spatialReference": {"latestWkid":3857, "wkid":102100}}"""
-
-    val outbuildingCoordinatesJson = """{"points":[[-1067984.26,6998346.28],[-1067966.80,6998244.84],
-            [-1067921.88,6998284.65],[-1067934.36,6998340.74],
-            [-1067917.93,6998373.97],[-1067828.30,6998355.28],
-            [-1067832.25,6998339.70],[-1067823.10,6998336.93],
-            [-1067873.22,6998386.78],[-1067896.72,6998244.49]],
-            "spatialReference":{"latestWkid":3857,"wkid":102100}}"""
-
-    val road1CoordinatesJson = """{"paths":[[[-1068095.40,6998123.52],[-1068086.16,6998134.60],
-            [-1068083.20,6998160.44],[-1068104.27,6998205.37],
-            [-1068070.63,6998255.22],[-1068014.44,6998291.54],
-            [-1067952.33,6998351.85],[-1067927.93,6998386.93],
-            [-1067907.97,6998396.78],[-1067889.86,6998406.63],
-            [-1067848.08,6998495.26],[-1067832.92,6998521.11]]],
-            "spatialReference":{"latestWkid":3857,"wkid":102100}}"""
-
-    val road2CoordinatesJson = """{"paths":[[[-1067999.28,6998061.97],[-1067994.48,6998086.59],
-            [-1067964.53,6998125.37],[-1067952.70,6998215.84],
-            [-1067923.13,6998347.54],[-1067903.90,6998391.86],
-            [-1067895.40,6998422.02],[-1067891.70,6998460.18],
-            [-1067889.49,6998483.56],[-1067880.98,6998527.26]]],
-            "spatialReference":{"latestWkid":3857,"wkid":102100}}"""
-
-    val boundaryCoordinatesJson = """{ "rings": [[[-1067943.67,6998403.86],[-1067938.17,6998427.60],
-            [-1067898.77,6998415.86],[-1067888.26,6998398.80],
-            [-1067800.85,6998372.93],[-1067799.61,6998342.81],
-            [-1067809.38,6998330.00],[-1067817.07,6998307.85],
-            [-1067838.07,6998285.34],[-1067849.10,6998250.38],
-            [-1067874.02,6998256.00],[-1067879.87,6998235.95],
-            [-1067913.41,6998245.03],[-1067934.84,6998291.34],
-            [-1067948.41,6998251.90],[-1067961.18,6998186.68],
-            [-1068008.59,6998199.49],[-1068052.89,6998225.45],
-            [-1068039.37,6998261.11],[-1068064.12,6998265.26],
-            [-1068043.32,6998299.88],[-1068036.25,6998327.93],
-            [-1068004.43,6998409.28],[-1067943.67,6998403.86]]],
-            "spatialReference":{"latestWkid":3857,"wkid":102100}}"""
+    // fill styles to be used for graphics
+    private val pointSymbol = SimpleMarkerSymbol(
+        style = SimpleMarkerSymbolStyle.Square,
+        color = Color.red,
+        size = 10f
+    )
+    private val multiPointSymbol = SimpleMarkerSymbol(
+        style = SimpleMarkerSymbolStyle.Circle,
+        color = Color.yellow,
+        size = 5f
+    )
+    private val polylineSymbol =  SimpleLineSymbol(
+        style = SimpleLineSymbolStyle.Solid,
+        color = Color.blue,
+        width = 2f
+    )
+    private val polygonLineSymbol = SimpleLineSymbol(
+        style = SimpleLineSymbolStyle.Dash,
+        color = Color.black,
+        width = 1f
+    )
+    private val polygonSymbol = SimpleFillSymbol(
+        style = SimpleFillSymbolStyle.Solid,
+        color = Color.fromRgba(r = 255, g = 0, b = 0, a = 100),
+        outline = polygonLineSymbol
+    )
 
     init {
         viewModelScope.launch {
@@ -156,32 +144,32 @@ class CreateAndEditGeometriesViewModel(application: Application) : AndroidViewMo
                 // create graphics for the initial geometries and add them to the graphics overlay
                 graphicsOverlay.graphics.add(
                     Graphic(
-                        geometry = Geometry.fromJsonOrNull(houseCoordinatesJson) as Point,
-                        symbol = geometryEditorStyle.vertexSymbol
+                        geometry = Geometry.fromJsonOrNull(houseCoordinatesJson),
+                        symbol = pointSymbol
                     )
                 )
                 graphicsOverlay.graphics.add(
                     Graphic(
                         geometry = Geometry.fromJsonOrNull(outbuildingCoordinatesJson),
-                        symbol = geometryEditorStyle.midVertexSymbol
+                        symbol = multiPointSymbol
                     )
                 )
                 graphicsOverlay.graphics.add(
                     Graphic(
                         geometry = Geometry.fromJsonOrNull(road1CoordinatesJson),
-                        symbol = geometryEditorStyle.lineSymbol
+                        symbol = polylineSymbol
                     )
                 )
                 graphicsOverlay.graphics.add(
                     Graphic(
                         geometry = Geometry.fromJsonOrNull(road2CoordinatesJson),
-                        symbol = geometryEditorStyle.lineSymbol
+                        symbol = polylineSymbol
                     )
                 )
                 graphicsOverlay.graphics.add(
                     Graphic(
                         geometry = Geometry.fromJsonOrNull(boundaryCoordinatesJson),
-                        symbol = geometryEditorStyle.fillSymbol
+                        symbol = polygonSymbol
                     )
                 )
             }
@@ -295,14 +283,10 @@ class CreateAndEditGeometriesViewModel(application: Application) : AndroidViewMo
 
         // give the graphic an appropriate fill based on the geometry type
         when (geometry) {
-            is Point, is Multipoint -> graphic.symbol = geometryEditorStyle.vertexSymbol
-            is Polyline -> graphic.symbol = geometryEditorStyle.lineSymbol
-            // give polygons a translucent fill to avoid obscuring geometries or features underneath
-            is Polygon -> graphic.symbol =
-                SimpleFillSymbol(
-                style = SimpleFillSymbolStyle.Solid,
-                color = Color(1694433280) // r = 255, g = 0, b = 0, a = 100
-            )
+            is Point -> graphic.symbol = pointSymbol
+            is Multipoint -> graphic.symbol = multiPointSymbol
+            is Polyline -> graphic.symbol = polylineSymbol
+            is Polygon -> graphic.symbol = polygonSymbol
             else -> {}
         }
         // add the graphic to the graphics overlay
@@ -343,5 +327,53 @@ class CreateAndEditGeometriesViewModel(application: Application) : AndroidViewMo
             }
         }
     }
+
+    // json formatted strings for initial geometries
+    private val houseCoordinatesJson = """{"x": -1067898.59, "y": 6998366.62,
+    "spatialReference": {"latestWkid":3857, "wkid":102100}}"""
+
+    private val outbuildingCoordinatesJson = """{"points":[[-1067984.26,6998346.28],[-1067966.80,6998244.84],
+            [-1067921.88,6998284.65],[-1067934.36,6998340.74],
+            [-1067917.93,6998373.97],[-1067828.30,6998355.28],
+            [-1067832.25,6998339.70],[-1067823.10,6998336.93],
+            [-1067873.22,6998386.78],[-1067896.72,6998244.49]],
+            "spatialReference":{"latestWkid":3857,"wkid":102100}}"""
+
+    private val road1CoordinatesJson = """{"paths":[[[-1068095.40,6998123.52],[-1068086.16,6998134.60],
+            [-1068083.20,6998160.44],[-1068104.27,6998205.37],
+            [-1068070.63,6998255.22],[-1068014.44,6998291.54],
+            [-1067952.33,6998351.85],[-1067927.93,6998386.93],
+            [-1067907.97,6998396.78],[-1067889.86,6998406.63],
+            [-1067848.08,6998495.26],[-1067832.92,6998521.11]]],
+            "spatialReference":{"latestWkid":3857,"wkid":102100}}"""
+
+    private val road2CoordinatesJson = """{"paths":[[[-1067999.28,6998061.97],[-1067994.48,6998086.59],
+            [-1067964.53,6998125.37],[-1067952.70,6998215.84],
+            [-1067923.13,6998347.54],[-1067903.90,6998391.86],
+            [-1067895.40,6998422.02],[-1067891.70,6998460.18],
+            [-1067889.49,6998483.56],[-1067880.98,6998527.26]]],
+            "spatialReference":{"latestWkid":3857,"wkid":102100}}"""
+
+    private val boundaryCoordinatesJson = """{ "rings": [[[-1067943.67,6998403.86],[-1067938.17,6998427.60],
+            [-1067898.77,6998415.86],[-1067888.26,6998398.80],
+            [-1067800.85,6998372.93],[-1067799.61,6998342.81],
+            [-1067809.38,6998330.00],[-1067817.07,6998307.85],
+            [-1067838.07,6998285.34],[-1067849.10,6998250.38],
+            [-1067874.02,6998256.00],[-1067879.87,6998235.95],
+            [-1067913.41,6998245.03],[-1067934.84,6998291.34],
+            [-1067948.41,6998251.90],[-1067961.18,6998186.68],
+            [-1068008.59,6998199.49],[-1068052.89,6998225.45],
+            [-1068039.37,6998261.11],[-1068064.12,6998265.26],
+            [-1068043.32,6998299.88],[-1068036.25,6998327.93],
+            [-1068004.43,6998409.28],[-1067943.67,6998403.86]]],
+            "spatialReference":{"latestWkid":3857,"wkid":102100}}"""
+
+    /**
+     * Define a blue color for polylines.
+     */
+    private val Color.Companion.blue: Color
+        get() {
+            return fromRgba(0, 0, 255, 255)
+        }
 
 }
