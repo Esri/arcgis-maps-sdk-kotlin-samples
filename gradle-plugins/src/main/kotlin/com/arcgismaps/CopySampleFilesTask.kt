00a24cd34ba54ae40e9bae0a51248bf92ad2b3e7
Shubham Sharma <shubhamsharma@esri.com>
2025-03-24 09:22:52 -0700

    Sample build JSON asset data (#332)
    Co-authored-by: TADraeseke <tdraeseke@esri.com>

--- gradle-plugins/src/main/kotlin/com/arcgismaps/CopySampleFilesTask.kt
+++ gradle-plugins/src/main/kotlin/com/arcgismaps/CopySampleFilesTask.kt
@@ -1,17 +1,23 @@
 package com.arcgismaps
 
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.file.DuplicatesStrategy
 import org.gradle.api.file.FileCollection
 import org.gradle.api.tasks.Copy
 import org.gradle.kotlin.dsl.register
+import java.io.File
 
 /**
  * Copy md, kt, and json files to a directory (with the name of the associated sample)
  * into the app's build assets directory.
  */
 class CopySampleFilesTask : Plugin<Project> {
+
+    private val json = Json { prettyPrint = true }
+
     override fun apply(project: Project) {
         project.tasks.register<Copy>("copyCodeFiles") {
             description = """
@@ -42,6 +48,58 @@ class CopySampleFilesTask : Plugin<Project> {
 
             // Place in the assets codeFiles folder of the app.
             into(outputDir)
+
+            // After the copy finishes, run JSON generation + cleanup
+            doLast {
+                // Create a JSON file that maps each sample folder’s files to their contents
+                createJsonAssetFile(outputDir)
+
+                // Delete everything but the JSON file itself
+                outputDir.listFiles()?.forEach { file ->
+                    if (file.isDirectory || file.name != "samples.json") {
+                        file.deleteRecursively()
+                    }
+                }
+            }
         }
+    }
+
+    /**
+     * Creates the main list of all samples as "samples.json" file in [outputDir] with structure:
+     *
+     * {
+     *   "sampleA": {
+     *     "MainActivity.kt": "...content...",
+     *     "README.md": "...content...",
+     *     ...
+     *   },
+     *   "sampleB": {
+     *      ...
+     *   }
+     * }
+     */
+    private fun createJsonAssetFile(outputDir: File) {
+        // This map will hold the structure: sampleDirName -> { filename -> fileContent }
+        val samplesMap = mutableMapOf<String, MutableMap<String, String>>()
+
+        // List subdirectories in outputDir (i.e., each sample folder)
+        outputDir.listFiles()
+            ?.filter { it.isDirectory }
+            ?.forEach { sampleDir ->
+                val filesMap = mutableMapOf<String, String>()
+                sampleDir.listFiles()?.forEach { file ->
+                    if (file.isFile) {
+                        filesMap[file.name] = file.readText()
+                    }
+                }
+                samplesMap[sampleDir.name] = filesMap
+            }
+
+        // Serialize using Kotlin Serialization with pretty printing
+        val jsonString = json.encodeToString(samplesMap)
+
+        // Write the JSON to a file named "samples.json" in outputDir
+        val jsonFile = File(outputDir, "samples.json")
+        jsonFile.writeText(jsonString)
     }
 }
