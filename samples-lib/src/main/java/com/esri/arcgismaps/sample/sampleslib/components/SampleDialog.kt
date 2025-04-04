e656637a773c63542537fd2ac6504814b63692ce
Shubham Sharma <shubhamsharma@esri.com>
2025-04-04 15:31:25 -0700

    Added SampleTemplateTypes FAB_DialogOptions

---
+++ samples-lib/src/main/java/com/esri/arcgismaps/sample/sampleslib/components/SampleDialog.kt
@@ -0,0 +1,89 @@
+/* Copyright 2025 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.esri.arcgismaps.sample.sampleslib.components
+
+import android.content.res.Configuration
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+
+/**
+ * A simplified implementation of the [Dialog] with layout all the dialog UI
+ * style configurations to display the [content] using the default [properties].
+ */
+@Composable
+fun SampleDialog(
+    modifier: Modifier = Modifier,
+    properties: DialogProperties = DialogProperties(),
+    onDismissRequest: () -> Unit,
+    content: @Composable (ColumnScope) -> Unit
+) {
+    Dialog(onDismissRequest = onDismissRequest, properties = properties) {
+        Column(
+            modifier = modifier
+                .clip(RoundedCornerShape(12.dp))
+                .background(MaterialTheme.colorScheme.background)
+                .padding(12.dp)
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState())
+                .animateContentSize(),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            content(this)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun DialogOptionsPreview() {
+    SamplePreviewSurface {
+        SampleDialog(onDismissRequest = {}) {
+            Text("Sample options: ", style = MaterialTheme.typography.titleMedium)
+            DropDownMenuBox(
+                textFieldValue = "Current selection",
+                textFieldLabel = "Select an option",
+                dropDownItemList = emptyList(),
+                onIndexSelected = { }
+            )
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+                OutlinedButton(onClick = { }) { Text("Dismiss") }
+            }
+        }
+    }
+}
