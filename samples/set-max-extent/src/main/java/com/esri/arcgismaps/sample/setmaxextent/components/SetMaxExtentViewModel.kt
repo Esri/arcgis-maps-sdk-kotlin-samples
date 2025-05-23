03ae28d6b8ddd0fc6cd437fd03c44292fda0d61d
rom14514 <rsavitski@esri.com>
2025-05-23 16:18:56 +0100

    create new set-max-extent sample

---
+++ samples/set-max-extent/src/main/java/com/esri/arcgismaps/sample/setmaxextent/components/SetMaxExtentViewModel.kt
@@ -0,0 +1,39 @@
+/* Copyright 2025 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.esri.arcgismaps.sample.setmaxextent.components
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.arcgismaps.mapping.ArcGISMap
+import com.arcgismaps.mapping.BasemapStyle
+import com.esri.arcgismaps.sample.sampleslib.components.MessageDialogViewModel
+import kotlinx.coroutines.launch
+
+class SetMaxExtentViewModel(app: Application) : AndroidViewModel(app) {
+
+    val arcGISMap = ArcGISMap(BasemapStyle.ArcGISNavigationNight)
+
+    // Create a message dialog view model for handling error messages
+    val messageDialogVM = MessageDialogViewModel()
+
+    init {
+        viewModelScope.launch {
+            arcGISMap.load().onFailure { messageDialogVM.showMessageDialog(it) }
+        }
+    }
+}
