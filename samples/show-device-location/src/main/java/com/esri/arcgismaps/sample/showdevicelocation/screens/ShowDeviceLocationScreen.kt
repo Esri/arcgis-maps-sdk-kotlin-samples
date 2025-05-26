4668dcd7b431d061e097851bb3ac52f4bd8f11fb
rom14514 <rsavitski@esri.com>
2025-05-26 14:42:46 +0100

    add display location

--- samples/show-device-location/src/main/java/com/esri/arcgismaps/sample/showdevicelocation/screens/ShowDeviceLocationScreen.kt
+++ samples/show-device-location/src/main/java/com/esri/arcgismaps/sample/showdevicelocation/screens/ShowDeviceLocationScreen.kt
@@ -16,18 +16,32 @@
 
 package com.esri.arcgismaps.sample.showdevicelocation.screens
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
+import com.arcgismaps.ArcGISEnvironment
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.arcgismaps.location.LocationDisplayAutoPanMode
 import com.arcgismaps.toolkit.geoviewcompose.MapView
+import com.arcgismaps.toolkit.geoviewcompose.rememberLocationDisplay
 import com.esri.arcgismaps.sample.showdevicelocation.components.ShowDeviceLocationViewModel
 import com.esri.arcgismaps.sample.sampleslib.components.MessageDialog
 import com.esri.arcgismaps.sample.sampleslib.components.SampleTopAppBar
+import kotlinx.coroutines.launch
 
 /**
  * Main screen layout for the sample app
@@ -35,6 +49,35 @@ import com.esri.arcgismaps.sample.sampleslib.components.SampleTopAppBar
 @Composable
 fun ShowDeviceLocationScreen(sampleName: String) {
     val mapViewModel: ShowDeviceLocationViewModel = viewModel()
+
+    val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+
+    // some parts of the API require an Android Context to properly interact with Android system
+    // features, such as LocationProvider and application resources
+    ArcGISEnvironment.applicationContext = context.applicationContext
+
+    // Create and remember a location display with a recenter auto pan mode.
+    val locationDisplay = rememberLocationDisplay().apply {
+        setAutoPanMode(LocationDisplayAutoPanMode.Recenter)
+    }
+
+    if (checkPermissions(context)) {
+        // Permissions are already granted.
+        LaunchedEffect(Unit) {
+            locationDisplay.dataSource.start()
+        }
+    } else {
+        RequestPermissions(
+            context = context,
+            onPermissionsGranted = {
+                coroutineScope.launch {
+                    locationDisplay.dataSource.start()
+                }
+            }
+        )
+    }
+
     Scaffold(
         topBar = { SampleTopAppBar(title = sampleName) },
         content = {
@@ -47,9 +90,9 @@ fun ShowDeviceLocationScreen(sampleName: String) {
                     modifier = Modifier
                         .fillMaxSize()
                         .weight(1f),
-                    arcGISMap = mapViewModel.arcGISMap
+                    arcGISMap = mapViewModel.arcGISMap,
+                    locationDisplay = locationDisplay,
                 )
-                // TODO: Add UI components in this Column ...
             }
 
             mapViewModel.messageDialogVM.apply {
@@ -63,4 +106,53 @@ fun ShowDeviceLocationScreen(sampleName: String) {
             }
         }
     )
+}
+
+
+@Composable
+fun RequestPermissions(context: Context, onPermissionsGranted: () -> Unit) {
+
+    // Create an activity result launcher using permissions contract and handle the result.
+    val activityResultLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        // Check if both fine & coarse location permissions are true.
+        if (permissions.all { it.value }) {
+            onPermissionsGranted()
+        } else {
+            showError(context, "Location permissions were denied")
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        activityResultLauncher.launch(
+            // Request both fine and coarse location permissions.
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        )
+    }
+}
+
+
+fun checkPermissions(context: Context): Boolean {
+    // Check permissions to see if both permissions are granted.
+    // Coarse location permission.
+    val permissionCheckCoarseLocation = ContextCompat.checkSelfPermission(
+        context,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+    // Fine location permission.
+    val permissionCheckFineLocation = ContextCompat.checkSelfPermission(
+        context,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    return permissionCheckCoarseLocation && permissionCheckFineLocation
+}
+
+
+fun showError(context: Context, message: String) {
+    Toast.makeText(context, message, Toast.LENGTH_LONG).show()
 }
