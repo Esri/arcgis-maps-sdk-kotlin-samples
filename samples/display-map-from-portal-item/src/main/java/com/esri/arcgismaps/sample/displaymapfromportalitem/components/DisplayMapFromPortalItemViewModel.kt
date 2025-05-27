2d68b746d71ed2f0e6944598e386a13cf50db681
Trevor Draeseke <trev8939@esri.com>
2025-05-27 15:18:10 -0700

    finished sample

--- samples/display-map-from-portal-item/src/main/java/com/esri/arcgismaps/sample/displaymapfromportalitem/components/DisplayMapFromPortalItemViewModel.kt
+++ samples/display-map-from-portal-item/src/main/java/com/esri/arcgismaps/sample/displaymapfromportalitem/components/DisplayMapFromPortalItemViewModel.kt
@@ -1,46 +1,81 @@
-/* Copyright 2025 Esri
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 package com.esri.arcgismaps.sample.displaymapfromportalitem.components
 
 import android.app.Application
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.arcgismaps.mapping.ArcGISMap
-import com.arcgismaps.mapping.BasemapStyle
-import com.arcgismaps.mapping.Viewpoint
+import com.arcgismaps.mapping.PortalItem
+import com.arcgismaps.portal.Portal
 import com.esri.arcgismaps.sample.sampleslib.components.MessageDialogViewModel
 import kotlinx.coroutines.launch
 
+/**
+ * ViewModel for displaying a map from a portal item.
+ */
 class DisplayMapFromPortalItemViewModel(app: Application) : AndroidViewModel(app) {
-    //TODO - delete mutable state when the map does not change or the screen does not need to observe changes
-    val arcGISMap by mutableStateOf(
-        ArcGISMap(BasemapStyle.ArcGISNavigationNight).apply {
-            initialViewpoint = Viewpoint(39.8, -98.6, 10e7)
+    /**
+     * Data class representing a portal item map option.
+     */
+    data class PortalItemMap(
+        val title: String,
+        val itemId: String
+    ) {
+        val portalItem: PortalItem by lazy {
+            PortalItem(
+                portal = Portal.arcGISOnline(Portal.Connection.Anonymous),
+                itemId = itemId
+            )
         }
+    }
+
+    // List of available portal item map options (matching Swift sample)
+    val mapOptions = listOf(
+        PortalItemMap(
+            title = "Terrestrial Ecosystems of the World",
+            itemId = "5be0bc3ee36c4e058f7b3cebc21c74e6"
+        ),
+        PortalItemMap(
+            title = "Recent Hurricanes, Cyclones and Typhoons",
+            itemId = "064f2e898b094a17b84e4a4cd5e5f549"
+        ),
+        PortalItemMap(
+            title = "Geology of United States",
+            itemId = "92ad152b9da94dee89b9e387dfe21acd"
+        )
     )
 
-    // Create a message dialog view model for handling error messages
+    // The currently selected portal item map option
+    var currentMapOption by mutableStateOf(mapOptions.first())
+        private set
+
+    // The ArcGISMap to display
+    var arcGISMap by mutableStateOf(
+        ArcGISMap(item = mapOptions.first().portalItem)
+    )
+        private set
+
+    // Message dialog for error handling
     val messageDialogVM = MessageDialogViewModel()
 
     init {
         viewModelScope.launch {
             arcGISMap.load().onFailure { messageDialogVM.showMessageDialog(it) }
+        }
+    }
+
+    /**
+     * Updates the map to display the selected portal item map option.
+     */
+    fun selectMapOption(option: PortalItemMap) {
+        if (option != currentMapOption) {
+            currentMapOption = option
+            arcGISMap = ArcGISMap(item = option.portalItem)
+            viewModelScope.launch {
+                arcGISMap.load().onFailure { messageDialogVM.showMessageDialog(it) }
+            }
         }
     }
 }
--- samples/display-map-from-portal-item/src/main/java/com/esri/arcgismaps/sample/displaymapfromportalitem/screens/DisplayMapFromPortalItemScreen.kt
+++ samples/display-map-from-portal-item/src/main/java/com/esri/arcgismaps/sample/displaymapfromportalitem/screens/DisplayMapFromPortalItemScreen.kt
@@ -1,47 +1,38 @@
-/* Copyright 2025 Esri
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 package com.esri.arcgismaps.sample.displaymapfromportalitem.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.arcgismaps.toolkit.geoviewcompose.MapView
 import com.esri.arcgismaps.sample.displaymapfromportalitem.components.DisplayMapFromPortalItemViewModel
+import com.esri.arcgismaps.sample.sampleslib.components.DropDownMenuBox
 import com.esri.arcgismaps.sample.sampleslib.components.MessageDialog
 import com.esri.arcgismaps.sample.sampleslib.components.SampleTopAppBar
 
 /**
- * Main screen layout for the sample app
+ * Main screen layout for the sample app.
  */
 @Composable
 fun DisplayMapFromPortalItemScreen(sampleName: String) {
     val mapViewModel: DisplayMapFromPortalItemViewModel = viewModel()
+    val mapOptions = mapViewModel.mapOptions
+    val currentMapOption = mapViewModel.currentMapOption
+
     Scaffold(
         topBar = { SampleTopAppBar(title = sampleName) },
-        content = {
+        content = { padding ->
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(it),
+                    .padding(padding),
             ) {
                 MapView(
                     modifier = Modifier
@@ -49,7 +40,11 @@ fun DisplayMapFromPortalItemScreen(sampleName: String) {
                         .weight(1f),
                     arcGISMap = mapViewModel.arcGISMap
                 )
-                // TODO: Add UI components in this Column ...
+                MapSelectionDropDown(
+                    mapOptions = mapOptions,
+                    selectedOption = currentMapOption,
+                    onOptionSelected = mapViewModel::selectMapOption
+                )
             }
 
             mapViewModel.messageDialogVM.apply {
@@ -63,4 +58,29 @@ fun DisplayMapFromPortalItemScreen(sampleName: String) {
             }
         }
     )
+}
+
+@Composable
+private fun MapSelectionDropDown(
+    mapOptions: List<DisplayMapFromPortalItemViewModel.PortalItemMap>,
+    selectedOption: DisplayMapFromPortalItemViewModel.PortalItemMap,
+    onOptionSelected: (DisplayMapFromPortalItemViewModel.PortalItemMap) -> Unit
+) {
+    val titles = remember(mapOptions) { mapOptions.map { it.title } }
+    val selectedIndex = mapOptions.indexOf(selectedOption).coerceAtLeast(0)
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        DropDownMenuBox(
+            textFieldValue = titles.getOrNull(selectedIndex) ?: "",
+            textFieldLabel = "Maps",
+            dropDownItemList = titles,
+            onIndexSelected = { index ->
+                mapOptions.getOrNull(index)?.let { onOptionSelected(it) }
+            }
+        )
+    }
 }
