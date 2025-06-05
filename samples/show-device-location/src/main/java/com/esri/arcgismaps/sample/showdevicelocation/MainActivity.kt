955a977b06f2abb4a4ad2cb1febb20f7ed25a15d
Roman Savitski <rsavitski@esri.com>
2025-06-05 17:11:23 +0100

    Migrate Show Device Location to Compose (#363)
    Co-authored-by: hud10837 <hmiears@esri.com>
    Co-authored-by: Gunther Heppner <gheppner@esri.com>

--- samples/show-device-location/src/main/java/com/esri/arcgismaps/sample/showdevicelocation/MainActivity.kt
+++ samples/show-device-location/src/main/java/com/esri/arcgismaps/sample/showdevicelocation/MainActivity.kt
@@ -1,4 +1,4 @@
-/* Copyright 2022 Esri
+/* Copyright 2025 Esri
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,168 +16,38 @@
 
 package com.esri.arcgismaps.sample.showdevicelocation
 
-import android.Manifest.permission.ACCESS_COARSE_LOCATION
-import android.Manifest.permission.ACCESS_FINE_LOCATION
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.lifecycleScope
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import com.arcgismaps.ApiKey
 import com.arcgismaps.ArcGISEnvironment
-import com.arcgismaps.location.LocationDisplayAutoPanMode
-import com.arcgismaps.mapping.ArcGISMap
-import com.arcgismaps.mapping.BasemapStyle
-import com.esri.arcgismaps.sample.showdevicelocation.databinding.ShowDeviceLocationActivityMainBinding
-import com.google.android.material.snackbar.Snackbar
-import kotlinx.coroutines.launch
+import com.esri.arcgismaps.sample.sampleslib.theme.SampleAppTheme
+import com.esri.arcgismaps.sample.showdevicelocation.screens.ShowDeviceLocationScreen
 
-
-class MainActivity : AppCompatActivity() {
-
-    // set up data binding for the activity
-    private val activityMainBinding: ShowDeviceLocationActivityMainBinding by lazy {
-        DataBindingUtil.setContentView(this, R.layout.show_device_location_activity_main)
-    }
-
-    private val mapView by lazy {
-        activityMainBinding.mapView
-    }
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         // authentication with an API key or named user is
         // required to access basemaps and other location services
         ArcGISEnvironment.apiKey = ApiKey.create(BuildConfig.ACCESS_TOKEN)
-        // some parts of the API require an Android Context to properly interact with Android system
-        // features, such as LocationProvider and application resources
-        ArcGISEnvironment.applicationContext = applicationContext
 
-        lifecycle.addObserver(mapView)
-
-        // create and add a map with a navigation night basemap style
-        val map = ArcGISMap(BasemapStyle.ArcGISNavigationNight)
-        mapView.map = map
-
-        val locationDisplay = mapView.locationDisplay
-
-        lifecycleScope.launch {
-            // listen to changes in the status of the location data source
-            locationDisplay.dataSource.start()
-                .onSuccess {
-                    // permission already granted, so start the location display
-                    activityMainBinding.spinner.setSelection(1, true)
-                }.onFailure {
-                    // check permissions to see if failure may be due to lack of permissions
-                    requestPermissions()
-                }
-            }
-
-        // populate the list for the location display options for the spinner's adapter
-        val panModeSpinnerElements = arrayListOf(
-            ItemData("Stop", R.drawable.locationdisplaydisabled),
-            ItemData("On", R.drawable.locationdisplayon),
-            ItemData("Re-center", R.drawable.locationdisplayrecenter),
-            ItemData("Navigation", R.drawable.locationdisplaynavigation),
-            ItemData("Compass", R.drawable.locationdisplayheading)
-        )
-
-        activityMainBinding.spinner.apply {
-            adapter = SpinnerAdapter(this@MainActivity, R.id.locationTextView, panModeSpinnerElements)
-            onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(
-                    parent: AdapterView<*>?,
-                    view: View,
-                    position: Int,
-                    id: Long
-                ) {
-                    when (panModeSpinnerElements[position].text) {
-                        "Stop" ->  // stop location display
-                            lifecycleScope.launch {
-                                locationDisplay.dataSource.stop()
-                            }
-                        "On" ->  // start location display
-                            lifecycleScope.launch {
-                                locationDisplay.dataSource.start()
-                            }
-                        "Re-center" -> {
-                            // re-center MapView on location
-                            locationDisplay.setAutoPanMode(LocationDisplayAutoPanMode.Recenter)
-                        }
-                        "Navigation" -> {
-                            // start navigation mode
-                            locationDisplay.setAutoPanMode(LocationDisplayAutoPanMode.Navigation)
-                        }
-                        "Compass" -> {
-                            // start compass navigation mode
-                            locationDisplay.setAutoPanMode(LocationDisplayAutoPanMode.CompassNavigation)
-                        }
-                    }
-                }
-
-                override fun onNothingSelected(parent: AdapterView<*>?) {}
+        setContent {
+            SampleAppTheme {
+                ShowDeviceLocationApp()
             }
         }
     }
 
-    /**
-     * Request fine and coarse location permissions for API level 23+.
-     */
-    private fun requestPermissions() {
-        // coarse location permission
-        val permissionCheckCoarseLocation =
-            ContextCompat.checkSelfPermission(this@MainActivity, ACCESS_COARSE_LOCATION) ==
-                    PackageManager.PERMISSION_GRANTED
-        // fine location permission
-        val permissionCheckFineLocation =
-            ContextCompat.checkSelfPermission(this@MainActivity, ACCESS_FINE_LOCATION) ==
-                    PackageManager.PERMISSION_GRANTED
-
-        // if permissions are not already granted, request permission from the user
-        if (!(permissionCheckCoarseLocation && permissionCheckFineLocation)) {
-            ActivityCompat.requestPermissions(
-                this@MainActivity,
-                arrayOf(ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION),
-                2
+    @Composable
+    private fun ShowDeviceLocationApp() {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            ShowDeviceLocationScreen(
+                sampleName = getString(R.string.show_device_location_app_name)
             )
-        } else {
-            // permission already granted, so start the location display
-            lifecycleScope.launch {
-                mapView.locationDisplay.dataSource.start().onSuccess {
-                    activityMainBinding.spinner.setSelection(1, true)
-                }
-            }
-        }
-    }
-
-    /**
-     * Handle the permissions request response.
-     */
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            lifecycleScope.launch {
-                mapView.locationDisplay.dataSource.start().onSuccess {
-                    activityMainBinding.spinner.setSelection(1, true)
-                }
-            }
-        } else {
-            Snackbar.make(
-                mapView,
-                "Location permissions required to run this sample!",
-                Snackbar.LENGTH_LONG
-            ).show()
-            // update UI to reflect that the location display did not actually start
-            activityMainBinding.spinner.setSelection(0, true)
         }
     }
 }
