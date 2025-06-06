5e65ec31e50f671d1e93691cd4e66064f46a5bcc
Trevor Draeseke <trev8939@esri.com>
2025-06-06 11:54:19 -0700

    revert to initial commit

--- samples/augment-reality-to-show-hidden-infrastructure/src/main/java/com/esri/arcgismaps/sample/augmentrealitytoshowhiddeninfrastructure/components/AugmentRealityToShowHiddenInfrastructureViewModel.kt
+++ samples/augment-reality-to-show-hidden-infrastructure/src/main/java/com/esri/arcgismaps/sample/augmentrealitytoshowhiddeninfrastructure/components/AugmentRealityToShowHiddenInfrastructureViewModel.kt
@@ -1,159 +1,46 @@
+/* Copyright 2025 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.esri.arcgismaps.sample.augmentrealitytoshowhiddeninfrastructure.components
 
 import android.app.Application
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
-import com.arcgismaps.Color
-import com.arcgismaps.geometry.Polyline
-import com.arcgismaps.location.LocationDisplayAutoPanMode
 import com.arcgismaps.mapping.ArcGISMap
 import com.arcgismaps.mapping.BasemapStyle
 import com.arcgismaps.mapping.Viewpoint
-import com.arcgismaps.mapping.symbology.SimpleLineSymbol
-import com.arcgismaps.mapping.symbology.SimpleLineSymbolStyle
-import com.arcgismaps.mapping.view.Graphic
-import com.arcgismaps.mapping.view.GraphicsOverlay
-import com.arcgismaps.mapping.view.LocationDisplay
-import com.arcgismaps.mapping.view.geometryeditor.FreehandTool
-import com.arcgismaps.mapping.view.geometryeditor.GeometryEditor
-import com.arcgismaps.toolkit.geoviewcompose.MapViewProxy
 import com.esri.arcgismaps.sample.sampleslib.components.MessageDialogViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class AugmentRealityToShowHiddenInfrastructureViewModel(app: Application) : AndroidViewModel(app) {
-    // Map and overlays
-    val arcGISMap = ArcGISMap(BasemapStyle.ArcGISImagery).apply {
-        initialViewpoint = Viewpoint(39.8, -98.6, 10e7)
-    }
-    val pipesGraphicsOverlay = GraphicsOverlay().apply {
-        renderer = SimpleLineSymbol(
-            style = SimpleLineSymbolStyle.Solid,
-            color = Color.red,
-            width = 2f
-        ).let { renderer ->
-            com.arcgismaps.mapping.symbology.SimpleRenderer(renderer)
+    //TODO - delete mutable state when the map does not change or the screen does not need to observe changes
+    val arcGISMap by mutableStateOf(
+        ArcGISMap(BasemapStyle.ArcGISNavigationNight).apply {
+            initialViewpoint = Viewpoint(39.8, -98.6, 10e7)
         }
-    }
-    val mapViewProxy = MapViewProxy()
-
-    // Location display
-    val locationDisplay = LocationDisplay().apply {
-        setAutoPanMode(LocationDisplayAutoPanMode.Recenter)
-        initialZoomScale = 1000.0
-    }
-
-    // Geometry editor for drawing pipes
-    val geometryEditor = GeometryEditor().apply {
-        tool = FreehandTool()
-    }
-
-    val pipeSymbol = SimpleLineSymbol(
-        style = SimpleLineSymbolStyle.Dash,
-        color = Color.red,
-        width = 2f
     )
 
-    // State for UI
-    private val _statusMessage = MutableStateFlow("Tap the map to add pipe points.")
-    val statusMessage: StateFlow<String> = _statusMessage.asStateFlow()
-
-    private val _canDelete = MutableStateFlow(false)
-    val canDelete: StateFlow<Boolean> = _canDelete.asStateFlow()
-
-    private val _canApplyEdits = MutableStateFlow(false)
-    val canApplyEdits: StateFlow<Boolean> = _canApplyEdits.asStateFlow()
-
-    private val _geometryEditorCanUndo = MutableStateFlow(false)
-    val geometryEditorCanUndo: StateFlow<Boolean> = _geometryEditorCanUndo.asStateFlow()
-
-    // For elevation dialog
-    var isElevationDialogVisible by mutableStateOf(false)
-    var elevationInput by mutableStateOf(0.0)
-
-    // Store elevation offset for each pipe
-    private val pipeElevationOffsets = mutableListOf<Double>()
-
-    // Error dialog
+    // Create a message dialog view model for handling error messages
     val messageDialogVM = MessageDialogViewModel()
 
     init {
         viewModelScope.launch {
             arcGISMap.load().onFailure { messageDialogVM.showMessageDialog(it) }
-            // Start location display
-            locationDisplay.dataSource.start().onFailure {
-                messageDialogVM.showMessageDialog("Failed to start location display", it.message ?: "")
-            }
         }
-        viewModelScope.launch {
-            // Listen for geometry editor geometry changes
-            geometryEditor.geometry.collect {
-                val polyline = it as? Polyline
-                val canApply = polyline?.parts?.any { part -> part.points.toList().size >= 2 } == true
-                _canApplyEdits.value = canApply
-                if (canApply) {
-                    _statusMessage.value = "Tap the check mark to add the pipe."
-                }
-                _geometryEditorCanUndo.value = geometryEditor.canUndo.value
-            }
-        }
-    }
-
-    fun startDrawingPipe() {
-       // geometryEditor.start()
-        _statusMessage.value = "Tap the map to add pipe points."
-        _canApplyEdits.value = false
-        _geometryEditorCanUndo.value = false
-    }
-
-    fun undoOrDelete() {
-        if (geometryEditor.canUndo.value) {
-            geometryEditor.undo()
-        } else {
-            removeAllGraphics()
-        }
-    }
-
-    fun removeAllGraphics() {
-        pipesGraphicsOverlay.graphics.clear()
-        pipeElevationOffsets.clear()
-        _canDelete.value = false
-        _statusMessage.value = "Tap the map to add pipe points."
-    }
-
-    fun applyPipe() {
-        isElevationDialogVisible = true
-    }
-
-    fun confirmElevationInput(elevation: Double) {
-        val geometry = geometryEditor.stop() as? Polyline ?: return
-        val pipeGraphic = Graphic(geometry)
-        pipesGraphicsOverlay.graphics.add(pipeGraphic)
-        pipeElevationOffsets.add(elevation)
-        _canDelete.value = true
-        if (elevation < 0) {
-            _statusMessage.value = "Pipe added ${elevation} meter(s) below surface.\nTap the camera to view the pipe(s) in AR."
-        } else if (elevation == 0.0) {
-            _statusMessage.value = "Pipe added at ground level.\nTap the camera to view the pipe(s) in AR."
-        } else {
-            _statusMessage.value = "Pipe added ${elevation} meter(s) above surface.\nTap the camera to view the pipe(s) in AR."
-        }
-        startDrawingPipe()
-        isElevationDialogVisible = false
-    }
-
-    fun cancelElevationInput() {
-        //geometryEditor.start()
-        isElevationDialogVisible = false
-    }
-
-    // For AR: Expose the list of pipe graphics and their elevation offsets
-    fun getPipeGraphicsWithElevations(): List<Pair<Graphic, Double>> {
-        return pipesGraphicsOverlay.graphics.zip(pipeElevationOffsets)
     }
 }
---
+++ samples/show-device-location/src/main/java/com/esri/arcgismaps/sample/showdevicelocation/SpinnerAdapter.kt
@@ -0,0 +1,62 @@
+package com.esri.arcgismaps.sample.showdevicelocation
+
+/* Copyright 2022 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import android.app.Activity
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.esri.arcgismaps.sample.showdevicelocation.databinding.SpinnerLayoutBinding
+
+/**
+ * Adapter to display both a string and icon beside each other in a spinner.
+ * Used here to populate the options for LocationDisplay.AutoPanMode.
+ */
+class SpinnerAdapter(
+    context: Activity,
+    id: Int,
+    private val list: ArrayList<ItemData>
+) :
+    ArrayAdapter<ItemData?>(context, id, list as List<ItemData?>) {
+    private val inflater: LayoutInflater =
+        context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    override fun getView(
+        position: Int,
+        convertView: View?,
+        parent: ViewGroup
+    ): View {
+        val spinnerLayoutBinding = SpinnerLayoutBinding.inflate(this.inflater)
+        val imageView = spinnerLayoutBinding.locationPointImageView
+        imageView.setImageResource(list[position].imageId)
+        val textView = spinnerLayoutBinding.locationTextView
+        textView.text = list[position].text
+        return spinnerLayoutBinding.root
+    }
+
+    override fun getDropDownView(
+        position: Int,
+        convertView: View?,
+        parent: ViewGroup
+    ): View {
+        return getView(position, convertView, parent)
+    }
+}
+
+data class ItemData(val text: String, val imageId: Int)
