60cdf356ed8dac15c8dd4cac64880f8b0cc291d9
Puneet Prakash <puneet_prakash@esri.com>
2025-06-10 14:03:19 -0700

    Init sample

---
+++ samples/apply-simple-renderer-to-graphics-overlay/README.md
@@ -0,0 +1,1 @@
+# Apply Simple Renderer To Graphics Overlay
---
+++ samples/apply-simple-renderer-to-graphics-overlay/src/main/java/com/esri/arcgismaps/sample/applysimplerenderertographicsoverlay/screens/ApplySimpleRendererToGraphicsOverlayScreen.kt
@@ -0,0 +1,66 @@
+/* Copyright 2025 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.esri.arcgismaps.sample.applysimplerenderertographicsoverlay.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.arcgismaps.toolkit.geoviewcompose.MapView
+import com.esri.arcgismaps.sample.applysimplerenderertographicsoverlay.components.ApplySimpleRendererToGraphicsOverlayViewModel
+import com.esri.arcgismaps.sample.sampleslib.components.MessageDialog
+import com.esri.arcgismaps.sample.sampleslib.components.SampleTopAppBar
+
+/**
+ * Main screen layout for the sample app
+ */
+@Composable
+fun ApplySimpleRendererToGraphicsOverlayScreen(sampleName: String) {
+    val mapViewModel: ApplySimpleRendererToGraphicsOverlayViewModel = viewModel()
+    Scaffold(
+        topBar = { SampleTopAppBar(title = sampleName) },
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(it),
+            ) {
+                MapView(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(1f),
+                    arcGISMap = mapViewModel.arcGISMap
+                )
+                // TODO: Add UI components in this Column ...
+            }
+
+            mapViewModel.messageDialogVM.apply {
+                if (dialogStatus) {
+                    MessageDialog(
+                        title = messageTitle,
+                        description = messageDescription,
+                        onDismissRequest = ::dismissDialog
+                    )
+                }
+            }
+        }
+    )
+}
