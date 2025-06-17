592c6ad873826360b32a259af624294f83b75fc2
Shelly Gill <sgill@esri.com>
2025-06-17 12:27:18 +0100

    Add vertical prec

--- samples-lib/src/main/java/com/esri/arcgismaps/sample/sampleslib/components/CustomSrUtils.kt
+++ samples-lib/src/main/java/com/esri/arcgismaps/sample/sampleslib/components/CustomSrUtils.kt
@@ -21,6 +21,7 @@ import com.arcgismaps.geometry.Multipoint
 import com.arcgismaps.geometry.Point
 import com.arcgismaps.geometry.Polygon
 import com.arcgismaps.geometry.Polyline
+import com.arcgismaps.geometry.SpatialReference
 import com.arcgismaps.geometry.SpatialReferenceBuilder
 
 class CustomSrUtils {
@@ -33,38 +34,57 @@ class CustomSrUtils {
          * @return A new [SpatialReference] with adjusted resolution and tolerance.
          */
         fun createCustomPrecisionGeometry(geometry: Geometry): Geometry {
-            //TODO - not dealt with z values
-
-            val srBuilder = SpatialReferenceBuilder(geometry.spatialReference?.wkid ?: 0)
-            srBuilder.resolution = srBuilder.resolution / 5
-            srBuilder.tolerance = srBuilder.tolerance / 5
-            val customSr = srBuilder.toSpatialReference()
-
+            val customSr = createCustomPrecisionSpatialReference(geometry.spatialReference!!, geometry.hasZ)
             var retVal: Geometry? = null
             when (geometry) {
                 is Point -> {
-                    retVal = Point(
-                        geometry.x, geometry.y,
-                        spatialReference = customSr
-                    )
+                    retVal = if (geometry.hasZ) {
+                        // If the point has Z, we need to create a new Point with the custom SR
+                        Point(
+                            geometry.x, geometry.y, geometry.z,
+                            spatialReference = customSr
+                        )
+                    } else {
+                        // If the point does not have Z, we can just use x and y
+                        Point(
+                            geometry.x, geometry.y,
+                            spatialReference = customSr
+                        )
+                    }
                 }
 
                 is Envelope -> {
-                    retVal = Envelope(
-                        geometry.xMin, geometry.yMin,
-                        geometry.xMax, geometry.yMax,
-                        spatialReference = customSr
-                    )
+                    retVal = if (geometry.hasZ) {
+                        // If the envelope has Z, we need to create a new Envelope with the custom SR
+                        Envelope(
+                            geometry.xMin, geometry.yMin, geometry.xMax, geometry.yMax,
+                            geometry.zMin, geometry.zMax,
+                            spatialReference = customSr
+                        )
+                    } else {
+                        // If the envelope does not have Z, we can just use xMin, yMin, xMax, yMax
+                        Envelope(
+                            geometry.xMin, geometry.yMin, geometry.xMax, geometry.yMax,
+                            spatialReference = customSr
+                        )
+                    }
                 }
 
                 else -> throw (Throwable("Unsupported geometry type"))
             }
 
-//            assertThat(retVal).isNotNull()
-//            assertThat(retVal!!.spatialReference).isNotNull()
-//            assertThat(retVal.spatialReference?.tolerance).isEqualTo(srBuilder.tolerance)
-
             return retVal
+        }
+
+        private fun createCustomPrecisionSpatialReference(spatialReference: SpatialReference, customVertical: Boolean): SpatialReference {
+            val srBuilder = SpatialReferenceBuilder(spatialReference.wkid, spatialReference.verticalWkid)
+            srBuilder.resolution = srBuilder.resolution / 5
+            srBuilder.tolerance = srBuilder.tolerance / 4
+            if (customVertical) {
+                srBuilder.verticalResolution = srBuilder.verticalResolution / 3
+                srBuilder.verticalTolerance = srBuilder.verticalTolerance / 2
+            }
+            return srBuilder.toSpatialReference()
         }
     }
 }
