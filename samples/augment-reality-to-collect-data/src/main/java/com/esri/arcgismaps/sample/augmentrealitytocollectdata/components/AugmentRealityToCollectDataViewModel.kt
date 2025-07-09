a8cacc9e813a31c8d84bd75cd2bec1dfd1992c1e
rom14514 <rsavitski@esri.com>
2025-07-09 17:43:37 +0100

    general improvements

--- samples/augment-reality-to-collect-data/src/main/java/com/esri/arcgismaps/sample/augmentrealitytocollectdata/components/AugmentRealityToCollectDataViewModel.kt
+++ samples/augment-reality-to-collect-data/src/main/java/com/esri/arcgismaps/sample/augmentrealitytocollectdata/components/AugmentRealityToCollectDataViewModel.kt
@@ -22,18 +22,16 @@ import android.util.Log
 import android.widget.Toast
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.arcgismaps.Color
 import com.arcgismaps.data.ServiceFeatureTable
 import com.arcgismaps.geometry.Point
-import com.arcgismaps.mapping.ArcGISMap
 import com.arcgismaps.mapping.ArcGISScene
 import com.arcgismaps.mapping.Basemap
 import com.arcgismaps.mapping.BasemapStyle
 import com.arcgismaps.mapping.ElevationSource
-import com.arcgismaps.mapping.Viewpoint
-import com.arcgismaps.mapping.layers.ArcGISSceneLayer
 import com.arcgismaps.mapping.layers.FeatureLayer
 import com.arcgismaps.mapping.symbology.SimpleMarkerSceneSymbol
 import com.arcgismaps.mapping.symbology.SimpleMarkerSceneSymbolStyle
@@ -46,9 +44,10 @@ import com.esri.arcgismaps.sample.sampleslib.components.MessageDialogViewModel
 import kotlinx.coroutines.launch
 
 class AugmentRealityToCollectDataViewModel(app: Application) : AndroidViewModel(app) {
-    val basemap = Basemap(BasemapStyle.ArcGISHumanGeography)
-    val featureTable = ServiceFeatureTable("https://services2.arcgis.com/ZQgQTuoyBrtmoGdP/arcgis/rest/services/AR_Tree_Survey/FeatureServer/0")
-    val featureLayer = FeatureLayer.createWithFeatureTable(featureTable)
+    private val basemap = Basemap(BasemapStyle.ArcGISHumanGeography)
+    // The AR tree survey service feature table
+    private val featureTable = ServiceFeatureTable("https://services2.arcgis.com/ZQgQTuoyBrtmoGdP/arcgis/rest/services/AR_Tree_Survey/FeatureServer/0")
+    private val featureLayer = FeatureLayer.createWithFeatureTable(featureTable)
     val arcGISScene = ArcGISScene(basemap).apply {
         // an elevation source is required for the scene to be placed at the correct elevation
         // if not used, the scene may appear far below the device position because the device position
@@ -60,6 +59,7 @@ class AugmentRealityToCollectDataViewModel(app: Application) : AndroidViewModel(
         operationalLayers.add(featureLayer)
     }
 
+    // The graphics overlay which shows marker symbols.
     val graphicsOverlay = GraphicsOverlay().apply {
         sceneProperties.surfacePlacement = SurfacePlacement.Absolute
     }
@@ -69,7 +69,11 @@ class AugmentRealityToCollectDataViewModel(app: Application) : AndroidViewModel(
     // Create a message dialog view model for handling error messages
     val messageDialogVM = MessageDialogViewModel()
 
-    private var marker : Graphic? = null
+    var isDialogOptionsVisible by mutableStateOf(false)
+        private set
+
+    // The current marker graphic representing the user's selection
+    private var treeMarker : Graphic? = null
 
     init {
         viewModelScope.launch {
@@ -77,11 +81,28 @@ class AugmentRealityToCollectDataViewModel(app: Application) : AndroidViewModel(
         }
     }
 
+    // Displays a dialog for adding tree data if a marker exists
+    fun showDialog(context: Context){
+        if (treeMarker == null) {
+            showToast(context, "Please create marker by tapping on the screen")
+            return
+        }
+        isDialogOptionsVisible = true
+    }
+
+    fun hideDialog(){
+        isDialogOptionsVisible = false
+    }
+
+    // Adds a marker to the graphics overlay based on a single tap event
     fun addMarker(singleTapConfirmedEvent: SingleTapConfirmedEvent) {
-        graphicsOverlay.graphics.removeFirstOrNull()
+        // Remove all graphics from the graphics overlay
+        graphicsOverlay.graphics.clear()
+        // Convert the screen coordinate of the tap event to a point in the base surface
         worldScaleSceneViewProxy
             .screenToBaseSurface(singleTapConfirmedEvent.screenCoordinate)
             ?.let { point ->
+                // Create a new marker graphic at the specified point with a diamond symbol
                 val newMarker = Graphic(
                     point,
                     SimpleMarkerSceneSymbol(
@@ -92,39 +113,35 @@ class AugmentRealityToCollectDataViewModel(app: Application) : AndroidViewModel(
                         depth = 1.0
                     )
                 )
-                marker = newMarker
+                treeMarker = newMarker
                 graphicsOverlay.graphics.add(newMarker)
             }
     }
 
+    // Adds a feature to represent a tree to the tree survey service feature table.
     fun addTree(context: Context, health: TreeHealth){
-        if (marker == null) {
-            showToast(context, "Please create marker by tapping on the screen")
-            return
-        }
-        marker?.let {
-            // set up the feature attributes
+        treeMarker?.let { marker ->
+            // Set up the feature attributes
             val featureAttributes = mapOf<String, Any>(
                 "Health" to health.value,
                 "Height" to 3.2,
                 "Diameter" to 1.2,
             )
 
-            // get Point
-            val point = (it.geometry as? Point)
-            if (point == null) {
+            // Retrieve the marker's geometry as a Point
+            val point = (marker.geometry as? Point) ?: run {
                 showToast(context, "Something went wrong")
                 return@let
             }
 
-            // create a new feature at the mapPoint
+            // Create a new feature at the point
             val feature = featureTable.createFeature(featureAttributes, point)
 
-            // add the feature to the feature table
+            // Add the feature to the feature table
             viewModelScope.launch {
                 featureTable.addFeature(feature)
                     .onSuccess {
-                        // Upload changes to the local table to the feature service.
+                        // Upload changes from the local feature table to the feature service
                         featureTable.applyEdits()
                             .onSuccess { showToast(context, "Successfully added tree data!")}
                             .onFailure { e -> showError(context, e) }
@@ -136,17 +153,22 @@ class AugmentRealityToCollectDataViewModel(app: Application) : AndroidViewModel(
     }
 }
 
+/**
+ * Represents the health status of a tree.
+ *
+ * @property value The numerical value associated with the health status.
+ */
 enum class TreeHealth(val value: Short){
     Dead(0),
     Distressed(5),
     Healthy(10),
 }
 
-fun showError(context: Context, e: Throwable){
+private fun showError(context: Context, e: Throwable){
     Log.d("AugmentRealityToCollectDataViewModel", e.message ?: e.toString())
     showToast(context, e.message ?: "Unknown error")
 }
 
-fun showToast(context: Context, message: String) {
+private fun showToast(context: Context, message: String) {
     Toast.makeText(context, message, Toast.LENGTH_LONG).show()
 }
--- samples/augment-reality-to-collect-data/src/main/java/com/esri/arcgismaps/sample/augmentrealitytocollectdata/screens/AugmentRealityToCollectDataScreen.kt
+++ samples/augment-reality-to-collect-data/src/main/java/com/esri/arcgismaps/sample/augmentrealitytocollectdata/screens/AugmentRealityToCollectDataScreen.kt
@@ -16,31 +16,29 @@
 
 package com.esri.arcgismaps.sample.augmentrealitytocollectdata.screens
 
-import android.content.Context
 import android.content.res.Configuration
-import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -50,23 +48,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.arcgismaps.Color
 import com.arcgismaps.LoadStatus
-import com.arcgismaps.mapping.symbology.SimpleMarkerSceneSymbol
-import com.arcgismaps.mapping.symbology.SimpleMarkerSceneSymbolStyle
-import com.arcgismaps.mapping.view.Graphic
 import com.arcgismaps.toolkit.ar.WorldScaleSceneView
 import com.arcgismaps.toolkit.ar.WorldScaleSceneViewStatus
 import com.arcgismaps.toolkit.ar.WorldScaleTrackingMode
 import com.arcgismaps.toolkit.ar.rememberWorldScaleSceneViewStatus
+import com.esri.arcgismaps.sample.augmentrealitytocollectdata.BuildConfig
 import com.esri.arcgismaps.sample.augmentrealitytocollectdata.components.AugmentRealityToCollectDataViewModel
 import com.esri.arcgismaps.sample.augmentrealitytocollectdata.components.TreeHealth
-import com.esri.arcgismaps.sample.sampleslib.components.DropDownMenuBox
 import com.esri.arcgismaps.sample.sampleslib.components.MessageDialog
 import com.esri.arcgismaps.sample.sampleslib.components.SampleDialog
 import com.esri.arcgismaps.sample.sampleslib.components.SamplePreviewSurface
 import com.esri.arcgismaps.sample.sampleslib.components.SampleTopAppBar
-import kotlin.enums.EnumEntries
 
 /**
  * Main screen layout for the sample app
@@ -74,19 +67,27 @@ import kotlin.enums.EnumEntries
 @Composable
 fun AugmentRealityToCollectDataScreen(sampleName: String) {
     val augmentedRealityViewModel: AugmentRealityToCollectDataViewModel = viewModel()
-    var isDialogOptionsVisible by remember { mutableStateOf(false) }
 
     var initializationStatus by rememberWorldScaleSceneViewStatus()
 
     val context = LocalContext.current
 
+    val hasNonDefaultAPIKey = BuildConfig.GOOGLE_API_KEY != "DEFAULT_GOOGLE_API_KEY"
+    // Initialize the world scale tracking mode based on whether a google API key is provided
+    val worldScaleTrackingMode = remember {
+        when {
+            hasNonDefaultAPIKey -> { WorldScaleTrackingMode.Geospatial() }
+            else -> { WorldScaleTrackingMode.World() }
+        }
+    }
+
     Scaffold(
         topBar = { SampleTopAppBar(title = sampleName) },
         floatingActionButton = {
-            if (!isDialogOptionsVisible) {
+            if (!augmentedRealityViewModel.isDialogOptionsVisible) {
                 FloatingActionButton(
                     modifier = Modifier.padding(bottom = 36.dp, end = 12.dp),
-                    onClick = { isDialogOptionsVisible = true }
+                    onClick = { augmentedRealityViewModel.showDialog(context) }
                 ) { Icon(Icons.Filled.Add, contentDescription = "Add tree") }
             }
         },
@@ -102,25 +103,31 @@ fun AugmentRealityToCollectDataScreen(sampleName: String) {
                     onInitializationStatusChanged = { status ->
                         initializationStatus = status
                     },
-                    worldScaleTrackingMode = WorldScaleTrackingMode.Geospatial(),
+                    worldScaleTrackingMode = worldScaleTrackingMode,
                     worldScaleSceneViewProxy = augmentedRealityViewModel.worldScaleSceneViewProxy,
                     graphicsOverlays = listOf(augmentedRealityViewModel.graphicsOverlay),
                     onSingleTapConfirmed = augmentedRealityViewModel::addMarker,
                 )
             }
 
-            if (isDialogOptionsVisible) {
-                DialogOptions(
-                    options = TreeHealth.entries,
+            if (augmentedRealityViewModel.isDialogOptionsVisible) {
+                TreeHealthDialog(
                     onOptionSelected = { selectedOption ->
                         augmentedRealityViewModel.addTree(context ,selectedOption)},
-                    onDismissRequest = { isDialogOptionsVisible = false }
+                    onDismissRequest = augmentedRealityViewModel::hideDialog
                 )
             }
 
             when (val status = initializationStatus) {
                 is WorldScaleSceneViewStatus.Initializing -> {
-                    TextWithScrim("Initializing AR...")
+                    // Display a message indicating the initialization status
+                    TextWithScrim(
+                        if (worldScaleTrackingMode is WorldScaleTrackingMode.Geospatial) {
+                            "Initializing AR in geospatial mode..."
+                        } else {
+                            "Initializing AR in world mode..."
+                        }
+                    )
                 }
 
                 is WorldScaleSceneViewStatus.Initialized -> {
@@ -142,7 +149,7 @@ fun AugmentRealityToCollectDataScreen(sampleName: String) {
                             TextWithScrim("Failed to load world scale AR scene: " + sceneLoadStatus.error)
                         }
 
-                        else -> {}
+                        is LoadStatus.Loaded -> {} // Display the main content of the AR scene once it has successfully loaded.
                     }
                 }
 
@@ -182,16 +189,19 @@ private fun TextWithScrim(text: String) {
     }
 }
 
+/**
+ * Displays a dialog for selecting the health status of a tree.
+ */
 @Composable
-fun DialogOptions(
-    options: EnumEntries<TreeHealth>,
+fun TreeHealthDialog(
     onOptionSelected: (TreeHealth) -> Unit,
     onDismissRequest: () -> Unit
 ) {
     SampleDialog(onDismissRequest = onDismissRequest) {
-        Text("Add Tree ", style = MaterialTheme.typography.titleMedium)
-        Text("How healthy is this tree?", style = MaterialTheme.typography.titleSmall)
-        options.forEach { option ->
+        Text("Add Tree ", style = MaterialTheme.typography.titleLarge)
+        Text("How healthy is this tree?", style = MaterialTheme.typography.titleMedium)
+        Spacer(modifier = Modifier.height(10.dp))
+        TreeHealth.entries.forEach { option ->
             Button(
                 onClick = {
                     onOptionSelected(option)
@@ -199,12 +209,16 @@ fun DialogOptions(
                 },
                 modifier = Modifier
                     .fillMaxWidth()
+                    .height(48.dp)
             ) {
-                Text(option.name)
+                Text(
+                    text = option.name,
+                    style = MaterialTheme.typography.bodyMedium
+                )
             }
         }
         Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
-            OutlinedButton(onClick = onDismissRequest) { Text("Dismiss") }
+            TextButton(onClick = onDismissRequest) { Text("Dismiss") }
         }
     }
 }
@@ -214,8 +228,7 @@ fun DialogOptions(
 @Composable
 fun DialogOptionsPreview() {
     SamplePreviewSurface {
-        DialogOptions(
-            options = TreeHealth.entries,
+        TreeHealthDialog(
             onOptionSelected = { _ -> },
             onDismissRequest = {}
         )
