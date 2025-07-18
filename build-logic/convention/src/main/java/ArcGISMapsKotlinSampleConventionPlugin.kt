869b3cbfb02b93db73ec7f49f723dde533115715
Shubham Sharma <shubhamsharma@esri.com>
2025-07-18 15:23:49 -0700

    resolve -D build flag issue

--- build-logic/convention/src/main/java/ArcGISMapsKotlinSampleConventionPlugin.kt
+++ build-logic/convention/src/main/java/ArcGISMapsKotlinSampleConventionPlugin.kt
@@ -9,14 +9,23 @@ class ArcGISMapsKotlinSampleConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             dependencies {
-                // ArcGIS Maps SDK for Kotlin
-                implementation(libs.findLibrary("arcgis-maps-kotlin").get())
-                // Get the Toolkit BOM
-                implementation(platform(libs.findLibrary("arcgis-maps-kotlin-toolkit-bom").get()))
-                // ArcGIS Maps SDK Toolkit GeoView Compose
-                implementation(libs.findLibrary("arcgis-maps-kotlin-toolkit-geoview-compose").get())
-                // Local project common samples library
-                implementation(project(":samples-lib"))
+                dependencies {
+                    // if a "build" property is set from the command line like: "-D build=200.X.X-XXXX"
+                    val buildVersion = System.getProperty("build")
+                    // Override version in libs.versions.toml file
+                    if (buildVersion != null) {
+                        implementation("com.esri:arcgis-maps-kotlin:$buildVersion")
+                        implementation(platform("com.esri:arcgis-maps-kotlin-toolkit-bom:$buildVersion"))
+                        implementation("com.esri:arcgis-maps-kotlin-toolkit-geoview-compose")
+                    } else {
+                        // Use version catalog when no build flag is provided
+                        implementation(libs.findLibrary("arcgis-maps-kotlin").get())
+                        implementation(platform(libs.findLibrary("arcgis-maps-kotlin-toolkit-bom").get()))
+                        implementation(libs.findLibrary("arcgis-maps-kotlin-toolkit-geoview-compose").get())
+                    }
+                    // Local project common samples library
+                    implementation(project(":samples-lib"))
+                }
             }
         }
     }
