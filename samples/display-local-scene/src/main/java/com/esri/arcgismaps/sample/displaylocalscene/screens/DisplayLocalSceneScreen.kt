ebb8b48b8477da019d3a3dc01461858b762bb397
Colin Anderson <colin_anderson@esri.com>
2025-11-25 12:17:31 +0000

    Correct error in camera values

--- samples/display-local-scene/src/main/java/com/esri/arcgismaps/sample/displaylocalscene/screens/DisplayLocalSceneScreen.kt
+++ samples/display-local-scene/src/main/java/com/esri/arcgismaps/sample/displaylocalscene/screens/DisplayLocalSceneScreen.kt
@@ -57,18 +57,18 @@ fun DisplayLocalSceneScreen(sampleName: String) {
                     ArcGISTiledElevationSource("https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer")
                 }
 
-                val camera = remember {
-                    Camera(
-                        locationPoint = Point(
-                            19455578.6821,
-                            -5056336.2227,
-                            1699.3366,
-                            SpatialReference.webMercator()),
-                        heading = 338.7410,
-                        pitch = 40.3763,
-                        roll = 0.0,
-                    )
-                }
+//                val camera = remember {
+//                    Camera(
+//                        locationPoint = Point(
+//                            19455578.6821,
+//                            -5056336.2227,
+//                            1699.3366,
+//                            SpatialReference.webMercator()),
+//                        heading = 338.7410,
+//                        pitch = 40.3763,
+//                        roll = 0.0,
+//                    )
+//                }
 
                 val arcGISScene = remember {
                     ArcGISScene(
@@ -81,18 +81,16 @@ fun DisplayLocalSceneScreen(sampleName: String) {
                         initialViewpoint = Viewpoint(
                             center = Point(19455026.8116, -5054995.7415, SpatialReference.webMercator()),
                             scale = 8314.6991,
-                            camera = camera
-//                            camera = Camera(
-//                                locationPoint = Point(
-//                                    x = 19455578.6821,
-//                                    y = 5056336.2227,
-//                                    z = 1699.3366,
-//                                    spatialReference = SpatialReference.webMercator()
-//                                ),
-//                                heading = 338.7410,
-//                                pitch = 40.3763,
-//                                roll = 0.0
-//                            )
+                            camera = Camera(
+                                locationPoint = Point(
+                                    19455578.6821,
+                                    -5056336.2227,
+                                    1699.3366,
+                                    SpatialReference.webMercator()),
+                                heading = 338.7410,
+                                pitch = 40.3763,
+                                roll = 0.0,
+                            )
                         )
                         clippingArea = Envelope(
                             xMin = 19454578.8235,
