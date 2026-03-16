58c490b1f5400594ee5509b847c902791ecd37c4
rama6471 <rchintapalli@esri.com>
2026-03-16 08:55:12 -0700

    remove unused imports

--- samples/set-feature-layer-rendering-mode-on-map/src/main/java/com/esri/arcgismaps/sample/setfeaturelayerrenderingmodeonmap/screens/SetFeatureLayerRenderingModeOnMapScreen.kt
+++ samples/set-feature-layer-rendering-mode-on-map/src/main/java/com/esri/arcgismaps/sample/setfeaturelayerrenderingmodeonmap/screens/SetFeatureLayerRenderingModeOnMapScreen.kt
@@ -16,10 +16,8 @@
 
 package com.esri.arcgismaps.sample.setfeaturelayerrenderingmodeonmap.screens
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
