8d584f6fa263452c67bf139f95dc13d11265a206
rom14514 <rsavitski@esri.com>
2026-03-30 11:26:25 +0100

    add import

--- samples/configure-scene-environment/src/main/java/com/esri/arcgismaps/sample/configuresceneenvironment/screens/ConfigureSceneEnvironmentScreen.kt
+++ samples/configure-scene-environment/src/main/java/com/esri/arcgismaps/sample/configuresceneenvironment/screens/ConfigureSceneEnvironmentScreen.kt
@@ -34,6 +34,7 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SegmentedButton
 import androidx.compose.material3.SegmentedButtonDefaults
