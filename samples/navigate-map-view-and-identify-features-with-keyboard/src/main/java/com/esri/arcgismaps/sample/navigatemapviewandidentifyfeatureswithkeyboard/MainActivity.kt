ca5de0241368f94c209fbfb7a063db82c1ef66b2
Shubham Sharma <shubhamsharma@esri.com>
2026-07-01 09:38:07 -0700

    Remove build config from v.next samples

--- samples/navigate-map-view-and-identify-features-with-keyboard/src/main/java/com/esri/arcgismaps/sample/navigatemapviewandidentifyfeatureswithkeyboard/MainActivity.kt
+++ samples/navigate-map-view-and-identify-features-with-keyboard/src/main/java/com/esri/arcgismaps/sample/navigatemapviewandidentifyfeatureswithkeyboard/MainActivity.kt
@@ -22,9 +22,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import com.arcgismaps.ApiKey
-import com.arcgismaps.ArcGISEnvironment
 import com.esri.arcgismaps.sample.sampleslib.theme.SampleAppTheme
 import com.esri.arcgismaps.sample.navigatemapviewandidentifyfeatureswithkeyboard.screens.NavigateMapViewAndIdentifyFeaturesWithKeyboardScreen
 
@@ -32,9 +29,6 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // authentication with an API key or named user is
-        // required to access basemaps and other location services
-        ArcGISEnvironment.apiKey = ApiKey.create(BuildConfig.ACCESS_TOKEN)
         enableEdgeToEdge()
         setContent {
             SampleAppTheme {
