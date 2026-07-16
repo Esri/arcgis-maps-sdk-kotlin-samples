1cad026fe9486c66f2c96deeb439cfb4a1f060ef
Karishma Kishore <74136680+karishma-kishore@users.noreply.github.com>
2026-07-16 14:42:19 -0700

    removed unused imports

--- samples/add-feature-layers/src/main/java/com/esri/arcgismaps/sample/addfeaturelayers/screens/AddFeatureLayersScreen.kt
+++ samples/add-feature-layers/src/main/java/com/esri/arcgismaps/sample/addfeaturelayers/screens/AddFeatureLayersScreen.kt
@@ -17,8 +17,6 @@
 package com.esri.arcgismaps.sample.addfeaturelayers.screens
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -31,7 +29,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
