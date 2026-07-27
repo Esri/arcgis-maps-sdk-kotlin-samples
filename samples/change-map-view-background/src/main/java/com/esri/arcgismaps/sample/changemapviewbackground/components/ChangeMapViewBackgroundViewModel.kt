471d3ff6a0d165c6e35b0649288a4927f256164b
Karishma Kishore <74136680+karishma-kishore@users.noreply.github.com>
2026-07-27 13:48:05 -0700

    removing unused imports

--- samples/change-map-view-background/src/main/java/com/esri/arcgismaps/sample/changemapviewbackground/components/ChangeMapViewBackgroundViewModel.kt
+++ samples/change-map-view-background/src/main/java/com/esri/arcgismaps/sample/changemapviewbackground/components/ChangeMapViewBackgroundViewModel.kt
@@ -30,7 +30,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import com.arcgismaps.Color as ArcGISColor
 
 class ChangeMapViewBackgroundViewModel(app: Application) : AndroidViewModel(app) {
 
--- samples/change-map-view-background/src/main/java/com/esri/arcgismaps/sample/changemapviewbackground/screens/ChangeMapViewBackgroundSupportingPane.kt
+++ samples/change-map-view-background/src/main/java/com/esri/arcgismaps/sample/changemapviewbackground/screens/ChangeMapViewBackgroundSupportingPane.kt
@@ -33,7 +33,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.esri.arcgismaps.sample.changemapviewbackground.components.ChangeMapViewBackgroundUiState
 import com.esri.arcgismaps.sample.changemapviewbackground.components.ChangeMapViewBackgroundViewModel
 import com.esri.arcgismaps.sample.sampleslib.components.ColorPickerPanel
