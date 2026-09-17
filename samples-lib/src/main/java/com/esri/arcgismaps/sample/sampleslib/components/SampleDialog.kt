e630e9a790e697954ee140adda9a23b451f71a6e
Karishma Kishore <74136680+karishma-kishore@users.noreply.github.com>
2026-09-17 09:14:34 -0700

    removing unused import

--- samples-lib/src/main/java/com/esri/arcgismaps/sample/sampleslib/components/SampleDialog.kt
+++ samples-lib/src/main/java/com/esri/arcgismaps/sample/sampleslib/components/SampleDialog.kt
@@ -35,7 +35,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
 import androidx.compose.ui.semantics.testTagsAsResourceId
