456402e1b43aedb883e7f184d9bef72176e71790
Leon Wang <leonwang@esri.com>
2026-09-21 11:06:16 -0700

    fix metadata_style_checker.py

--- tools/CI/README_Metadata_StyleCheck/metadata_style_checker.py
+++ tools/CI/README_Metadata_StyleCheck/metadata_style_checker.py
@@ -218,7 +218,7 @@ class MetadataCreator:
             self.keywords += self.relevant_apis
             if readme_parts.__contains__('Offline data'):
                 offline_data_section_index = readme_parts.index('Offline data') + 1
-                self.offline_data = parse_provision_from(readme_parts[offline_data_section_index])
+                self.offline_data = [parse_qs(urlparse(item_url).query)["id"][0] for item_url in parse_provision_from(readme_parts[offline_data_section_index])]
 
         except Exception as err:
             print(f'Error parsing README - {self.readme_path} - {err}.')
