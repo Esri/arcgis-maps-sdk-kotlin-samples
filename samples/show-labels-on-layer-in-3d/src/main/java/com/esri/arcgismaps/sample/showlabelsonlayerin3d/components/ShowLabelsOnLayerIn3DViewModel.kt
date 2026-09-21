206ea8674169710a24bf9d544ebede1249864953
mar11613 <mar11613@esri.com>
2026-09-21 14:30:27 -0500

    show-labels-on-layer-in-3d branch

---
+++ samples/show-labels-on-layer-in-3d/src/main/java/com/esri/arcgismaps/sample/showlabelsonlayerin3d/components/ShowLabelsOnLayerIn3DViewModel.kt
@@ -0,0 +1,98 @@
+/* Copyright 2026 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.esri.arcgismaps.sample.showlabelsonlayerin3d.components
+
+import android.app.Application
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.arcgismaps.Color
+import com.arcgismaps.arcade.ArcadeExpression
+import com.arcgismaps.arcgisservices.LabelingPlacement
+import com.arcgismaps.mapping.ArcGISScene
+import com.arcgismaps.mapping.PortalItem
+import com.arcgismaps.mapping.labeling.ArcadeLabelExpression
+import com.arcgismaps.mapping.labeling.LabelDefinition
+import com.arcgismaps.mapping.layers.FeatureLayer
+import com.arcgismaps.mapping.layers.GroupLayer
+import com.arcgismaps.mapping.symbology.TextSymbol
+import com.arcgismaps.portal.Portal
+import com.esri.arcgismaps.sample.sampleslib.components.MessageDialogViewModel
+import kotlinx.coroutines.launch
+
+class ShowLabelsOnLayerIn3DViewModel(app: Application) : AndroidViewModel(app) {
+
+    val arcGISScene by mutableStateOf(
+        ArcGISScene(
+            item = PortalItem(
+                portal = Portal.arcGISOnline(Portal.Connection.Anonymous),
+                itemId = "850dfee7d30f4d9da0ebca34a533c169"
+            )
+        )
+    )
+
+    // Create a message dialog view model for handling error messages
+    val messageDialogVM = MessageDialogViewModel()
+
+    init {
+        viewModelScope.launch {
+            //Load the scene
+            arcGISScene.load().onFailure { messageDialogVM.showMessageDialog(it) }
+            arcGISScene.apply {
+                // Get the feature layer.
+                val operationalLayers = this.operationalLayers
+                val groupLayer = operationalLayers.find { it.name == "Gas" } as GroupLayer
+                val layer = groupLayer.layers.find { it.name == "Gas Main" } as FeatureLayer
+                layer.apply {
+                    // Enable labels on the feature layer.
+                    this.labelsEnabled = true
+                    this.labelDefinitions.clear()
+                    val labelDefinition = makeLabelDefinition()
+                    // Add the label definition to the layer.
+                    this.labelDefinitions.add(labelDefinition)
+                }
+            }
+        }
+    }
+
+
+    fun makeLabelDefinition() : LabelDefinition {
+        // Make and stylize the text symbol.
+        val textSymbol = TextSymbol().apply {
+            this.color = Color.red
+            this.haloColor = Color.white
+            this.haloWidth = 2.0f
+            this.size = 16.0f
+        }
+
+        // Create and return a label definition using the text symbol.
+        val labelDefinition = LabelDefinition(
+            labelExpression = ArcadeLabelExpression(
+                arcadeExpression = ArcadeExpression(
+                    expression = $$"Text($feature.INSTALLATIONDATE, `DD MMM YY`)"
+                )
+            ),
+            textSymbol = textSymbol
+        ).apply {
+            placement = LabelingPlacement.LineAboveAlong
+            useCodedValues = true
+        }
+        return labelDefinition
+    }
+}
+
